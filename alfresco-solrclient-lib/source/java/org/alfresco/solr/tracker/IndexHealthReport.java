eb9c080d1af89f75b84cd64ad9a7906487e46df1
Mark Rogers <mark.rogers@alfresco.com>
2014-07-23 10:50:14 +0000

    Merged HEAD-BUG-FIX (5.0/Cloud) to HEAD (5.0/Cloud)    76927: Merged PLATFORM1 (5.0/Cloud) to HEAD-BUG-FIX (5.0/Cloud)       67096: Merged BRANCHES/DEV/aowian/plat1_solr4 to BRANCHES/DEV/PLATFORM1       BRANCHES/DEV/PLATFORM1:          65032: ACE-916: Carve out tracking from SOLR 1.4               - Initial checkin. Does not compile          65034: ACE-916: Carve out tracking from SOLR 1.4               - Another checkin. Does not compile          65036: ACE-916: Carve out tracking from SOLR 1.4               - Another checkin. Does not compile               - Added InformationServer interface          65393: ACE-916: Carve out tracking from SOLR 1.4               - Another checkin. Does not compile          65963: ACE-916: Carve out tracking from SOLR 1.4               - Changed references of toDocument method to LegacySolrInformationServer          65988: ACE-916: Carve out tracking from SOLR 1.4               - Created individual classes and got CoreTracker and LegacySolrInformationServer to compile          66090: ACE-916: Carve out tracking from SOLR 1.4               - Getting closer to no red          66091: ACE-916: Carve out tracking from SOLR 1.4               - Getting closer to no red          66092: ACE-916: Carve out tracking from SOLR 1.4               - Getting closer to no red          66847: ACE-916: Carve out tracking from SOLR 1.4               - Updated eclipse .project files to include SONAR nature               - Updated solr4 pom to use different port               - Added adapter for solr data object in SOLR Client, therefore introducing dependency on legacy jars while keeping the main client code version agnostic               - TODO: Finish making Tracker API work in client project, and explore moving trackers into client project          66863: ACE-916: Carve out tracking from SOLR 1.4               - Moved Tracker, TrackerStats, etc into Client project, and abstracted some data objects.               - TODO: Deal with AlfrescoSolrDataModel          66926: ACE-916: Carve out tracking from SOLR 1.4               - Removed some classes that had already been moved               - TODO: Deal with AlfrescoSolrDataModel          66957: ACE-916: Carve out tracking from SOLR 1.4               - Moved adapters into legacy solr project and removed client project's dependency on legacy jars.               - TODO: Deal with AlfrescoSolrDataModel          66959: ACE-916: Carve out tracking from SOLR 1.4               - Moved AlfrescoSolrDataModel to InformationServer and exposed needed APIs               - Finally, the red is gone, and the code compiles.          67027: ACE-916: Carve out tracking from SOLR 1.4               - Removed unused import          67028: ACE-916: Carve out tracking from SOLR 1.4               - Merged PLATFORM1 to aowian/plat1_solr4          67029: ACE-916: Carve out tracking from SOLR 1.4               - Merged PLATFORM1 to aowian/plat1_solr4          67076: ACE-916: Carve out tracking from SOLR 1.4               - Reverse merged from aowian/plat1_solr4 to aowian/plat1_solr4 at -67028          67090: ACE-916: Carve out tracking from SOLR 1.4               - Merged PLATFORM1 at 67076 to aowian/plat1_solr4          67093: ACE-916: Carve out tracking from SOLR 1.4               - Merged PLATFORM1 at 67085 to aowian/plat1_solr4
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/alfresco/HEAD/root@77771 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261

---
+++ alfresco-solrclient-lib/source/java/org/alfresco/solr/tracker/IndexHealthReport.java
@@ -0,0 +1,428 @@
+/*
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.solr.tracker;
+
+import org.alfresco.solr.InformationServer;
+import org.alfresco.solr.adapters.IOpenBitSet;
+
+
+public class IndexHealthReport
+{
+    long dbTransactionCount;
+
+    IOpenBitSet missingTxFromIndex;
+
+    IOpenBitSet duplicatedTxInIndex;
+
+    IOpenBitSet txInIndexButNotInDb;
+
+    IOpenBitSet duplicatedLeafInIndex;
+    
+    IOpenBitSet duplicatedAuxInIndex;
+
+    long transactionDocsInIndex;
+    
+    long uniqueTransactionDocsInIndex;
+    
+    long uniqueAclTransactionDocsInIndex;
+
+    long aclTransactionDocsInIndex;
+
+    long leafDocCountInIndex;
+    
+    long auxDocCountInIndex;
+
+    long lastIndexedCommitTime;
+
+    long lastIndexedIdBeforeHoles;
+
+    long dbAclTransactionCount;
+
+    IOpenBitSet missingAclTxFromIndex;
+
+    IOpenBitSet duplicatedAclTxInIndex;
+
+    IOpenBitSet aclTxInIndexButNotInDb;
+    
+    IOpenBitSet duplicatedErrorInIndex;
+    
+    IOpenBitSet duplicatedUnindexedInIndex;
+    
+    long errorDocCountInIndex;
+    
+    long unindexedDocCountInIndex;
+
+    public IndexHealthReport(InformationServer srv)
+    {
+        this.missingTxFromIndex = srv.getOpenBitSetInstance();
+        this.duplicatedTxInIndex = srv.getOpenBitSetInstance();
+        this.txInIndexButNotInDb = srv.getOpenBitSetInstance();
+        this.duplicatedLeafInIndex = srv.getOpenBitSetInstance();
+        this.duplicatedAuxInIndex = srv.getOpenBitSetInstance();
+        
+        this.missingAclTxFromIndex = srv.getOpenBitSetInstance();
+        this.duplicatedAclTxInIndex = srv.getOpenBitSetInstance();
+        this.aclTxInIndexButNotInDb = srv.getOpenBitSetInstance();
+        this.duplicatedErrorInIndex = srv.getOpenBitSetInstance();
+        this.duplicatedUnindexedInIndex = srv.getOpenBitSetInstance();
+    }
+    
+    /**
+     * @return the transactionDocsInIndex
+     */
+    public long getTransactionDocsInIndex()
+    {
+        return transactionDocsInIndex;
+    }
+
+    public long getAclTransactionDocsInIndex()
+    {
+        return aclTransactionDocsInIndex;
+    }
+
+    /**
+     * @param leafCount
+     */
+    public void setLeafDocCountInIndex(long leafDocCountInIndex)
+    {
+        this.leafDocCountInIndex = leafDocCountInIndex;
+    }
+
+    /**
+     * @return the leafDocCountInIndex
+     */
+    public long getLeafDocCountInIndex()
+    {
+        return leafDocCountInIndex;
+    }
+    
+    /**
+     * @param auxCount
+     */
+    public void setAuxDocCountInIndex(long auxDocCountInIndex)
+    {
+        this.auxDocCountInIndex = auxDocCountInIndex;
+    }
+
+    /**
+     * @return the leafDocCountInIndex
+     */
+    public long getAuxDocCountInIndex()
+    {
+        return auxDocCountInIndex;
+    }
+
+    /**
+     * @param txid
+     */
+    public void setDuplicatedLeafInIndex(long txid)
+    {
+        duplicatedLeafInIndex.set(txid);
+
+    }
+
+    /**
+     * @return the duplicatedLeafInIndex
+     */
+    public IOpenBitSet getDuplicatedLeafInIndex()
+    {
+        return duplicatedLeafInIndex;
+    }
+    
+    /**
+     * @param txid
+     */
+    public void setDuplicatedAuxInIndex(long txid)
+    {
+        duplicatedAuxInIndex.set(txid);
+
+    }
+
+    /**
+     * @return the duplicatedLeafInIndex
+     */
+    public IOpenBitSet getDuplicatedAuxInIndex()
+    {
+        return duplicatedAuxInIndex;
+    }
+
+    /**
+     * @param transactionDocsInIndex
+     *            the transactionDocsInIndex to set
+     */
+    public void setTransactionDocsInIndex(long transactionDocsInIndex)
+    {
+        this.transactionDocsInIndex = transactionDocsInIndex;
+    }
+
+    /**
+     * @param transactionDocsInIndex
+     *            the transactionDocsInIndex to set
+     */
+    public void setAclTransactionDocsInIndex(long aclTransactionDocsInIndex)
+    {
+        this.aclTransactionDocsInIndex = aclTransactionDocsInIndex;
+    }
+
+    /**
+     * @return the missingFromIndex
+     */
+    public IOpenBitSet getMissingTxFromIndex()
+    {
+        return missingTxFromIndex;
+    }
+
+    /**
+     * @return the missingFromIndex
+     */
+    public IOpenBitSet getMissingAclTxFromIndex()
+    {
+        return missingAclTxFromIndex;
+    }
+
+    /**
+     * @return the duplicatedInIndex
+     */
+    public IOpenBitSet getDuplicatedTxInIndex()
+    {
+        return duplicatedTxInIndex;
+    }
+
+    /**
+     * @return the duplicatedInIndex
+     */
+    public IOpenBitSet getDuplicatedAclTxInIndex()
+    {
+        return duplicatedAclTxInIndex;
+    }
+
+    /**
+     * @return the inIndexButNotInDb
+     */
+    public IOpenBitSet getTxInIndexButNotInDb()
+    {
+        return txInIndexButNotInDb;
+    }
+
+    /**
+     * @return the inIndexButNotInDb
+     */
+    public IOpenBitSet getAclTxInIndexButNotInDb()
+    {
+        return aclTxInIndexButNotInDb;
+    }
+
+    /**
+     * @return the dbTransactionCount
+     */
+    public long getDbTransactionCount()
+    {
+        return dbTransactionCount;
+    }
+
+    /**
+     * @param dbTransactionCount
+     *            the dbTransactionCount to set
+     */
+    public void setDbTransactionCount(long dbTransactionCount)
+    {
+        this.dbTransactionCount = dbTransactionCount;
+    }
+
+    public void setMissingTxFromIndex(long txid)
+    {
+        missingTxFromIndex.set(txid);
+    }
+    
+    public void setMissingAclTxFromIndex(long txid)
+    {
+        missingAclTxFromIndex.set(txid);
+    }
+
+    public void setDuplicatedTxInIndex(long txid)
+    {
+        duplicatedTxInIndex.set(txid);
+    }
+    
+    public void setDuplicatedAclTxInIndex(long txid)
+    {
+        duplicatedAclTxInIndex.set(txid);
+    }
+
+    public void setTxInIndexButNotInDb(long txid)
+    {
+        txInIndexButNotInDb.set(txid);
+    }
+    
+    public void setAclTxInIndexButNotInDb(long txid)
+    {
+        aclTxInIndexButNotInDb.set(txid);
+    }
+
+    /**
+     * @param lastIndexCommitTime
+     *            the lastIndexCommitTime to set
+     */
+    public void setLastIndexedCommitTime(long lastIndexedCommitTime)
+    {
+        this.lastIndexedCommitTime = lastIndexedCommitTime;
+    }
+
+    /**
+     * @return the lastIndexedIdBeforeHoles
+     */
+    public long getLastIndexedIdBeforeHoles()
+    {
+        return lastIndexedIdBeforeHoles;
+    }
+
+    /**
+     * @param lastIndexedIdBeforeHoles
+     *            the lastIndexedIdBeforeHoles to set
+     */
+    public void setLastIndexedIdBeforeHoles(long lastIndexedIdBeforeHoles)
+    {
+        this.lastIndexedIdBeforeHoles = lastIndexedIdBeforeHoles;
+    }
+
+    /**
+     * @param cardinality
+     */
+    public void setDbAclTransactionCount(long dbAclTransactionCount)
+    {
+        this.dbAclTransactionCount = dbAclTransactionCount;
+    }
+
+    /**
+     * @return the dbAclTransactionCount
+     */
+    public long getDbAclTransactionCount()
+    {
+        return dbAclTransactionCount;
+    }
+
+    /**
+     * @return the uniqueTransactionDocsInIndex
+     */
+    public long getUniqueTransactionDocsInIndex()
+    {
+        return uniqueTransactionDocsInIndex;
+    }
+
+    /**
+     * @param uniqueTransactionDocsInIndex the uniqueTransactionDocsInIndex to set
+     */
+    public void setUniqueTransactionDocsInIndex(long uniqueTransactionDocsInIndex)
+    {
+        this.uniqueTransactionDocsInIndex = uniqueTransactionDocsInIndex;
+    }
+
+    /**
+     * @return the uniqueAclTransactionDocsInIndex
+     */
+    public long getUniqueAclTransactionDocsInIndex()
+    {
+        return uniqueAclTransactionDocsInIndex;
+    }
+
+    /**
+     * @param uniqueAclTransactionDocsInIndex the uniqueAclTransactionDocsInIndex to set
+     */
+    public void setUniqueAclTransactionDocsInIndex(long uniqueAclTransactionDocsInIndex)
+    {
+        this.uniqueAclTransactionDocsInIndex = uniqueAclTransactionDocsInIndex;
+    }
+
+    /**
+     * @return the lastIndexedCommitTime
+     */
+    public long getLastIndexedCommitTime()
+    {
+        return lastIndexedCommitTime;
+    }
+
+    /**
+     * @param unindexedCount
+     */
+    public void setUnindexedDocCountInIndex(long unindexedDocCountInIndex)
+    {
+        this.unindexedDocCountInIndex = unindexedDocCountInIndex;
+    }
+
+    /**
+     * @param txid
+     */
+    public void setDuplicatedUnindexedInIndex(long txid)
+    {
+        duplicatedUnindexedInIndex.set(txid);
+        
+    }
+
+    /**
+     * @param errorCount
+     */
+    public void setErrorDocCountInIndex(long errorDocCountInIndex)
+    {
+        this.errorDocCountInIndex = errorDocCountInIndex;
+    }
+
+    /**
+     * @param txid
+     */
+    public void setDuplicatedErrorInIndex(long txid)
+    {
+        duplicatedErrorInIndex.set(txid);
+        
+    }
+
+    /**
+     * @return the duplicatedErrorInIndex
+     */
+    public IOpenBitSet getDuplicatedErrorInIndex()
+    {
+        return duplicatedErrorInIndex;
+    }
+
+    /**
+     * @return the duplicatedUnindexedInIndex
+     */
+    public IOpenBitSet getDuplicatedUnindexedInIndex()
+    {
+        return duplicatedUnindexedInIndex;
+    }
+
+    /**
+     * @return the errorDocCountInIndex
+     */
+    public long getErrorDocCountInIndex()
+    {
+        return errorDocCountInIndex;
+    }
+
+    /**
+     * @return the unindexedDocCountInIndex
+     */
+    public long getUnindexedDocCountInIndex()
+    {
+        return unindexedDocCountInIndex;
+    }
+    
+    
+
+}
