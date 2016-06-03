eb30c2db861ed5b97c7d85e3cad2489eb8537a37
Gethin James <gethin.james@alfresco.com>
2016-06-03 14:33:30 +0200

    SEARCH-9: Updated the distribution to get the files from the new jar.

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 rm -rf target/alfresco-solr
 rm -rf target/solr-*
-export SOLR_VER=5.1-K_2-SNAPSHOT
+export SOLR_VER=1.0-SNAPSHOT
 docker build --build-arg SOLR_ZIP=target/alfresco-solr-${SOLR_VER}.zip -t dockerreg.alfresco.com/alfresco-solr:6.0${bamboo_buildNumber} -t dockerreg.alfresco.com/alfresco-solr:latest .
 
 echo "Now type: docker run --rm -p 8983:8983 dockerreg.alfresco.com/alfresco-solr:6.0${bamboo_buildNumber}"
