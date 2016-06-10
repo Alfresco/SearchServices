aa5d66c04f86427139255a9e72f0f9778c4c8c79
Gethin James <gethin.james@alfresco.com>
2016-06-10 10:55:12 +0200

    Using a script from source control to build/run the docker image

--- search-services/packaging/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-rm -rf target/alfresco-solr
-rm -rf target/solr-*
-rm -rf target/dependency-maven-plugin-markers
-
-docker build -t dockerreg.alfresco.com/alfresco-solr:6.0${bamboo_buildNumber} -t dockerreg.alfresco.com/alfresco-solr:latest .
-
-echo "Now type: docker run --rm -p 8983:8983 dockerreg.alfresco.com/alfresco-solr:6.0${bamboo_buildNumber}"
