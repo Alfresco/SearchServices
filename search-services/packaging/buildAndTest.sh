196480f7a08909dbf6ce52c8953c1425440b2e53
Gethin James <gethin.james@alfresco.com>
2016-06-14 14:47:04 +0200

    SEARCH-9: Configuring directories for content / models / index

--- search-services/packaging/buildAndTest.sh
+++ search-services/packaging/buildAndTest.sh
@@ -26,8 +26,10 @@ if ! docker inspect "$dockerImage" &> /dev/null; then
 fi
 
 # running tests
-docker run --rm "$dockerImage" [ -d /opt/alfresco-solr/data ] || (echo "Data dir does not exist" && exit 1)
-docker run --rm "$dockerImage" [ -e /opt/alfresco-solr/solrhome/conf/shared.properties ] || (echo "shared.properties does not exist" && exit 1)
+docker run --rm "$dockerImage" [ -d /opt/alfresco-solr/solr ] || (echo "solr dir does not exist" && exit 1)
+docker run --rm "$dockerImage" [ -d /opt/alfresco-solr/data/content ] || (echo "content dir does not exist" && exit 1)
+docker run --rm "$dockerImage" [ -d /opt/alfresco-solr/data/alfrescoModels ] || (echo "alfrescoModels dir does not exist" && exit 1)
+docker run --rm "$dockerImage" [ -e /opt/alfresco-solr/data/solrhome/conf/shared.properties ] || (echo "shared.properties does not exist" && exit 1)
 docker run --rm "$dockerImage" /opt/alfresco-solr/solr/bin/solr start
 
 echo "Publishing $dockerImage..."
---
+++ search-services/packaging/src/test/resources/solr.in.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Path to a directory for Solr to store cores and their data. By default, Solr will use server/solr
+# This directory needs to contain solr.xml
+SOLR_HOME=$DIST_DIR/data/solrhome
