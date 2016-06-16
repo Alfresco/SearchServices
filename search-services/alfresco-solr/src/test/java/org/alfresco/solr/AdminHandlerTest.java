31f749efb86113306a8497c676d8e616f89ea328
Gethin James <gethin.james@alfresco.com>
2016-06-16 11:25:25 +0200

    SEARCH-66: AlfrescoCoreAdminHandler.handleCustomAction now handles unknown actions

---
+++ search-services/alfresco-solr/src/test/java/org/alfresco/solr/AdminHandlerTest.java
@@ -0,0 +1,62 @@
+package org.alfresco.solr;
+
+import org.apache.lucene.util.LuceneTestCase;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.SolrTestCaseJ4;
+import org.apache.solr.common.params.CoreAdminParams;
+import org.apache.solr.handler.admin.CoreAdminHandler;
+import org.apache.solr.response.SolrQueryResponse;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+@LuceneTestCase.SuppressCodecs({"Appending","Lucene3x","Lucene40","Lucene41","Lucene42","Lucene43", "Lucene44", "Lucene45","Lucene46","Lucene47","Lucene48","Lucene49"})
+@SolrTestCaseJ4.SuppressSSL
+public class AdminHandlerTest extends AlfrescoSolrTestCaseJ4 {
+
+    static CoreAdminHandler admin;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        initAlfrescoCore("solrconfig-afts.xml", "schema-afts.xml");
+        admin = h.getCoreContainer().getMultiCoreHandler();
+    }
+
+    @Test(expected = SolrException.class)
+    public void testUnhandled() throws Exception
+    {
+        SolrQueryResponse resp = new SolrQueryResponse();
+        admin.handleRequestBody(req(CoreAdminParams.ACTION, "totalnonsense",
+                CoreAdminParams.NAME, h.getCore().getName()),
+                resp);
+    }
+
+    @Test
+    public void testhandledTestActions() throws Exception
+    {
+        requestAction("test");
+        requestAction("CMISTEST");
+        requestAction("authTEST");
+    }
+
+    @Test
+    public void testhandledCores() throws Exception
+    {
+        requestAction("newCore");
+        requestAction("updateCore");
+        requestAction("removeCore");
+    }
+
+    @Test
+    public void testhandledReports() throws Exception
+    {
+        requestAction("CHECK");
+        requestAction("REPORT");
+        requestAction("SUMMARY");
+    }
+
+    private void requestAction(String actionName) throws Exception {
+        admin.handleRequestBody(req(CoreAdminParams.ACTION, actionName,
+                CoreAdminParams.NAME, h.getCore().getName()),
+                new SolrQueryResponse());
+    }
+}
