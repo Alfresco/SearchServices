9a87e266941c3f381f9b6a80176c1ae1ee948313
Michael Suzuki <michael.suzuki@alfresco.com>
2016-07-01 14:50:57 +0100

    inital commit of distributed test fix

---
+++ search-services/alfresco-solr/src/test/java/org/alfresco/solr/AlfrescoDistributedTest.java
@@ -0,0 +1,29 @@
+
+package org.alfresco.solr;
+
+import org.alfresco.distributed.AbstractAlfrescoDistributedTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.carrotsearch.randomizedtesting.RandomizedRunner;
+@RunWith(RandomizedRunner.class)
+public class AlfrescoDistributedTest extends AbstractAlfrescoDistributedTest
+{
+    @Before
+    public void setup() throws Exception
+    {
+        del("*:*");
+        index_specific(0, "id", "1", "content@s___t@{http://www.alfresco.org/model/content/1.0}content", "YYYY");
+        index_specific(0, "id", "2", "content@s___t@{http://www.alfresco.org/model/content/1.0}content", "YYYY");
+        index_specific(1, "id", "3", "content@s___t@{http://www.alfresco.org/model/content/1.0}content", "YYYY");
+        index_specific(1, "id", "4", "content@s___t@{http://www.alfresco.org/model/content/1.0}content", "YYYY");
+        commit();
+    }
+    @Test
+    public void test() throws Exception
+    {
+        query("{\"locales\":[\"en\"], \"templates\": [{\"name\":\"t1\", \"template\":\"%cm:content\"}]}",
+                params("q", "t1:YYYY", "qt", "/afts", "shards.qt","/afts","start", "0", "rows", "6", "sort", "id asc"));
+    }
+}
