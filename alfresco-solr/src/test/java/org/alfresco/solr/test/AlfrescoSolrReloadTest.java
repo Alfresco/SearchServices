9cc0a9bec27b000a9d205cf51f522429b7cbcfd6
Gethin James <gethin.james@alfresco.com>
2016-07-21 17:00:00 +0200

    SEARCH-94: Added a basic reload() test

---
+++ alfresco-solr/src/test/java/org/alfresco/solr/test/AlfrescoSolrReloadTest.java
@@ -0,0 +1,84 @@
+package org.alfresco.solr.test;
+
+import org.alfresco.repo.search.adaptor.lucene.QueryConstants;
+import org.alfresco.solr.AbstractAlfrescoSolrTests;
+import org.alfresco.solr.client.Acl;
+import org.alfresco.solr.client.AclChangeSet;
+import org.alfresco.solr.client.AclReaders;
+import org.alfresco.solr.client.SOLRAPIQueueClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.LegacyNumericRangeQuery;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.util.LuceneTestCase;
+import org.apache.solr.SolrTestCaseJ4;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.alfresco.solr.AlfrescoSolrUtils.*;
+import static org.alfresco.solr.AlfrescoSolrUtils.list;
+
+@LuceneTestCase.SuppressCodecs({"Appending","Lucene3x","Lucene40","Lucene41","Lucene42","Lucene43", "Lucene44", "Lucene45","Lucene46","Lucene47","Lucene48","Lucene49"})
+@SolrTestCaseJ4.SuppressSSL
+public class AlfrescoSolrReloadTest extends AbstractAlfrescoSolrTests {
+    private static Log logger = LogFactory.getLog(org.alfresco.solr.tracker.AlfrescoSolrTrackerTest.class);
+    private static long MAX_WAIT_TIME = 80000;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        initAlfrescoCore("solrconfig-afts.xml", "schema-afts.xml");
+    }
+
+    @After
+    public void clearQueue() throws Exception {
+        SOLRAPIQueueClient.nodeMetaDataMap.clear();
+        SOLRAPIQueueClient.transactionQueue.clear();
+        SOLRAPIQueueClient.aclChangeSetQueue.clear();
+        SOLRAPIQueueClient.aclReadersMap.clear();
+        SOLRAPIQueueClient.aclMap.clear();
+        SOLRAPIQueueClient.nodeMap.clear();
+    }
+
+    @Test
+    public void testReload() throws Exception {
+        /*
+        * Create and index an AclChangeSet.
+        */
+
+        logger.info("######### Starting tracker test ###########");
+        AclChangeSet aclChangeSet = getAclChangeSet(1);
+
+        Acl acl = getAcl(aclChangeSet);
+        Acl acl2 = getAcl(aclChangeSet);
+
+
+        AclReaders aclReaders = getAclReaders(aclChangeSet, acl, list("joel"), list("phil"), null);
+        AclReaders aclReaders2 = getAclReaders(aclChangeSet, acl2, list("jim"), list("phil"), null);
+
+
+        indexAclChangeSet(aclChangeSet,
+                list(acl, acl2),
+                list(aclReaders, aclReaders2));
+
+
+        //Check for the ACL state stamp.
+        BooleanQuery.Builder builder = new BooleanQuery.Builder();
+        builder.add(new BooleanClause(new TermQuery(new Term(QueryConstants.FIELD_SOLR4_ID, "TRACKER!STATE!ACLTX")), BooleanClause.Occur.MUST));
+        builder.add(new BooleanClause(LegacyNumericRangeQuery.newLongRange(QueryConstants.FIELD_S_ACLTXID, aclChangeSet.getId(), aclChangeSet.getId() + 1, true, false), BooleanClause.Occur.MUST));
+        BooleanQuery waitForQuery = builder.build();
+        waitForDocCount(waitForQuery, 1, MAX_WAIT_TIME);
+
+        logger.info("#################### Passed First Test ##############################");
+
+        h.reload();
+        TimeUnit.SECONDS.sleep(8);
+        waitForDocCount(waitForQuery, 1, MAX_WAIT_TIME);
+    }
+}
