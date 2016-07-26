003945bd199999c4d9be1d69b9495a8629988c80
Gethin James <gethin.james@alfresco.com>
2016-07-26 11:26:30 +0200

    Revert "SEARCH-97: Handles NULL state object in finally{}"
    This reverts commit 049b82c897ad8e8d5e47725e8e15e8e092c2a443.

--- search-services/alfresco-solr/src/main/java/org/alfresco/solr/tracker/AbstractTracker.java
+++ search-services/alfresco-solr/src/main/java/org/alfresco/solr/tracker/AbstractTracker.java
@@ -133,14 +133,14 @@ public abstract class AbstractTracker implements Tracker
     public void track()
     {
         if(runLock.availablePermits() == 0) {
-            log.info("... " + this.getClass().getSimpleName() + " " + hashCode() + " for core [" + coreName + "] is already in use "+ this.getClass());
+            log.info("... " + this.getClass().getSimpleName() + " for core [" + coreName + "] is already in use "+ this.getClass());
             return;
         }
 
         try
         {
             runLock.acquire();
-            log.info("... Running " + this.getClass().getSimpleName() + " " + hashCode() + " for core [" + coreName + "].");
+            log.info("... Running " + this.getClass().getSimpleName() + " for core [" + coreName + "].");
 
             if(state == null)
             {
@@ -191,11 +191,8 @@ public abstract class AbstractTracker implements Tracker
         finally
         {
             infoSrv.unregisterTrackerThread();
-            if (state != null)
-            {
-                state.setRunning(false);
-                state.setCheck(false);
-            }
+            state.setRunning(false);
+            state.setCheck(false);
             runLock.release();
         }
     }
