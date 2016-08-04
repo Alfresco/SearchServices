57458b28ae0c1acfb928aca00c5322c905e56947
Paul Brodner <paul.brodner@ness.com>
2016-08-04 15:47:44 +0300

    TAS-188 update packages for models

--- e2e-test/java/org/alfresco/rest/SampleBTest.java
+++ e2e-test/java/org/alfresco/rest/SampleBTest.java
@@ -1,6 +1,6 @@
 package org.alfresco.rest;
 
-import org.alfresco.rest.v1.Sites;
+import org.alfresco.rest.v1.RestSites;
 import org.alfresco.tester.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 public class SampleBTest extends BaseRestTest
 {
     @Autowired
-    Sites onSites;
+    RestSites onSites;
 
     @Test
     public void sampleBTestMethod1()
