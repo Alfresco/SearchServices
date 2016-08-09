eacab1602bf8359b03997067c01aa9eb892478ad
Valentin Popa <P3700520@ness.com>
2016-08-09 17:15:41 +0300

    PUT request sample for comments API

--- e2e-test/java/org/alfresco/rest/v1/SampleCommentsTest.java
+++ e2e-test/java/org/alfresco/rest/v1/SampleCommentsTest.java
@@ -6,6 +6,8 @@ import org.alfresco.dataprep.CMISUtil.DocumentType;
 import org.alfresco.dataprep.ContentService;
 import org.alfresco.rest.exception.JsonToModelConversionException;
 import org.alfresco.rest.model.Content;
+import org.alfresco.rest.model.RestCommentModel;
+import org.alfresco.rest.model.RestCommentsModel;
 import org.alfresco.tester.data.DataUser;
 import org.alfresco.tester.exception.DataPreparationException;
 import org.alfresco.tester.model.UserModel;
@@ -48,7 +50,7 @@ public class SampleCommentsTest extends RestTest
     public void addComments() throws JsonToModelConversionException
     {
         Content content = new Content("This is a new comment");
-        commentsAPI.addCommentToNode(document.getId(), content);
+        commentsAPI.addComment(document.getId(), content);
         Assert.assertEquals(commentsAPI.usingRestWrapper().getStatusCode(), HttpStatus.CREATED.toString(), "Add comments response status code is not correct");
 
     }
@@ -58,6 +60,19 @@ public class SampleCommentsTest extends RestTest
     {
         commentsAPI.getNodeComments(document.getId());
         Assert.assertEquals(commentsAPI.usingRestWrapper().getStatusCode(), HttpStatus.OK.toString(), "Get comments response status code is not correct");
+    }
+
+    @Test
+    public void updateComment() throws JsonToModelConversionException
+    {
+        // add initial comment
+        Content content = new Content("This is a new comment");
+        String commentId = commentsAPI.addComment(document.getId(), content).getId();
+
+        // update comment
+        content = new Content("This is the updated comment");
+        RestCommentModel commentEntry = commentsAPI.updateComment(document.getId(), commentId, content);
+        Assert.assertEquals(commentEntry.getContent(), "This is the updated comment", "New comment was not updated");
     }
 
 }
