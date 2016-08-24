39ab6adca989e73eb238edc5f0958042d9955fa5
bogdanbocancea <bogdan.bocancea@ness.com>
2016-08-24 10:23:27 +0300

    add sanity-cmis.xml and modify test package structure

---
+++ e2e-test/src/test/java/org/alfresco/cmis/CmisTest.java
@@ -0,0 +1,35 @@
+package org.alfresco.cmis;
+import org.alfresco.cmis.CmisWrapper;
+import org.alfresco.utility.ServerHealth;
+import org.alfresco.utility.data.DataContent;
+import org.alfresco.utility.data.DataSite;
+import org.alfresco.utility.data.DataUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
+
+@ContextConfiguration("classpath:alfresco-cmis-context.xml")
+public abstract class CmisTest extends AbstractTestNGSpringContextTests
+{
+    @Autowired
+    CmisWrapper cmisApi;
+
+    @Autowired
+    protected DataUser dataUser;
+
+    @Autowired
+    protected DataSite dataSite;
+    
+    @Autowired
+    protected DataContent dataContent;
+    
+    @Autowired
+    ServerHealth serverHealth;
+
+    @BeforeClass(alwaysRun = true)
+    public void setup() throws Exception
+    {
+        serverHealth.assertServerIsOnline();
+    }
+}
