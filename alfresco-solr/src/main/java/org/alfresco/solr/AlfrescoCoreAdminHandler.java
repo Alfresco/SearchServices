8865e7ace5bdefc74be3a04f2c606317e9a727e1
Gethin James <gethin.james@alfresco.com>
2016-09-13 12:12:00 +0200

    SEARCH-146: Moving handler methods to their own class.

--- alfresco-solr/src/main/java/org/alfresco/solr/AlfrescoCoreAdminHandler.java
+++ alfresco-solr/src/main/java/org/alfresco/solr/AlfrescoCoreAdminHandler.java
@@ -63,6 +63,7 @@ import org.apache.solr.response.SolrQueryResponse;
 import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import static org.alfresco.solr.HandlerOfResources.*;
 
 public class AlfrescoCoreAdminHandler extends CoreAdminHandler
 {
@@ -981,27 +982,6 @@ public class AlfrescoCoreAdminHandler extends CoreAdminHandler
         }
     }
 
-
-    private boolean getSafeBoolean(SolrParams params, String paramName)
-    {
-        boolean paramValue = false;
-        if (params.get(paramName) != null)
-        {
-            paramValue = Boolean.valueOf(params.get(paramName));
-        }
-        return paramValue;
-    }
-
-    private Long getSafeLong(SolrParams params, String paramName)
-    {
-        Long paramValue = null;
-        if (params.get(paramName) != null)
-        {
-            paramValue = Long.valueOf(params.get(paramName));
-        }
-        return paramValue;
-    }
-
     private void actionNODEREPORTS(SolrQueryResponse rsp, SolrParams params, String cname) throws IOException,
                 JSONException
     {
@@ -1357,141 +1337,6 @@ public class AlfrescoCoreAdminHandler extends CoreAdminHandler
         }
     }
 
-    
-    
-    /**
-     * Note files can alter due to background processes so file not found is Ok
-     * 
-     * @param srcDir
-     * @param destDir
-     * @param preserveFileDate
-     * @throws IOException
-     */
-    static void copyDirectory(File srcDir, File destDir, boolean preserveFileDate) throws IOException
-    {
-        if (destDir.exists())
-        {
-            throw new IOException("Destination should be created from clean");
-        }
-        else
-        {
-            if (!destDir.mkdirs()) { throw new IOException("Destination '" + destDir + "' directory cannot be created"); }
-            if (preserveFileDate)
-            {
-                // OL if file not found so does not need to check
-                destDir.setLastModified(srcDir.lastModified());
-            }
-        }
-        if (!destDir.canWrite()) { throw new IOException("No access to destination directory" + destDir); }
-
-        File[] files = srcDir.listFiles();
-        if (files != null)
-        {
-            for (int i = 0; i < files.length; i++)
-            {
-                File currentCopyTarget = new File(destDir, files[i].getName());
-                if (files[i].isDirectory())
-                {
-                    copyDirectory(files[i], currentCopyTarget, preserveFileDate);
-                }
-                else
-                {
-                    copyFile(files[i], currentCopyTarget, preserveFileDate);
-                }
-            }
-        }
-    }
-
-    private static void copyFile(File srcFile, File destFile, boolean preserveFileDate) throws IOException
-    {
-        try
-        {
-            if (destFile.exists()) { throw new IOException("File shoud not exist " + destFile); }
-
-            FileInputStream input = new FileInputStream(srcFile);
-            try
-            {
-                FileOutputStream output = new FileOutputStream(destFile);
-                try
-                {
-                    copy(input, output);
-                }
-                finally
-                {
-                    try
-                    {
-                        output.close();
-                    }
-                    catch (IOException io)
-                    {
-
-                    }
-                }
-            }
-            finally
-            {
-                try
-                {
-                    input.close();
-                }
-                catch (IOException io)
-                {
-
-                }
-            }
-
-            // check copy
-            if (srcFile.length() != destFile.length()) { throw new IOException("Failed to copy full from '" + srcFile
-                        + "' to '" + destFile + "'"); }
-            if (preserveFileDate)
-            {
-                destFile.setLastModified(srcFile.lastModified());
-            }
-        }
-        catch (FileNotFoundException fnfe)
-        {
-            fnfe.printStackTrace();
-        }
-    }
-
-    private static int copy(InputStream input, OutputStream output) throws IOException
-    {
-        byte[] buffer = new byte[2048 * 4];
-        int count = 0;
-        int n = 0;
-        while ((n = input.read(buffer)) != -1)
-        {
-            output.write(buffer, 0, n);
-            count += n;
-        }
-        return count;
-    }
-
-    static void deleteDirectory(File directory) throws IOException
-    {
-        if (!directory.exists()) { return; }
-        if (!directory.isDirectory()) { throw new IllegalArgumentException("Not a directory " + directory); }
-
-        File[] files = directory.listFiles();
-        if (files == null) { throw new IOException("Failed to delete director - no access" + directory); }
-
-        for (int i = 0; i < files.length; i++)
-        {
-            File file = files[i];
-
-            if (file.isDirectory())
-            {
-                deleteDirectory(file);
-            }
-            else
-            {
-                if (!file.delete()) { throw new IOException("Unable to delete file: " + file); }
-            }
-        }
-
-        if (!directory.delete()) { throw new IOException("Unable to delete directory " + directory); }
-    }
-    
     public ConcurrentHashMap<String, InformationServer> getInformationServers()
     {
         return this.informationServers;
