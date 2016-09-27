ee547f221882c7d6ce6b7bdbc9783d57f33bcd61
bogdanbocancea <bogdan.bocancea@ness.com>
2016-09-27 18:06:27 +0300

    update CmisTest with @Scope

--- e2e-test/src/test/java/org/alfresco/cmis/CmisTest.java
+++ e2e-test/src/test/java/org/alfresco/cmis/CmisTest.java
@@ -6,11 +6,15 @@ import org.alfresco.utility.data.DataSite;
 import org.alfresco.utility.data.DataUser;
 import org.alfresco.utility.network.ServerHealth;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.BeforeClass;
 
 @ContextConfiguration("classpath:alfresco-cmis-context.xml")
+@Component
+@Scope(value = "prototype")
 public abstract class CmisTest extends AbstractTestNGSpringContextTests
 {
     @Autowired
