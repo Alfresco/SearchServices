e8a4e73cb66c0337fd89ea5c51efeacea8e28cf1
Cristina Jalba <cristina.jalba@ness.com>
2016-09-27 16:39:22 +0300

    TAS-863 getComment created by another user

--- e2e-test/java/org/alfresco/rest/GetCommentsTest.java
+++ e2e-test/java/org/alfresco/rest/GetCommentsTest.java
@@ -103,4 +103,17 @@ public class GetCommentsTest extends RestTest
         userModel.setPassword("password");
         restClient.authenticateUser(userModel);
     }
+    
+    @TestRail(section={"rest-api", "comments"}, executionType= ExecutionType.SANITY,
+            description= "Verify Manager user gets comments created by another user and status code is 200")
+    public void managerIsAbleToRetrieveCommentsCreatedByAnotherUser() throws JsonToModelConversionException, Exception
+    {
+        userModel = usersWithRoles.get(UserRole.SiteCollaborator);
+        restClient.authenticateUser(userModel);
+        commentsAPI.addComment(document.getNodeRef(), "This is a new comment added by " + userModel.getUsername());
+        commentsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED.toString()); 
+        restClient.authenticateUser(usersWithRoles.get(UserRole.SiteManager));
+        commentsAPI.getNodeComments(document.getNodeRef());
+        commentsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK.toString());
+    }
 }
