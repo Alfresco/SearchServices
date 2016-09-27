16758317b886e52ec7e86ce13fe22b6c2b424215
Iulia Nechita <P3700436@ness.com>
2016-09-27 16:52:30 +0300

    TAS-897 getSite with admin user

--- e2e-test/java/org/alfresco/rest/GetSiteTests.java
+++ e2e-test/java/org/alfresco/rest/GetSiteTests.java
@@ -85,4 +85,13 @@ public class GetSiteTests extends RestTest
         siteAPI.getSite(siteModel.getId());
         siteAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK.toString());
     }
+    
+    @TestRail(section = { "rest-api", "sites" }, executionType = ExecutionType.SANITY, description = "Verify user with Consumer role gets site information and gets status code OK (200)")
+    public void getSiteWithAdminRole() throws JsonToModelConversionException, Exception
+    {
+
+        restClient.authenticateUser(adminUserModel);
+        siteAPI.getSite(siteModel.getId());
+        siteAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK.toString());
+    }
 }
