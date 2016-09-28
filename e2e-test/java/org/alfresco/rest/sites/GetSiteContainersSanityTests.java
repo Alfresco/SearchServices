8c5c6bc0a64ef72dddc465f153dd109c282da549
Iulia Nechita <iulia.cojocea@ness.com>
2016-09-28 16:42:02 +0300

    TAS-934 getSiteContainers with Manager role

---
+++ e2e-test/java/org/alfresco/rest/sites/GetSiteContainersSanityTests.java
@@ -0,0 +1,58 @@
+package org.alfresco.rest.sites;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.alfresco.rest.RestTest;
+import org.alfresco.rest.exception.JsonToModelConversionException;
+import org.alfresco.rest.requests.RestSitesApi;
+import org.alfresco.utility.constants.UserRole;
+import org.alfresco.utility.data.DataSite;
+import org.alfresco.utility.data.DataUser;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.testrail.ExecutionType;
+import org.alfresco.utility.testrail.annotation.TestRail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * @author iulia.cojocea
+ */
+@Test(groups = { "rest-api", "sites", "sanity" })
+public class GetSiteContainersSanityTests extends RestTest
+{
+    @Autowired
+    RestSitesApi siteAPI;
+
+    @Autowired
+    DataUser dataUser;
+
+    @Autowired
+    DataSite dataSite;
+
+    private UserModel adminUserModel;
+    private SiteModel siteModel;
+    private HashMap<UserRole, UserModel> usersWithRoles;
+
+    @BeforeClass
+    public void initTest() throws Exception
+    {
+        adminUserModel = dataUser.getAdminUser();
+        siteAPI.useRestClient(restClient);
+        siteModel = dataSite.usingUser(adminUserModel).createPublicRandomSite();
+        usersWithRoles = dataUser.addUsersWithRolesToSite(siteModel,
+                Arrays.asList(UserRole.SiteManager, UserRole.SiteCollaborator, UserRole.SiteConsumer, UserRole.SiteContributor));
+    }
+
+    @TestRail(section = { "rest-api", "sites" }, executionType = ExecutionType.SANITY, 
+            description = "Verify user with Manager role gets site containers and gets status code OK (200)")
+    public void getSiteContainersWithManagerRole() throws JsonToModelConversionException, Exception
+    {
+        restClient.authenticateUser(usersWithRoles.get(UserRole.SiteManager));
+        siteAPI.getSiteContainers(siteModel.getId());
+        siteAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK.toString());
+    }
+}
