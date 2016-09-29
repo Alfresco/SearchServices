26763e1d3566fda4e390509e53b62796727253ef
Cristina Axinte <cristina.axinte@ness.com>
2016-09-29 12:41:46 +0300

    TAS-972 created test: getPerson call test with Collaborator role

--- e2e-test/java/org/alfresco/rest/people/GetPeopleTest.java
+++ e2e-test/java/org/alfresco/rest/people/GetPeopleTest.java
@@ -3,7 +3,6 @@ package org.alfresco.rest.people;
 import org.alfresco.rest.RestTest;
 import org.alfresco.rest.requests.RestPeopleApi;
 import org.alfresco.utility.constants.UserRole;
-import org.alfresco.utility.data.DataUser;
 import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.UserModel;
 import org.alfresco.utility.testrail.ExecutionType;
@@ -41,6 +40,18 @@ public class GetPeopleTest extends RestTest
         dataUser.usingUser(userModel).addUserToSite(managerUser, siteModel, UserRole.SiteManager);
 
         restClient.authenticateUser(managerUser);
+        peopleApi.getPerson(searchedUser.getUsername());
+        peopleApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK.toString());
+    }
+
+    @Test(groups = "sanity")
+    @TestRail(section = { "rest-api", "people" }, executionType = ExecutionType.SANITY, description = "Verify collaborator user gets a person with Rest API and response is successful")
+    public void collaboratorUserChecksIfPersonIsPresent() throws Exception
+    {
+        UserModel collaboratorUser = dataUser.usingAdmin().createRandomTestUser();
+        dataUser.usingUser(userModel).addUserToSite(collaboratorUser, siteModel, UserRole.SiteCollaborator);
+
+        restClient.authenticateUser(collaboratorUser);
         peopleApi.getPerson(searchedUser.getUsername());
         peopleApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK.toString());
     }
