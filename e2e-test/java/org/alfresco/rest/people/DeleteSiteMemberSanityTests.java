680b7fee839239964365db4cfb910729d8a34dc4
Valentin Popa <valentin-iulian.popa@ness.com>
2016-09-29 10:45:17 +0300

    TAS-965 REST API - valid delete site member request returns success status code 204 with Manager role

---
+++ e2e-test/java/org/alfresco/rest/people/DeleteSiteMemberSanityTests.java
@@ -0,0 +1,74 @@
+package org.alfresco.rest.people;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.alfresco.rest.RestTest;
+import org.alfresco.rest.body.SiteMember;
+import org.alfresco.rest.exception.JsonToModelConversionException;
+import org.alfresco.rest.requests.RestPeopleApi;
+import org.alfresco.rest.requests.RestSitesApi;
+import org.alfresco.utility.constants.UserRole;
+import org.alfresco.utility.data.DataSite;
+import org.alfresco.utility.data.DataUser;
+import org.alfresco.utility.exception.DataPreparationException;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.testrail.ExecutionType;
+import org.alfresco.utility.testrail.annotation.TestRail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.social.alfresco.api.entities.Role;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Test(groups = { "rest-api", "people", "sanity" })
+public class DeleteSiteMemberSanityTests extends RestTest
+{
+    @Autowired
+    RestPeopleApi peopleApi;
+    
+    @Autowired
+    RestSitesApi sitesApi;
+
+    @Autowired
+    DataUser dataUser;
+
+    @Autowired
+    DataSite dataSite;
+
+    private SiteModel siteModel;
+    private UserModel adminUser;
+    private UserModel newUser;
+    private SiteMember siteMember;
+    private HashMap<UserRole, UserModel> usersWithRoles;
+
+    @BeforeClass
+    public void initTest() throws DataPreparationException
+    {
+        adminUser = dataUser.getAdminUser();
+        siteModel = dataSite.usingUser(adminUser).createPublicRandomSite();
+        
+        usersWithRoles = dataUser.addUsersWithRolesToSite(siteModel,
+                Arrays.asList(UserRole.SiteManager, UserRole.SiteCollaborator, UserRole.SiteConsumer, UserRole.SiteContributor));
+
+        peopleApi.useRestClient(restClient);
+        sitesApi.useRestClient(restClient);
+    }
+
+    @TestRail(section = { "rest-api", "people" }, 
+                executionType = ExecutionType.SANITY, 
+                description = "Verify site manager is able to delete another member of the site")
+    public void siteManagerCanDeleteSiteMember() throws JsonToModelConversionException, DataPreparationException, Exception
+    {
+        newUser = dataUser.createRandomTestUser();
+        siteMember = new SiteMember(Role.SiteCollaborator.toString(), newUser.getUsername());
+        restClient.authenticateUser(usersWithRoles.get(UserRole.SiteManager));
+        sitesApi.addPerson(siteModel.getId(), siteMember);
+        
+        peopleApi.deleteSiteMember(newUser.getUsername(), siteModel.getId());
+        sitesApi.usingRestWrapper()
+            .assertStatusCodeIs(HttpStatus.NO_CONTENT.toString());
+    }
+
+}
