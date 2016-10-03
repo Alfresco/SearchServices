62416d883fa51441c510e55b5cba3ca11fedeac2
Cristina Jalba <cristina.jalba@ness.com>
2016-10-03 14:35:00 +0300

    TAS-1031 addFavorites with manager role

--- e2e-test/java/org/alfresco/rest/Favorites/PostFavoritesSanityTests.java
+++ e2e-test/java/org/alfresco/rest/Favorites/PostFavoritesSanityTests.java
@@ -4,7 +4,9 @@ import org.alfresco.rest.RestTest;
 import org.alfresco.rest.exception.JsonToModelConversionException;
 import org.alfresco.rest.requests.RestFavoritesApi;
 import org.alfresco.rest.requests.RestSitesApi;
+import org.alfresco.utility.constants.UserRole;
 import org.alfresco.utility.data.DataUser;
+import org.alfresco.utility.data.DataUser.ListUserWithRoles;
 import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.UserModel;
 import org.alfresco.utility.testrail.ExecutionType;
@@ -29,6 +31,7 @@ public class PostFavoritesSanityTests extends RestTest
     
     private UserModel adminUserModel; 
     private SiteModel siteModel;
+    private ListUserWithRoles usersWithRoles;
     
     @BeforeClass(alwaysRun=true)
     public void dataPreparation() throws Exception
@@ -39,6 +42,8 @@ public class PostFavoritesSanityTests extends RestTest
         favoritesAPI.useRestClient(restClient);
         sitesApi.useRestClient(restClient);
         siteModel.setGuid(sitesApi.getSite(siteModel).getGuid());
+        
+        usersWithRoles = dataUser.addUsersWithRolesToSite(siteModel, UserRole.SiteManager, UserRole.SiteCollaborator, UserRole.SiteConsumer, UserRole.SiteContributor);
     }
     
     @TestRail(section={"rest-api", "favorites"}, executionType= ExecutionType.SANITY,
@@ -50,5 +55,12 @@ public class PostFavoritesSanityTests extends RestTest
         favoritesAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
     }
     
-
+    @TestRail(section={"rest-api", "favorites"}, executionType= ExecutionType.SANITY,
+            description= "Verify Manager user add site to favorites with Rest API and status code is 201")
+    public void managerIsAbleToAddToFavorites() throws JsonToModelConversionException, Exception
+    {
+        restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteManager));
+        favoritesAPI.addUserFavorites(usersWithRoles.getOneUserWithRole(UserRole.SiteManager), siteModel);
+        favoritesAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
+    }
 }
