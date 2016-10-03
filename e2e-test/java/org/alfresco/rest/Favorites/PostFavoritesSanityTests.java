11ce80a494ec4dcf7812d993ff852a1b69bf3916
Cristina Jalba <cristina.jalba@ness.com>
2016-10-03 14:32:42 +0300

    TAS-1030 addFavorites with admin user

---
+++ e2e-test/java/org/alfresco/rest/Favorites/PostFavoritesSanityTests.java
@@ -0,0 +1,54 @@
+package org.alfresco.rest.Favorites;
+
+import org.alfresco.rest.RestTest;
+import org.alfresco.rest.exception.JsonToModelConversionException;
+import org.alfresco.rest.requests.RestFavoritesApi;
+import org.alfresco.rest.requests.RestSitesApi;
+import org.alfresco.utility.data.DataUser;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.testrail.ExecutionType;
+import org.alfresco.utility.testrail.annotation.TestRail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Test(groups = { "rest-api", "favorites", "sanity" })
+public class PostFavoritesSanityTests extends RestTest
+{
+
+    @Autowired
+    RestFavoritesApi favoritesAPI;
+    
+    @Autowired
+    RestSitesApi sitesApi;
+    
+    @Autowired
+    DataUser dataUser;
+    
+    private UserModel adminUserModel; 
+    private SiteModel siteModel;
+    
+    @BeforeClass(alwaysRun=true)
+    public void dataPreparation() throws Exception
+    {
+        adminUserModel = dataUser.getAdminUser();
+        restClient.authenticateUser(adminUserModel);
+        siteModel = dataSite.usingUser(adminUserModel).createPublicRandomSite();
+        favoritesAPI.useRestClient(restClient);
+        sitesApi.useRestClient(restClient);
+        siteModel.setGuid(sitesApi.getSite(siteModel).getGuid());
+    }
+    
+    @TestRail(section={"rest-api", "favorites"}, executionType= ExecutionType.SANITY,
+            description= "Verify Admin user add site to favorites with Rest API and status code is 201")
+    public void adminIsAbleToAddToFavorites() throws JsonToModelConversionException, Exception
+    {
+        
+        favoritesAPI.addUserFavorites(adminUserModel, siteModel);
+        favoritesAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
+    }
+    
+
+}
