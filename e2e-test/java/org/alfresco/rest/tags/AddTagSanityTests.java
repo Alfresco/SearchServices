02789f8fa16fa83b0cadabee80b667de89f8260f
cnechifor <corina.luchian@yahoo.com>
2016-10-04 20:39:35 +0300

    code review

--- e2e-test/java/org/alfresco/rest/tags/AddTagSanityTests.java
+++ e2e-test/java/org/alfresco/rest/tags/AddTagSanityTests.java
@@ -6,6 +6,7 @@ import org.alfresco.rest.exception.JsonToModelConversionException;
 import org.alfresco.rest.requests.RestTagsApi;
 import org.alfresco.utility.constants.UserRole;
 import org.alfresco.utility.data.DataUser;
+import org.alfresco.utility.data.RandomData;
 import org.alfresco.utility.model.FileModel;
 import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.UserModel;
@@ -32,6 +33,7 @@ public class AddTagSanityTests extends RestTest
     private FileModel document;
     private SiteModel siteModel;
     private DataUser.ListUserWithRoles usersWithRoles;
+    private String tagValue;
 
     @BeforeClass(alwaysRun=true)
     public void dataPreparation() throws Exception
@@ -47,16 +49,20 @@ public class AddTagSanityTests extends RestTest
             "tags" }, executionType = ExecutionType.SANITY, description = "Verify admin user adds tags with Rest API and status code is 201")
     public void adminIsAbleToAddTag() throws JsonToModelConversionException, Exception
     {
-        tagsAPI.addTag(document, "tag" + adminUserModel.getUsername()).assertTagIs("tag" + adminUserModel.getUsername());
+        tagValue =  RandomData.getRandomName("tag");
+        restClient.authenticateUser(adminUserModel);
+        tagsAPI.addTag(document, tagValue).assertTagIs(tagValue);
         tagsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
+        
     }
 
     @TestRail(section = { "rest-api",
             "tags" }, executionType = ExecutionType.SANITY, description = "Verify Manager user adds tags with Rest API and status code is 201")
     public void managerIsAbleToAddTag() throws JsonToModelConversionException, Exception
     {
+        tagValue =  RandomData.getRandomName("tag");
         restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteManager));
-        tagsAPI.addTag(document, "tag" + UserRole.SiteManager).assertTagIs("tag" + UserRole.SiteManager);
+        tagsAPI.addTag(document, tagValue).assertTagIs(tagValue);
         tagsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
     }
 
@@ -64,8 +70,9 @@ public class AddTagSanityTests extends RestTest
             "tags" }, executionType = ExecutionType.SANITY, description = "Verify Collaborator user adds tags with Rest API and status code is 201")
     public void collaboratorIsAbleToAddTag() throws JsonToModelConversionException, Exception
     {
+        tagValue =  RandomData.getRandomName("tag");
         restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteCollaborator));
-        tagsAPI.addTag(document, "tag" + UserRole.SiteCollaborator).assertTagIs("tag" + UserRole.SiteCollaborator);
+        tagsAPI.addTag(document, tagValue).assertTagIs(tagValue);
         tagsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
     }
 
@@ -73,8 +80,9 @@ public class AddTagSanityTests extends RestTest
             "tags" }, executionType = ExecutionType.SANITY, description = "Verify Contributor user doesn't have permission to add tags with Rest API and status code is 403")
     public void contributorIsNotAbleToAddTag() throws JsonToModelConversionException, Exception
     {
+        tagValue =  RandomData.getRandomName("tag");
         restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteContributor));
-        tagsAPI.addTag(document, "tag" + UserRole.SiteContributor);
+        tagsAPI.addTag(document, tagValue);
         tagsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.FORBIDDEN);
     }
 
@@ -82,8 +90,9 @@ public class AddTagSanityTests extends RestTest
             "tags" }, executionType = ExecutionType.SANITY, description = "Verify Consumer user doesn't have permission to add tags with Rest API and status code is 403")
     public void consumerIsNotAbleToAddTag() throws JsonToModelConversionException, Exception
     {
+        tagValue =  RandomData.getRandomName("tag");
         restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteConsumer));
-        tagsAPI.addTag(document, "tag" + UserRole.SiteConsumer);
+        tagsAPI.addTag(document, tagValue);
         tagsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.FORBIDDEN);
     }
 
