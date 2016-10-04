bf42c68e0e812c818b9a953c83286bea80bd7ebc
Cristina Axinte <cristina.axinte@ness.com>
2016-10-04 20:05:21 +0300

    TAS-1059 getPreference call test with Manager role

---
+++ e2e-test/java/org/alfresco/rest/people/GetPeoplePreferenceTests.java
@@ -0,0 +1,51 @@
+package org.alfresco.rest.people;
+
+import org.alfresco.rest.RestTest;
+import org.alfresco.rest.requests.RestPeopleApi;
+import org.alfresco.utility.constants.PreferenceName;
+import org.alfresco.utility.constants.UserRole;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.testrail.ExecutionType;
+import org.alfresco.utility.testrail.annotation.TestRail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for Get a Peference (/people/{personId}/preferences/{preferenceName}) RestAPI call
+ * 
+ * @author Cristina Axinte
+ *
+ */
+@Test(groups = { "rest-api", "people", "preferences", "sanity" })
+public class GetPeoplePreferenceTests extends RestTest
+{
+    @Autowired
+    RestPeopleApi peopleApi;
+    
+    UserModel userModel;
+    SiteModel siteModel;
+
+    @BeforeClass(alwaysRun=true)
+    public void dataPreparation() throws Exception
+    {
+        userModel = dataUser.createRandomTestUser();
+        siteModel = dataSite.usingUser(userModel).createPublicRandomSite();
+
+        peopleApi.useRestClient(restClient);
+    }
+    
+    @TestRail(section = { "rest-api", "people", "preferences" }, executionType = ExecutionType.SANITY, description = "Verify manager user gets a specific preference with Rest API and response is successful (200)")
+    public void managerUserGetsAPreferenceWithSuccess() throws Exception
+    {
+        UserModel managerUser = dataUser.usingAdmin().createRandomTestUser();
+        dataUser.usingUser(userModel).addUserToSite(managerUser, siteModel, UserRole.SiteManager);
+        dataSite.usingUser(managerUser).usingSite(siteModel).addSiteToFavorites();
+
+        restClient.authenticateUser(managerUser);
+        peopleApi.getPersonPreferenceInformation(managerUser, PreferenceName.SITES_FAVORITES_PREFIX + siteModel.getId());
+        peopleApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+    }
+}
