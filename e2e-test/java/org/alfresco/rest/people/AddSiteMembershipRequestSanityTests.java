86f27ef4ebcf3ceebf3b8aa139e95b84fde10d84
Valentin Popa <valentin-iulian.popa@ness.com>
2016-10-04 11:02:22 +0300

    TAS-886 REST API - valid request returns success status code 201 with Collaborator role

--- e2e-test/java/org/alfresco/rest/people/AddSiteMembershipRequestSanityTests.java
+++ e2e-test/java/org/alfresco/rest/people/AddSiteMembershipRequestSanityTests.java
@@ -1,7 +1,7 @@
 package org.alfresco.rest.people;
 
 import org.alfresco.rest.RestTest;
-import org.alfresco.rest.body.SiteMembership;
+import org.alfresco.rest.body.SiteMembershipRequest;
 import org.alfresco.rest.exception.JsonToModelConversionException;
 import org.alfresco.rest.requests.RestPeopleApi;
 import org.alfresco.utility.constants.UserRole;
@@ -47,18 +47,32 @@ public class AddSiteMembershipRequestSanityTests extends RestTest
         peopleApi.useRestClient(restClient);
     }
 
-    @TestRail(section = { "rest-api","people" }, 
+    @TestRail(section = { "rest-api", "people" }, 
                 executionType = ExecutionType.SANITY, description = "Verify site manager is able to create new site membership request")    
     @Bug(id="MNT-16557")    
     public void siteManagerCanCreateSiteMembershipRequest() throws JsonToModelConversionException, Exception
     {
         UserModel newMember = dataUser.createRandomTestUser();
-        SiteMembership siteMembership = new SiteMembership("Please accept me", siteModel.getId(), "New request");
+        SiteMembershipRequest siteMembership = new SiteMembershipRequest("Please accept me", siteModel.getId(), "New request");
 
         restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteManager));
         peopleApi.addSiteMembershipRequest(newMember, siteMembership);
         peopleApi.usingRestWrapper()
             .assertStatusCodeIs(HttpStatus.CREATED);
     }
+    
+    @TestRail(section = { "rest-api", "people" }, 
+                executionType = ExecutionType.SANITY, description = "Verify site collaborator is able to create new site membership request")
+    @Bug(id = "MNT-16557")
+    public void siteCollaboatorCanCreateSiteMembershipRequest() throws JsonToModelConversionException, Exception
+    {
+        UserModel newMember = dataUser.createRandomTestUser();
+        SiteMembershipRequest siteMembership = new SiteMembershipRequest("Please accept me", siteModel.getId(), "New request");
+
+        restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteCollaborator));
+        peopleApi.addSiteMembershipRequest(newMember, siteMembership);
+        peopleApi.usingRestWrapper()
+            .assertStatusCodeIs(HttpStatus.CREATED);
+}
 
 }
