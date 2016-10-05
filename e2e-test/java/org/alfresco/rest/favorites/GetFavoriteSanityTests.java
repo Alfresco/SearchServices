ec3801961b33cc6f881497e55e51ccad87f3501b
Cristina Jalba <cristina.jalba@ness.com>
2016-10-05 19:11:19 +0300

    TAS-1175 getFavoriteSite with manager

--- e2e-test/java/org/alfresco/rest/favorites/GetFavoriteSanityTests.java
+++ e2e-test/java/org/alfresco/rest/favorites/GetFavoriteSanityTests.java
@@ -76,4 +76,14 @@ public class GetFavoriteSanityTests extends RestTest
         favoritesAPI.getUserFavorite(adminUserModel, fileModel.getNodeRef());
         favoritesAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
     }
+
+    @TestRail(section = { "rest-api",
+            "favorites" }, executionType = ExecutionType.SANITY, description = "Verify Manager user gets favorite site with Rest API and status code is 200")
+    public void managerIsAbleToRetrieveFavorite() throws JsonToModelConversionException, Exception
+    {
+        restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteManager));
+        favoritesAPI.addUserFavorites(usersWithRoles.getOneUserWithRole(UserRole.SiteManager), siteModel);
+        favoritesAPI.getUserFavorite(usersWithRoles.getOneUserWithRole(UserRole.SiteManager), siteModel.getGuid());
+        favoritesAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+    }
 }
