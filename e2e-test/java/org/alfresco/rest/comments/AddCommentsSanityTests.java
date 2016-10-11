a411695a3004f2ff2c088b96a6c8ea924096619d
cagache <claudia.agache@ness.com>
2016-10-11 10:48:34 +0300

    TAS-1381 - add comments by collaborator

--- e2e-test/java/org/alfresco/rest/comments/AddCommentsSanityTests.java
+++ e2e-test/java/org/alfresco/rest/comments/AddCommentsSanityTests.java
@@ -79,4 +79,13 @@ public class AddCommentsSanityTests extends RestTest
         commentsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
     }
 
+    @TestRail(section = { "rest-api",
+            "comments" }, executionType = ExecutionType.SANITY, description = "Verify Collaborator user adds multiple comments with Rest API and status code is 201")
+    public void collaboratorIsAbleToAddComments() throws JsonToModelConversionException, Exception
+    {
+        restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteCollaborator));
+        commentsAPI.addComments(document, comment1, comment2);
+        commentsAPI.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
+    }
+
 }
