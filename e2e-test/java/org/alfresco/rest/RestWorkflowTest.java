8384d3923e42068baf38a7d1498319bdd02ffdfa
Cristina Axinte <cristina.axinte@ness.com>
2016-10-12 11:14:38 +0300

    TAS-1374 getTasks call for asignee

--- e2e-test/java/org/alfresco/rest/RestWorkflowTest.java
+++ e2e-test/java/org/alfresco/rest/RestWorkflowTest.java
@@ -1,11 +1,16 @@
 package org.alfresco.rest;
 
+import org.alfresco.utility.data.DataWorkflow;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.BeforeClass;
 
 import com.jayway.restassured.RestAssured;
 
 public abstract class RestWorkflowTest extends RestTest
 {
+    @Autowired
+    protected DataWorkflow dataWorkflow;
+
     @BeforeClass(alwaysRun = true)
     public void checkServerHealth() throws Exception
     {
--- e2e-test/java/org/alfresco/rest/workflow/tasks/GetTasksSanityTests.java
+++ e2e-test/java/org/alfresco/rest/workflow/tasks/GetTasksSanityTests.java
@@ -1,7 +1,9 @@
 package org.alfresco.rest.workflow.tasks;
 
+import org.alfresco.dataprep.CMISUtil.DocumentType;
 import org.alfresco.rest.RestWorkflowTest;
 import org.alfresco.rest.requests.RestTasksApi;
+import org.alfresco.utility.model.FileModel;
 import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.UserModel;
 import org.alfresco.utility.testrail.ExecutionType;
@@ -19,21 +21,35 @@ public class GetTasksSanityTests extends RestWorkflowTest
 
     UserModel userModel;
     SiteModel siteModel;
+    UserModel candidateUser;
+    FileModel fileModel;
+    UserModel assigneeUser;
 
     @BeforeClass(alwaysRun=true)
     public void dataPreparation() throws Exception
     {
         userModel = dataUser.createRandomTestUser();
         siteModel = dataSite.usingUser(userModel).createPublicRandomSite();
+        fileModel = dataContent.usingSite(siteModel).createContent(DocumentType.TEXT_PLAIN);
+        assigneeUser = dataUser.createRandomTestUser();
+        dataWorkflow.usingUser(userModel).usingSite(siteModel).usingResource(fileModel).createNewTaskAndAssignTo(assigneeUser);
 
         tasksApi.useRestClient(restClient);
     }
 
-    @TestRail(section = { "rest-api", "workflow", "tasks" }, executionType = ExecutionType.SANITY, description = "Verify manager user fails to get an user favorite sites with Rest API (403)")
-    public void managerUserFailsToGetAnUserFavoriteSites() throws Exception
+    @TestRail(section = { "rest-api", "workflow", "tasks" }, executionType = ExecutionType.SANITY, description = "Verify admin user gets all existing tasks with Rest API and response is successfull (200)")
+    public void adminUserGetsAllTasks() throws Exception
     {
-        UserModel adminUser=dataUser.getAdminUser();
+        UserModel adminUser = dataUser.getAdminUser();
         restClient.authenticateUser(adminUser);
+        tasksApi.getTasks().assertEntriesListIsNotEmpty();
+        tasksApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+    }
+    
+    @TestRail(section = { "rest-api", "workflow", "tasks" }, executionType = ExecutionType.SANITY, description = "Verify admin user gets all existing tasks with Rest API and response is successfull (200)")
+    public void asigneeUserGetsItsTasks() throws Exception
+    {
+        restClient.authenticateUser(assigneeUser);
         tasksApi.getTasks().assertEntriesListIsNotEmpty();
         tasksApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
     }
