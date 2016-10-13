daaa0a287a354ba48c6388b57787df2fa9399b9c
cagache <claudia.agache@ness.com>
2016-10-13 12:42:50 +0300

    TAS-1451 - get a specific process by admin

---
+++ e2e-test/java/org/alfresco/rest/workflow/processDefinitions/GetProcessDefinitionSanityTests.java
@@ -0,0 +1,48 @@
+package org.alfresco.rest.workflow.processDefinitions;
+
+import org.alfresco.rest.RestWorkflowTest;
+import org.alfresco.rest.model.RestProcessDefinitionModel;
+import org.alfresco.rest.model.RestProcessDefinitionModelsCollection;
+import org.alfresco.rest.requests.RestProcessDefinitionsApi;
+import org.alfresco.utility.data.DataUser;
+import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.testrail.ExecutionType;
+import org.alfresco.utility.testrail.annotation.TestRail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Claudia Agache on 10/13/2016.
+ */
+@Test(groups = { "rest-api", "process-definitions", "sanity" })
+public class GetProcessDefinitionSanityTests extends RestWorkflowTest
+{
+    @Autowired
+    private DataUser dataUser;
+    @Autowired
+    private RestProcessDefinitionsApi processDefinitionsApi;
+
+    private UserModel testUser;
+    private RestProcessDefinitionModel randomProcessDefinition;
+
+    @BeforeClass(alwaysRun = true)
+    public void dataPreparation() throws Exception
+    {
+        testUser = dataUser.createRandomTestUser();
+        processDefinitionsApi.useRestClient(restClient);
+        restClient.authenticateUser(dataUser.getAdminUser());
+        randomProcessDefinition = processDefinitionsApi.getProcessDefinitions().getOneEntry();
+    }
+
+    @TestRail(section = { "rest-api", "process-definitions" },
+            executionType = ExecutionType.SANITY,
+            description = "Verify Admin user gets a specific process definition for non-network deployments using REST API and status code is OK (200)")
+    public void adminGetsProcessDefinition() throws Exception
+    {
+        processDefinitionsApi.getProcessDefinition(randomProcessDefinition).assertProcessDefinitionNameIs(randomProcessDefinition.onModel().getName());
+        processDefinitionsApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+    }
+
+}
