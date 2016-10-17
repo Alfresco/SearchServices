6d0c6c72aee4f32850fbead3af5a900913be8fb6
Iulia Nechita <iulia.cojocea@ness.com>
2016-10-17 10:21:05 +0300

    TAS-1506, Valid request to create non-existing variable

---
+++ e2e-test/java/org/alfresco/rest/workflow/processes/UpdateProcessVariableSanityTests.java
@@ -0,0 +1,61 @@
+package org.alfresco.rest.workflow.processes;
+
+import org.alfresco.dataprep.CMISUtil.DocumentType;
+import org.alfresco.rest.RestWorkflowTest;
+import org.alfresco.rest.exception.JsonToModelConversionException;
+import org.alfresco.rest.model.RestProcessModel;
+import org.alfresco.rest.model.RestProcessVariableModel;
+import org.alfresco.rest.requests.RestProcessesApi;
+import org.alfresco.utility.data.DataUser;
+import org.alfresco.utility.model.FileModel;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.testrail.ExecutionType;
+import org.alfresco.utility.testrail.annotation.TestRail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * @author iulia.cojocea
+ */
+@Test(groups = { "rest-api", "processes", "sanity" })
+public class UpdateProcessVariableSanityTests extends RestWorkflowTest     
+{
+    @Autowired
+    private DataUser dataUser;
+
+    @Autowired
+    private RestProcessesApi processesApi;
+    
+    private FileModel document;
+    private SiteModel siteModel;
+    private UserModel userWhoStartsTask, assignee;
+    private RestProcessModel processModel;
+    private UserModel adminUser;
+
+    @BeforeClass(alwaysRun = true)
+    public void dataPreparation() throws Exception
+    {
+        adminUser = dataUser.getAdminUser();
+        userWhoStartsTask = dataUser.createRandomTestUser();
+        assignee = dataUser.createRandomTestUser();
+        siteModel = dataSite.usingUser(userWhoStartsTask).createPublicRandomSite();
+        document = dataContent.usingSite(siteModel).createContent(DocumentType.TEXT_PLAIN);
+        dataWorkflow.usingUser(userWhoStartsTask).usingSite(siteModel).usingResource(document).createNewTaskAndAssignTo(assignee);
+        processesApi.useRestClient(restClient);
+    }
+    
+    @TestRail(section = {"rest-api", "processes" }, executionType = ExecutionType.SANITY, 
+            description = "Create non-existing variable using put call")
+    public void addProcessVariable() throws JsonToModelConversionException, Exception
+    {
+        restClient.authenticateUser(adminUser);
+        RestProcessVariableModel variableModel = RestProcessVariableModel.getRandomProcessVariableModel("d:text");
+        processModel = processesApi.getProcesses().getOneEntry();
+        processesApi.updateProcessVariable(processModel, variableModel);
+        processesApi.getProcessesVariables(processModel).assertProcessVariableExists(variableModel);
+        processesApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+    }
+}
