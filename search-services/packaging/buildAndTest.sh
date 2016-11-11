301517871391730cc61f6cd079882ceed0c1aec2
Gethin James <gethin.james@alfresco.com>
2016-11-11 14:42:04 +0100

    SEARCH-223: Better config test and README

--- search-services/packaging/buildAndTest.sh
+++ search-services/packaging/buildAndTest.sh
@@ -15,13 +15,13 @@ rm -f src/docker/alfresco-search-services-*.zip
 cp target/alfresco-search-services-*.zip src/docker
 docker build -t $dockerImage src/docker
 
-# running tests
+echo "Running tests"
 docker run --rm "$dockerImage" [ -d /opt/alfresco-solr/solr ] || (echo "solr dir does not exist" && exit 1)
 docker run --rm "$dockerImage" [ -d /opt/alfresco-solr/data/content ] || (echo "content dir does not exist" && exit 1)
 docker run --rm "$dockerImage" [ -d /opt/alfresco-solr/data/alfrescoModels ] || (echo "alfrescoModels dir does not exist" && exit 1)
 docker run --rm "$dockerImage" [ -e /opt/alfresco-solr/solr.in.sh ] || (echo "solr.in.sh does not exist" && exit 1)
-docker run --rm "$dockerImage" grep -q alfresco /opt/alfresco-solr/solr.in.sh || (echo "solr.in.sh does not contain alfresco config" && exit 1)
-docker run --rm "$dockerImage" grep -q alfresco /opt/alfresco-solr/solr.in.cmd || (echo "solr.in.cmd does not contain alfresco config" && exit 1)
+docker run --rm "$dockerImage" grep -q Alfresco /opt/alfresco-solr/solr.in.sh || (echo "solr.in.sh does not contain Alfresco config" && exit 1)
+docker run --rm "$dockerImage" grep -q Alfresco /opt/alfresco-solr/solr.in.cmd || (echo "solr.in.cmd does not contain Alfresco config" && exit 1)
 docker run --rm "$dockerImage" [ -e /opt/alfresco-solr/solrhome/conf/shared.properties ] || (echo "shared.properties does not exist" && exit 1)
 docker run --rm "$dockerImage" /opt/alfresco-solr/solr/bin/solr start
 
