e5c2e3703ae127347911fa2d23d0d39fefb15972
cagache <claudia.agache@ness.com>
2016-11-16 10:43:23 +0200

    update tests based on new refactored DSL

--- e2e-test/java/org/alfresco/rest/workflow/processes/AddProcessItemSanityTests.java
+++ e2e-test/java/org/alfresco/rest/workflow/processes/AddProcessItemSanityTests.java
@@ -5,16 +5,10 @@ import org.alfresco.rest.RestWorkflowTest;
 import org.alfresco.rest.exception.JsonToModelConversionException;
 import org.alfresco.rest.model.RestItemModel;
 import org.alfresco.rest.model.RestProcessModel;
-import org.alfresco.utility.data.DataUser;
-import org.alfresco.utility.model.FileModel;
-import org.alfresco.utility.model.FileType;
-import org.alfresco.utility.model.SiteModel;
-import org.alfresco.utility.model.TestGroup;
-import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.model.*;
 import org.alfresco.utility.report.Bug;
 import org.alfresco.utility.testrail.ExecutionType;
 import org.alfresco.utility.testrail.annotation.TestRail;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -25,14 +19,10 @@ import org.testng.annotations.Test;
 @Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.PROCESSES, TestGroup.SANITY})
 public class AddProcessItemSanityTests extends RestWorkflowTest
 {
-    @Autowired
-    private DataUser dataUser;
-    private FileModel document, document2, document3;
+    private FileModel document, document2;
     private SiteModel siteModel;
-    private UserModel userWhoStartsTask, assignee;
+    private UserModel userWhoStartsTask, assignee, adminUser;
     private RestProcessModel processModel;
-    private UserModel adminUser;
-
     private RestItemModel processItem;
 
     @BeforeClass(alwaysRun = true)
@@ -43,18 +33,19 @@ public class AddProcessItemSanityTests extends RestWorkflowTest
         assignee = dataUser.createRandomTestUser();
         siteModel = dataSite.usingUser(adminUser).createPublicRandomSite();
         document = dataContent.usingSite(siteModel).createContent(DocumentType.XML);
-        dataWorkflow.usingUser(userWhoStartsTask).usingSite(siteModel).usingResource(document).createNewTaskAndAssignTo(assignee);        
+        dataWorkflow.usingUser(userWhoStartsTask).usingSite(siteModel).usingResource(document).createNewTaskAndAssignTo(assignee);
     }
 
-    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY, 
+    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY,
             description = "Create non-existing process item")
     public void addProcessItem() throws JsonToModelConversionException, Exception
     {
-        restClient.authenticateUser(adminUser);
         document2 = FileModel.getRandomFileModel(FileType.TEXT_PLAIN, "file content");
         dataContent.usingSite(siteModel).createContent(document2);
-        processModel = restClient.getProcesses().getOneRandomEntry();
+        
+        processModel = restClient.authenticateUser(adminUser).getProcesses().getOneRandomEntry().onModel();
         processItem = restClient.usingProcess(processModel).addProcessItem(document2);
+        restClient.assertStatusCodeIs(HttpStatus.CREATED);
         processItem.assertThat().field("createdAt").isNotEmpty()
                    .and().field("size").is(document2.getContent().length())
                    .and().field("createdBy").is(adminUser.getUsername())
@@ -63,35 +54,34 @@ public class AddProcessItemSanityTests extends RestWorkflowTest
                    .and().field("modifiedBy").is(adminUser.getUsername())
                    .and().field("id").isNotEmpty()
                    .and().field("mimeType").is(document2.getFileType().mimeType);
-        
-        restClient.assertStatusCodeIs(HttpStatus.CREATED);
-        restClient.usingProcess(processModel).getProcessesItems().assertThat().entriesListContains("id", processItem.getId()); 
-        
+
+        restClient.usingProcess(processModel).getProcessItems()
+                .assertThat().entriesListContains("id", processItem.getId());
     }
-    
+
     @Bug(id= "MNT-16966")
-    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY, 
+    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY,
             description = "Add process item that already exists")
     public void addProcessItemThatAlreadyExists() throws JsonToModelConversionException, Exception
     {
-        restClient.authenticateUser(adminUser);
-        document3 = dataContent.usingSite(siteModel).createContent(DocumentType.XML);
-        processModel = processesApi.getProcesses().getOneRandomEntry();
-        processItem = processesApi.addProcessItem(processModel, document3);
+        document2 = dataContent.usingSite(siteModel).createContent(DocumentType.XML);
+
+        processModel = restClient.authenticateUser(adminUser).getProcesses().getOneRandomEntry().onModel();
+        processItem = restClient.usingProcess(processModel).addProcessItem(document2);
+        restClient.assertStatusCodeIs(HttpStatus.CREATED);
         processItem.assertThat().field("createdAt").isNotEmpty()
                 .and().field("size").is("19")
                 .and().field("createdBy").is(adminUser.getUsername())
                 .and().field("modifiedAt").isNotEmpty()
-                .and().field("name").is(document3.getName())
+                .and().field("name").is(document2.getName())
                 .and().field("modifiedBy").is(adminUser.getUsername())
                 .and().field("id").isNotEmpty()
-                .and().field("mimeType").is(document3.getFileType().mimeType);
+                .and().field("mimeType").is(document2.getFileType().mimeType);
 
-        processesApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.CREATED);
-        processesApi.getProcessesItems(processModel)
-                .assertThat().entriesListContains("id", processItem.getId()).and()
-                .entriesListContains("name", document3.getName());
-        processItem = processesApi.addProcessItem(processModel, document3);
-        processesApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.BAD_REQUEST);
+        restClient.usingProcess(processModel).getProcessItems()
+                .assertThat().entriesListContains("id", processItem.getId())
+                .and().entriesListContains("name", document2.getName());
+        restClient.usingProcess(processModel).addProcessItem(document2);
+        restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST);
     }
 }
