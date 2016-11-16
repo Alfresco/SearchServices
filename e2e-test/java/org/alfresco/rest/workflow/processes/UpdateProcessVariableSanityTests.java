307e77f38ae9e1a3ee927fa807968100ff9d7477
cagache <claudia.agache@ness.com>
2016-11-16 17:15:47 +0200

    update tests based on new refactored DSL

--- e2e-test/java/org/alfresco/rest/workflow/processes/UpdateProcessVariableSanityTests.java
+++ e2e-test/java/org/alfresco/rest/workflow/processes/UpdateProcessVariableSanityTests.java
@@ -2,19 +2,13 @@ package org.alfresco.rest.workflow.processes;
 
 import org.alfresco.dataprep.CMISUtil;
 import org.alfresco.dataprep.CMISUtil.DocumentType;
-import org.alfresco.rest.RestWorkflowTest;
-import org.alfresco.rest.exception.JsonToModelConversionException;
+import org.alfresco.rest.RestTest;
 import org.alfresco.rest.model.RestProcessModel;
+import org.alfresco.rest.model.RestProcessVariableCollection;
 import org.alfresco.rest.model.RestProcessVariableModel;
-import org.alfresco.rest.requests.Processes;
-import org.alfresco.utility.data.DataUser;
-import org.alfresco.utility.model.FileModel;
-import org.alfresco.utility.model.SiteModel;
-import org.alfresco.utility.model.TestGroup;
-import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.model.*;
 import org.alfresco.utility.testrail.ExecutionType;
 import org.alfresco.utility.testrail.annotation.TestRail;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -23,19 +17,14 @@ import org.testng.annotations.Test;
  * @author iulia.cojocea
  */
 @Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.PROCESSES, TestGroup.SANITY })
-public class UpdateProcessVariableSanityTests extends RestWorkflowTest     
+public class UpdateProcessVariableSanityTests extends RestTest
 {
-    @Autowired
-    private DataUser dataUser;
-
-    @Autowired
-    private Processes processesApi;
-    
     private FileModel document;
     private SiteModel siteModel;
-    private UserModel userWhoStartsTask, assignee;
+    private UserModel userWhoStartsTask, assignee, adminUser;
     private RestProcessModel processModel;
-    private UserModel adminUser;
+    private RestProcessVariableCollection variables;
+    private RestProcessVariableModel variableModel, updatedVariable;
 
     @BeforeClass(alwaysRun = true)
     public void dataPreparation() throws Exception
@@ -46,43 +35,44 @@ public class UpdateProcessVariableSanityTests extends RestWorkflowTest
         siteModel = dataSite.usingUser(userWhoStartsTask).createPublicRandomSite();
         document = dataContent.usingSite(siteModel).createContent(DocumentType.TEXT_PLAIN);
         dataWorkflow.usingUser(userWhoStartsTask).usingSite(siteModel).usingResource(document).createNewTaskAndAssignTo(assignee);
-        processesApi.useRestClient(restClient);
     }
-    
-    @TestRail(section = {"rest-api", TestGroup.PROCESSES }, executionType = ExecutionType.SANITY, 
+
+    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY,
             description = "Create non-existing variable using put call")
-    public void addProcessVariable() throws JsonToModelConversionException, Exception
+    public void addProcessVariable() throws Exception
     {
-        restClient.authenticateUser(adminUser);
-        RestProcessVariableModel variableModel = RestProcessVariableModel.getRandomProcessVariableModel("d:text");
-        processModel = processesApi.addProcess("activitiAdhoc", adminUser, false, CMISUtil.Priority.Normal);
-        processesApi.updateProcessVariable(processModel, variableModel);
-        processesApi.getProcessesVariables(processModel).assertThat().entriesListContains("name", variableModel.getName());
-        processesApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+        variableModel = RestProcessVariableModel.getRandomProcessVariableModel("d:text");
+        processModel = restClient.authenticateUser(adminUser).withWorkflowAPI().addProcess("activitiAdhoc", adminUser, false, CMISUtil.Priority.Normal);
+        restClient.withWorkflowAPI().usingProcess(processModel).updateProcessVariable(variableModel);
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        variables = restClient.withWorkflowAPI().usingProcess(processModel).getProcessVariables();
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        variables.assertThat().entriesListContains("name", variableModel.getName());
     }
-    
-    @TestRail(section = {"rest-api", TestGroup.PROCESSES }, executionType = ExecutionType.SANITY, 
+
+    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY,
             description = "Update existing variable using put call")
-    public void updateProcessVariable() throws JsonToModelConversionException, Exception
+    public void updateProcessVariable() throws Exception
     {
-        restClient.authenticateUser(adminUser);
-        RestProcessVariableModel variableModel = RestProcessVariableModel.getRandomProcessVariableModel("d:text");
-        processModel = processesApi.getProcesses().getOneRandomEntry();
-        processesApi.addProcessVariable(processModel, variableModel);
+        variableModel = RestProcessVariableModel.getRandomProcessVariableModel("d:text");
+        processModel = restClient.authenticateUser(adminUser).withWorkflowAPI().getProcesses().getOneRandomEntry().onModel();
+        restClient.withWorkflowAPI().usingProcess(processModel).addProcessVariable(variableModel);
+        restClient.assertStatusCodeIs(HttpStatus.CREATED);
         variableModel.setValue("newValue");
-        processesApi.updateProcessVariable(processModel, variableModel).assertThat().field("value").is("newValue");
-        processesApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+        updatedVariable = restClient.withWorkflowAPI().usingProcess(processModel).updateProcessVariable(variableModel);
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        updatedVariable.assertThat().field("value").is("newValue");
     }
-    
-    @TestRail(section = {"rest-api", TestGroup.PROCESSES }, executionType = ExecutionType.SANITY, 
+
+    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY,
             description = "Try to add process variable using an invalid processId")
-    public void addProcessVariableUsingInvalidProcessId() throws JsonToModelConversionException, Exception
+    public void addProcessVariableUsingInvalidProcessId() throws Exception
     {
-        restClient.authenticateUser(adminUser);
-        RestProcessVariableModel variableModel = RestProcessVariableModel.getRandomProcessVariableModel("d:text");
-        processModel = processesApi.getProcesses().getOneRandomEntry();
-        processModel.onModel().setId("abc");
-        processesApi.updateProcessVariable(processModel, variableModel);
-        processesApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.NOT_FOUND);
-    }   
+        variableModel = RestProcessVariableModel.getRandomProcessVariableModel("d:text");
+        processModel = restClient.authenticateUser(adminUser).withWorkflowAPI().getProcesses().getOneRandomEntry().onModel();
+        processModel.setId("abc");
+        restClient.withWorkflowAPI().usingProcess(processModel).updateProcessVariable(variableModel);
+        restClient.assertStatusCodeIs(HttpStatus.NOT_FOUND)
+                .assertLastError().containsSummary(String.format(ErrorModel.ENTITY_NOT_FOUND, "abc"));
+    }
 }
