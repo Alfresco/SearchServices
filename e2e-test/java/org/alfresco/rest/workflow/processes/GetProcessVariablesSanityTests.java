bcb6336e7c4b5b8071ffeb418bb680ac047c2bcd
cagache <claudia.agache@ness.com>
2016-11-16 11:11:05 +0200

    update tests based on new refactored DSL

--- e2e-test/java/org/alfresco/rest/workflow/processes/GetProcessVariablesSanityTests.java
+++ e2e-test/java/org/alfresco/rest/workflow/processes/GetProcessVariablesSanityTests.java
@@ -4,15 +4,13 @@ import org.alfresco.dataprep.CMISUtil.DocumentType;
 import org.alfresco.rest.RestWorkflowTest;
 import org.alfresco.rest.exception.JsonToModelConversionException;
 import org.alfresco.rest.model.RestProcessModel;
-import org.alfresco.rest.requests.Processes;
-import org.alfresco.utility.data.DataUser;
+import org.alfresco.rest.model.RestProcessVariableCollection;
 import org.alfresco.utility.model.FileModel;
 import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
 import org.alfresco.utility.model.UserModel;
 import org.alfresco.utility.testrail.ExecutionType;
 import org.alfresco.utility.testrail.annotation.TestRail;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,18 +19,13 @@ import org.testng.annotations.Test;
  * @author iulia.cojocea
  */
 @Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.PROCESSES, TestGroup.SANITY })
-public class GetProcessesVariablesSanityTests extends RestWorkflowTest
+public class GetProcessVariablesSanityTests extends RestWorkflowTest
 {
-    @Autowired
-    private DataUser dataUser;
-
-    @Autowired
-    private Processes processesApi;
-
     private FileModel document;
     private SiteModel siteModel;
     private UserModel userWhoStartsTask, assignee;
     private RestProcessModel processModel;
+    private RestProcessVariableCollection variables;
 
     @BeforeClass(alwaysRun = true)
     public void dataPreparation() throws Exception
@@ -42,26 +35,25 @@ public class GetProcessesVariablesSanityTests extends RestWorkflowTest
         siteModel = dataSite.usingUser(userWhoStartsTask).createPublicRandomSite();
         document = dataContent.usingSite(siteModel).createContent(DocumentType.TEXT_PLAIN);
         dataWorkflow.usingUser(userWhoStartsTask).usingSite(siteModel).usingResource(document).createNewTaskAndAssignTo(assignee);
-        processesApi.useRestClient(restClient);
     }
 
-    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY, 
+    @TestRail(section = {TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY,
             description = "Verify that user that started the process gets all process variables")
     public void getProcessVariablesUsingTheUserWhoStartedProcess() throws JsonToModelConversionException, Exception
     {
-        restClient.authenticateUser(userWhoStartsTask);
-        processModel = processesApi.getProcesses().getOneRandomEntry();
-        processesApi.getProcessesVariables(processModel).assertThat().entriesListIsNotEmpty();
-        processesApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+        processModel = restClient.authenticateUser(userWhoStartsTask).getProcesses().getOneRandomEntry().onModel();
+        variables = restClient.usingProcess(processModel).getProcessVariables();
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        variables.assertThat().entriesListIsNotEmpty();
     }
-    
-    @TestRail(section = { TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY, 
+
+    @TestRail(section = { TestGroup.REST_API, TestGroup.PROCESSES }, executionType = ExecutionType.SANITY,
             description = "Verify get all process variables call using a user that is involved in the process")
     public void getProcessVariablesUsingUserInvolvedInProcess() throws JsonToModelConversionException, Exception
     {
-        restClient.authenticateUser(assignee);
-        processModel = processesApi.getProcesses().getOneRandomEntry();
-        processesApi.getProcessesVariables(processModel).assertThat().entriesListIsNotEmpty();
-        processesApi.usingRestWrapper().assertStatusCodeIs(HttpStatus.OK);
+        processModel = restClient.authenticateUser(assignee).getProcesses().getOneRandomEntry().onModel();
+        variables = restClient.usingProcess(processModel).getProcessVariables();
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        variables.assertThat().entriesListIsNotEmpty();
     }
 }
