35fb448929769be5edf84780b830532d0541a48d
cagache <claudia.agache@ness.com>
2016-11-24 11:11:40 +0200

    fix tests

--- e2e-test/java/org/alfresco/rest/comments/UpdateCommentsCoreTests.java
+++ e2e-test/java/org/alfresco/rest/comments/UpdateCommentsCoreTests.java
@@ -7,12 +7,7 @@ import org.alfresco.rest.model.RestCommentModel;
 import org.alfresco.rest.model.RestCommentModelsCollection;
 import org.alfresco.utility.constants.UserRole;
 import org.alfresco.utility.data.DataUser.ListUserWithRoles;
-import org.alfresco.utility.model.FileModel;
-import org.alfresco.utility.model.FileType;
-import org.alfresco.utility.model.LinkModel;
-import org.alfresco.utility.model.SiteModel;
-import org.alfresco.utility.model.TestGroup;
-import org.alfresco.utility.model.UserModel;
+import org.alfresco.utility.model.*;
 import org.alfresco.utility.report.Bug;
 import org.alfresco.utility.testrail.ExecutionType;
 import org.alfresco.utility.testrail.annotation.TestRail;
@@ -43,7 +38,7 @@ public class UpdateCommentsCoreTests extends RestTest
 
     @TestRail(section = { TestGroup.REST_API, TestGroup.SANITY }, executionType = ExecutionType.SANITY, description = "Verify can not update comment if NodeId is neither document or folder and returns status code 405")
     @Bug(id="MNT-16904")
-    public void canNotUpdateCommentIfNodeIdIsNeitherDoumentOrFolder() throws JsonToModelConversionException, Exception
+    public void canNotUpdateCommentIfNodeIdIsNeitherDocumentOrFolder() throws JsonToModelConversionException, Exception
     {
         FileModel content = FileModel.getRandomFileModel(FileType.TEXT_PLAIN);
         content = dataContent.usingSite(siteModel).usingUser(adminUserModel).createContent(DocumentType.TEXT_PLAIN);
@@ -61,7 +56,7 @@ public class UpdateCommentsCoreTests extends RestTest
     
     @TestRail(section = { TestGroup.REST_API,
             TestGroup.COMMENTS }, executionType = ExecutionType.SANITY, description = "Verify Admin user is not able to update with empty comment body and status code is 400")
-    public void adminIsNotAbleToUpdateWithEptyCommentBody() throws JsonToModelConversionException, Exception
+    public void adminIsNotAbleToUpdateWithEmptyCommentBody() throws JsonToModelConversionException, Exception
     {
         restClient.authenticateUser(adminUserModel);
         commentModel = restClient.withCoreAPI().usingResource(document).addComment("This is a new comment added by admin");
@@ -83,16 +78,15 @@ public class UpdateCommentsCoreTests extends RestTest
         comments.assertThat().entriesListContains("content", "This is the updated comment with Collaborator user");
     }
 
-    @Bug(id="")
     @TestRail(section = { TestGroup.REST_API,
-            TestGroup.COMMENTS }, executionType = ExecutionType.SANITY, description = "Verify Collaborator user can update comments of another user and status code is 200")
-    public void collaboratorIsAbleToUpdateCommentOfAnotherUser() throws JsonToModelConversionException, Exception
+            TestGroup.COMMENTS }, executionType = ExecutionType.SANITY, description = "Verify Collaborator user can not update comments of another user and status code is 200")
+    public void collaboratorIsNotAbleToUpdateCommentOfAnotherUser() throws JsonToModelConversionException, Exception
     {
         restClient.authenticateUser(adminUserModel);
         commentModel = restClient.withCoreAPI().usingResource(document).addComment("This is a new comment added by admin");
         restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteCollaborator));
         restClient.withCoreAPI().usingResource(document).updateComment(commentModel, "This is the updated comment with Collaborator user");        
-        restClient.assertStatusCodeIs(HttpStatus.OK);   
+        restClient.assertStatusCodeIs(HttpStatus.FORBIDDEN).assertLastError().containsSummary(ErrorModel.PERMISSION_WAS_DENIED);
     }
     
     @TestRail(section = { TestGroup.REST_API,
