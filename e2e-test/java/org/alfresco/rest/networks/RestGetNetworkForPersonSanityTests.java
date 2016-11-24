b546a1e548a9b18d4354ad8f2355564921096fb5
cagache <claudia.agache@ness.com>
2016-11-24 15:04:30 +0200

    added network test group

--- e2e-test/java/org/alfresco/rest/networks/RestGetNetworkForPersonSanityTests.java
+++ e2e-test/java/org/alfresco/rest/networks/RestGetNetworkForPersonSanityTests.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+@Test(groups = TestGroup.NETWORKS)
 public class RestGetNetworkForPersonSanityTests extends RestTest
 {
     private UserModel adminUserModel;
