f7966d77ac73e89aba260f7e7b91d971559fbd8d
cagache <claudia.agache@ness.com>
2016-12-09 08:36:49 +0200

    test: added Test annotation on test level

--- e2e-test/java/org/alfresco/rest/workflow/tasks/AddTaskItemCoreTests.java
+++ e2e-test/java/org/alfresco/rest/workflow/tasks/AddTaskItemCoreTests.java
@@ -20,7 +20,6 @@ import org.springframework.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-@Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS, TestGroup.CORE })
 public class AddTaskItemCoreTests extends RestTest
 {
     private UserModel userModel, assigneeUser, adminUser;
@@ -45,6 +44,7 @@ public class AddTaskItemCoreTests extends RestTest
         restClient.authenticateUser(userModel);
     }
 
+    @Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS, TestGroup.CORE })
     @TestRail(section = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS }, executionType = ExecutionType.REGRESSION, description = "Add task item using random user.")
     public void addTaskItemByRandomUser() throws JsonToModelConversionException, Exception
     {
@@ -56,10 +56,10 @@ public class AddTaskItemCoreTests extends RestTest
         taskItem.assertThat().field("createdAt").isNotEmpty().and().field("size").isNotEmpty().and().field("createdBy").is(adminUser.getUsername()).and()
                 .field("modifiedAt").isNotEmpty().and().field("name").is(document.getName()).and().field("modifiedBy").is(userModel.getUsername()).and()
                 .field("id").is(document.getNodeRefWithoutVersion()).and().field("mimeType").is(document.getFileType().mimeType);
-
     }
 
     @Bug(id = "ACE-5683")
+    @Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS, TestGroup.CORE })
     @TestRail(section = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS }, executionType = ExecutionType.REGRESSION, description = "Adding task item, is falling in case invalid itemBody is provided")
     public void failedAddingTaskItemIfInvalidItemBodyIsProvided() throws Exception
     {
@@ -67,10 +67,10 @@ public class AddTaskItemCoreTests extends RestTest
         taskItem = restClient.withWorkflowAPI().usingTask(taskModel).addTaskItem(document);
 
         restClient.assertStatusCodeIs(HttpStatus.NOT_FOUND).assertLastError().containsSummary(String.format(RestErrorModel.ENTITY_NOT_FOUND, "invalidNodeRef"));
-
     }
 
     @Bug(id = "ACE-5675")
+    @Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS, TestGroup.CORE })
     @TestRail(section = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS }, executionType = ExecutionType.REGRESSION, description = "Adding task item is falling in case empty item body is provided")
     public void failedAddingTaskItemIfEmptyItemBodyIsProvided() throws Exception
     {
@@ -79,9 +79,9 @@ public class AddTaskItemCoreTests extends RestTest
 
         // TODO - expected error message to be added
         restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST).assertLastError().containsSummary("");
-        ;
     }
 
+    @Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS, TestGroup.CORE })
     @TestRail(section = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS }, executionType = ExecutionType.REGRESSION, description = "Adding task item is falling in case invalid task id is provided")
     public void failedAddingTaskItemIfInvalidTaskIdIsProvided() throws Exception
     {
@@ -92,6 +92,7 @@ public class AddTaskItemCoreTests extends RestTest
     }
 
     @Bug(id = "ACE-5675")
+    @Test(groups = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS, TestGroup.CORE })
     @TestRail(section = { TestGroup.REST_API, TestGroup.WORKFLOW, TestGroup.TASKS }, executionType = ExecutionType.REGRESSION, description = "Adding task item is falling in case incomplete body type is provided")
     public void failedAddingTaskVariableIfIncompleteBodyIsProvided() throws Exception
     {
@@ -100,7 +101,5 @@ public class AddTaskItemCoreTests extends RestTest
 
         // TODO - expected error message to be added
         restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST).assertLastError().containsSummary("");
-        ;
     }
-
 }
