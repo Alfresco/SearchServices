615caf5a6501d9316c61e416d15cac411a7836ad
cagache <claudia.agache@ness.com>
2017-01-09 17:30:26 +0200

    addSiteMembershipRequest full tests

--- e2e-test/java/org/alfresco/rest/people/AddSiteMembershipRequestCoreTests.java
+++ e2e-test/java/org/alfresco/rest/people/AddSiteMembershipRequestCoreTests.java
@@ -2,6 +2,7 @@ package org.alfresco.rest.people;
 
 import org.alfresco.rest.RestTest;
 import org.alfresco.rest.model.RestErrorModel;
+import org.alfresco.rest.model.RestSiteMembershipRequestModel;
 import org.alfresco.utility.constants.UserRole;
 import org.alfresco.utility.exception.DataPreparationException;
 import org.alfresco.utility.model.SiteModel;
@@ -16,26 +17,25 @@ import org.testng.annotations.Test;
 public class AddSiteMembershipRequestCoreTests extends RestTest
 {
     private UserModel adminUser;    
-    private SiteModel siteModel;
+    private SiteModel moderatedSite;
+    private RestSiteMembershipRequestModel requestModel;
 
     @BeforeClass(alwaysRun = true)
     public void dataPreparation() throws DataPreparationException
     {
         adminUser = dataUser.getAdminUser();
-        siteModel = dataSite.usingUser(adminUser).createModeratedRandomSite();
+        moderatedSite = dataSite.usingUser(adminUser).createModeratedRandomSite();
     }
-
-     
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.CORE })
     @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.REGRESSION, description = "Verify addSiteMembershipRequest Rest API status code is 400 for a user that has already been invited")
     public void addSiteMembershipRequestStatusCodeIs400ReceivedForAUserThatIsAlreadyInvited() throws Exception
     {
         UserModel newMember = dataUser.createRandomTestUser();
-        dataUser.addUserToSite(newMember, siteModel, UserRole.SiteContributor);
-        restClient.authenticateUser(newMember).withCoreAPI().usingMe().addSiteMembershipRequest(siteModel);
+        dataUser.addUserToSite(newMember, moderatedSite, UserRole.SiteContributor);
+        restClient.authenticateUser(newMember).withCoreAPI().usingMe().addSiteMembershipRequest(moderatedSite);
         restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST);
-        restClient.assertLastError().containsSummary(String.format(RestErrorModel.ALREADY_Site_MEMBER, newMember.getUsername(), siteModel.getId()));
+        restClient.assertLastError().containsSummary(String.format(RestErrorModel.ALREADY_Site_MEMBER, newMember.getUsername(), moderatedSite.getId()));
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.CORE })
@@ -43,7 +43,7 @@ public class AddSiteMembershipRequestCoreTests extends RestTest
     public void addSiteMembershipRequestStatusCodeIs404ReceivedForAUserThatDoesNotExist() throws Exception
     {
         UserModel newMember = dataUser.createRandomTestUser();
-        restClient.authenticateUser(newMember).withCoreAPI().usingUser(new UserModel("invalidUser", "password")).addSiteMembershipRequest(siteModel);
+        restClient.authenticateUser(newMember).withCoreAPI().usingUser(new UserModel("invalidUser", "password")).addSiteMembershipRequest(moderatedSite);
         restClient.assertStatusCodeIs(HttpStatus.NOT_FOUND);
         restClient.assertLastError().containsSummary(String.format(RestErrorModel.ENTITY_NOT_FOUND, "invalidUser"));
     }
@@ -64,7 +64,8 @@ public class AddSiteMembershipRequestCoreTests extends RestTest
     {
         UserModel newMember = dataUser.createRandomTestUser();
         restClient.authenticateUser(newMember).withCoreAPI().usingMe().addSiteMembershipRequest("");
-        restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST);
+        restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST)
+                .assertLastError().containsSummary(String.format(RestErrorModel.NO_CONTENT, "No content to map to Object due to end of input"));
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.CORE })
@@ -72,8 +73,9 @@ public class AddSiteMembershipRequestCoreTests extends RestTest
     public void addSiteMembershipRequestStatusCodeIs201ForRequestWithEmptyMessage() throws Exception
     {
         UserModel newMember = dataUser.createRandomTestUser();
-        restClient.authenticateUser(newMember).withCoreAPI().usingMe().addSiteMembershipRequest("", siteModel, "New request");
+        requestModel = restClient.authenticateUser(newMember).withCoreAPI().usingMe().addSiteMembershipRequest("", moderatedSite, "New request");
         restClient.assertStatusCodeIs(HttpStatus.CREATED);
+        requestModel.assertThat().field("id").is(moderatedSite.getId()).assertThat().field("site").isNotEmpty();
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.CORE })
@@ -81,7 +83,8 @@ public class AddSiteMembershipRequestCoreTests extends RestTest
     public void addSiteMembershipRequestStatusCodeIs201ForRequestWithEmptyTitle() throws Exception
     {
         UserModel newMember = dataUser.createRandomTestUser();
-        restClient.authenticateUser(newMember).withCoreAPI().usingMe().addSiteMembershipRequest("Please accept me", siteModel, "");
+        requestModel = restClient.authenticateUser(newMember).withCoreAPI().usingMe().addSiteMembershipRequest("Please accept me", moderatedSite, "");
         restClient.assertStatusCodeIs(HttpStatus.CREATED);
+        requestModel.assertThat().field("id").is(moderatedSite.getId()).assertThat().field("site").isNotEmpty();
     }
 }
--- e2e-test/java/org/alfresco/rest/people/AddSiteMembershipRequestSanityTests.java
+++ e2e-test/java/org/alfresco/rest/people/AddSiteMembershipRequestSanityTests.java
@@ -2,113 +2,95 @@ package org.alfresco.rest.people;
 
 import org.alfresco.rest.RestTest;
 import org.alfresco.rest.exception.JsonToModelConversionException;
+import org.alfresco.rest.model.RestErrorModel;
 import org.alfresco.rest.model.RestSiteMembershipRequestModel;
 import org.alfresco.utility.constants.UserRole;
-import org.alfresco.utility.data.DataSite;
 import org.alfresco.utility.data.DataUser;
 import org.alfresco.utility.data.DataUser.ListUserWithRoles;
 import org.alfresco.utility.exception.DataPreparationException;
 import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
 import org.alfresco.utility.model.UserModel;
-import org.alfresco.utility.report.Bug;
 import org.alfresco.utility.testrail.ExecutionType;
 import org.alfresco.utility.testrail.annotation.TestRail;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class AddSiteMembershipRequestSanityTests extends RestTest
 {
-    @Autowired
-    DataUser dataUser;
-
-    @Autowired
-    DataSite dataSite;
-
-    private SiteModel siteModel;
-
-    private ListUserWithRoles usersWithRoles;
-
-    private UserModel adminUser;    
+    private SiteModel publicSite, anotherPublicSite;
+    private DataUser.ListUserWithRoles usersWithRoles;
+    private UserModel adminUser, siteManager;
     private RestSiteMembershipRequestModel requestModel;
 
     @BeforeClass(alwaysRun = true)
     public void dataPreparation() throws DataPreparationException
     {
         adminUser = dataUser.getAdminUser();
-        siteModel = dataSite.usingUser(adminUser).createPublicRandomSite();
-        usersWithRoles = dataUser.addUsersWithRolesToSite(siteModel, UserRole.SiteManager, UserRole.SiteCollaborator, UserRole.SiteConsumer,
+        publicSite = dataSite.usingUser(adminUser).createPublicRandomSite();
+        usersWithRoles = dataUser.addUsersWithRolesToSite(publicSite, UserRole.SiteManager, UserRole.SiteCollaborator, UserRole.SiteConsumer,
                 UserRole.SiteContributor);
+
+        siteManager = dataUser.createRandomTestUser();
+        anotherPublicSite = dataSite.usingUser(siteManager).createPublicRandomSite();
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.SANITY })
-    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify site manager is able to create new site membership request")
-    @Bug(id = "MNT-16557")
-    public void siteManagerIsAbleToCreateSiteMembershipRequest() throws JsonToModelConversionException, Exception
+    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify site manager is able to create new site membership request for himself and status code is 201")
+    public void managerCreatesNewSiteMembershipRequestForSelf() throws Exception
     {
-        UserModel newMember = dataUser.createRandomTestUser();
-        requestModel = restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteManager)).withCoreAPI().usingUser(newMember)
-                .addSiteMembershipRequest(siteModel);
+        requestModel = restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteManager)).withCoreAPI().usingAuthUser().addSiteMembershipRequest(anotherPublicSite);
         restClient.assertStatusCodeIs(HttpStatus.CREATED);
-        requestModel.assertThat().field("id").isNotEmpty().assertThat().field("site").isNotEmpty();
+        requestModel.assertThat().field("id").is(anotherPublicSite.getId())
+                    .assertThat().field("site").isNotEmpty();
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.SANITY })
-    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify site collaborator is able to create new site membership request")
-    @Bug(id = "MNT-16557")
-    public void siteCollaboatorIsAbleToCreateSiteMembershipRequest() throws JsonToModelConversionException, Exception
+    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify site contributor is able to create new site membership request for himself and status code is 201")
+    public void contributorCreatesNewSiteMembershipRequestForSelf() throws Exception
     {
-        UserModel newMember = dataUser.createRandomTestUser();
-        requestModel = restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteCollaborator)).withCoreAPI().usingUser(newMember)
-                .addSiteMembershipRequest(siteModel);
+        requestModel = restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteContributor)).withCoreAPI().usingAuthUser().addSiteMembershipRequest(anotherPublicSite);
         restClient.assertStatusCodeIs(HttpStatus.CREATED);
-        requestModel.assertThat().field("id").isNotEmpty().assertThat().field("site").isNotEmpty();
+        requestModel.assertThat().field("id").is(anotherPublicSite.getId())
+                    .assertThat().field("site").isNotEmpty();
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.SANITY })
-    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify site contributor is able to create new site membership request")
-    @Bug(id = "MNT-16557")
-    public void siteContributorIsAbleToCreateSiteMembershipRequest() throws JsonToModelConversionException, Exception
+    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify site collaborator is able to create new site membership request for himself and status code is 201")
+    public void collaboratorCreatesNewSiteMembershipRequestForSelf() throws Exception
     {
-        UserModel newMember = dataUser.createRandomTestUser();
-        requestModel = restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteContributor)).withCoreAPI().usingUser(newMember)
-                .addSiteMembershipRequest(siteModel);
+        requestModel = restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteCollaborator)).withCoreAPI().usingAuthUser().addSiteMembershipRequest(anotherPublicSite);
         restClient.assertStatusCodeIs(HttpStatus.CREATED);
-        requestModel.assertThat().field("id").isNotEmpty().assertThat().field("site").isNotEmpty();
+        requestModel.assertThat().field("id").is(anotherPublicSite.getId())
+                    .assertThat().field("site").isNotEmpty();
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.SANITY })
-    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify site consumer is able to create new site membership request")
-    @Bug(id = "MNT-16557")
-    public void siteConsumerIsAbleToCreateSiteMembershipRequest() throws JsonToModelConversionException, Exception
+    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify site consumer is able to create new site membership request for himself and status code is 201")
+    public void consumerCreatesNewSiteMembershipRequestForSelf() throws Exception
     {
-        UserModel newMember = dataUser.createRandomTestUser();
-        requestModel = restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteConsumer)).withCoreAPI().usingUser(newMember)
-                .addSiteMembershipRequest(siteModel);
+        requestModel = restClient.authenticateUser(usersWithRoles.getOneUserWithRole(UserRole.SiteConsumer)).withCoreAPI().usingAuthUser().addSiteMembershipRequest(anotherPublicSite);
         restClient.assertStatusCodeIs(HttpStatus.CREATED);
-        requestModel.assertThat().field("id").isNotEmpty().assertThat().field("site").isNotEmpty();
+        requestModel.assertThat().field("id").is(anotherPublicSite.getId())
+                    .assertThat().field("site").isNotEmpty();
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.SANITY })
-    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify admin user is able to create new site membership request")
-    @Bug(id = "MNT-16557")
-    public void adminUserIsAbleToCreateSiteMembershipRequest() throws JsonToModelConversionException, Exception
+    @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify admin is able to create new site membership request for himself and status code is 201")
+    public void adminCreatesNewSiteMembershipRequestForSelf() throws Exception
     {
-        UserModel newMember = dataUser.createRandomTestUser();
-        requestModel = restClient.authenticateUser(adminUser).withCoreAPI().usingUser(newMember).addSiteMembershipRequest(siteModel);
+        requestModel = restClient.authenticateUser(adminUser).withCoreAPI().usingAuthUser().addSiteMembershipRequest(anotherPublicSite);
         restClient.assertStatusCodeIs(HttpStatus.CREATED);
-        requestModel.assertThat().field("id").isNotEmpty().assertThat().field("site").isNotEmpty();
+        requestModel.assertThat().field("id").is(anotherPublicSite.getId())
+                    .assertThat().field("site").isNotEmpty();
     }
 
     @Test(groups = { TestGroup.REST_API, TestGroup.PEOPLE, TestGroup.SANITY })
     @TestRail(section = { TestGroup.REST_API, TestGroup.PEOPLE }, executionType = ExecutionType.SANITY, description = "Verify unauthenticated user is not able to create new site membership request")
-    @Bug(id = "MNT-16557")
     public void unauthenticatedUserIsNotAbleToCreateSiteMembershipRequest() throws JsonToModelConversionException, Exception
     {
-        UserModel newMember = dataUser.createRandomTestUser();
-        restClient.authenticateUser(new UserModel("random user", "random password")).withCoreAPI().usingUser(newMember).addSiteMembershipRequest(siteModel);
-        restClient.assertStatusCodeIs(HttpStatus.UNAUTHORIZED).assertLastExceptionContains(HttpStatus.UNAUTHORIZED.toString());
+        restClient.authenticateUser(new UserModel("random user", "random password")).withCoreAPI().usingAuthUser().addSiteMembershipRequest(publicSite);
+        restClient.assertStatusCodeIs(HttpStatus.UNAUTHORIZED).assertLastError().containsSummary(RestErrorModel.AUTHENTICATION_FAILED);
     }
 }
