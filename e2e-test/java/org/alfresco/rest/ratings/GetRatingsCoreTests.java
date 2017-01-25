e31f0a30fce5d8b8d9192e9919a940cbaf840d14
Valentin Popa <valentin-iulian.popa@ness.com>
2017-01-25 17:45:15 +0200

    Get ratings full tests and fixes

--- e2e-test/java/org/alfresco/rest/ratings/GetRatingsCoreTests.java
+++ e2e-test/java/org/alfresco/rest/ratings/GetRatingsCoreTests.java
@@ -3,7 +3,7 @@ package org.alfresco.rest.ratings;
 import org.alfresco.dataprep.CMISUtil;
 import org.alfresco.rest.RestTest;
 import org.alfresco.rest.model.RestErrorModel;
-import org.alfresco.rest.model.RestRatingModel;
+import org.alfresco.rest.model.RestRatingModelsCollection;
 import org.alfresco.utility.exception.DataPreparationException;
 import org.alfresco.utility.model.FileModel;
 import org.alfresco.utility.model.SiteModel;
@@ -22,7 +22,7 @@ public class GetRatingsCoreTests extends RestTest
     private SiteModel siteModel;
     private UserModel adminUserModel, userModel;
     private FileModel document;
-    private RestRatingModel returnedRatingModel;
+    private RestRatingModelsCollection ratingsModel;
 
     @BeforeClass(alwaysRun = true)
     public void dataPreparation() throws DataPreparationException
@@ -36,63 +36,70 @@ public class GetRatingsCoreTests extends RestTest
     public void setUp() throws DataPreparationException, Exception
     {
         document = dataContent.usingSite(siteModel).usingAdmin().createContent(CMISUtil.DocumentType.TEXT_PLAIN);
+        restClient.authenticateUser(userModel).withCoreAPI().usingNode(document).rateStarsToDocument(5);
+        restClient.authenticateUser(userModel).withCoreAPI().usingNode(document).likeDocument();
     }
 
-    @TestRail(section = { TestGroup.REST_API,
-            TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, description = "Check that rating for invalid maxItems status code is 400")
+    @TestRail(section = { TestGroup.REST_API, TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, 
+            description = "Check that rating for invalid maxItems status code is 400")
     @Test(groups = { TestGroup.REST_API, TestGroup.RATINGS, TestGroup.CORE })
     public void checkInvalidMaxItemsStatusCode() throws Exception
     {
 
-        restClient.authenticateUser(adminUserModel).withParams("maxItems=0").withCoreAPI().usingResource(document).getLikeRating();
+        restClient.authenticateUser(adminUserModel).withParams("maxItems=0").withCoreAPI().usingResource(document).getRatings();
         restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST).assertLastError().containsSummary("Only positive values supported for maxItems");
     }
 
-    @TestRail(section = { TestGroup.REST_API,
-            TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, description = "Check that rating for invalid skipCount status code is 400")
+    @TestRail(section = { TestGroup.REST_API, TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, 
+            description = "Check that rating for invalid skipCount status code is 400")
     @Test(groups = { TestGroup.REST_API, TestGroup.RATINGS, TestGroup.CORE })
     public void checkInvalidSkipCountStatusCode() throws Exception
     {
 
-        restClient.authenticateUser(adminUserModel).withParams("skipCount=AB").withCoreAPI().usingResource(document).getLikeRating();
+        restClient.authenticateUser(adminUserModel).withParams("skipCount=AB").withCoreAPI().usingResource(document).getRatings();
         restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST).assertLastError().containsSummary("Invalid paging parameter skipCount:AB");
     }
 
-    @TestRail(section = { TestGroup.REST_API,
-            TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, description = "If nodeId does not exist status code is 404 when a document is liked")
+    @TestRail(section = { TestGroup.REST_API, TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, 
+            description = "If nodeId does not exist status code is 404 when a document is liked")
     @Test(groups = { TestGroup.REST_API, TestGroup.RATINGS, TestGroup.CORE })
     public void addLikeUsingInvalidNodeId() throws Exception
     {
-
+        FileModel document = dataContent.usingSite(siteModel).usingAdmin().createContent(CMISUtil.DocumentType.TEXT_PLAIN);
+        restClient.authenticateUser(userModel).withCoreAPI().usingNode(document).rateStarsToDocument(5);
+        restClient.authenticateUser(userModel).withCoreAPI().usingNode(document).likeDocument();
         document.setNodeRef(RandomStringUtils.randomAlphanumeric(20));
-        returnedRatingModel = restClient.authenticateUser(adminUserModel).withCoreAPI().usingResource(document).likeDocument();
+        
+        restClient.authenticateUser(adminUserModel).withCoreAPI().usingResource(document).getRatings();
 
         restClient.assertStatusCodeIs(HttpStatus.NOT_FOUND).assertLastError()
                 .containsSummary(String.format(RestErrorModel.ENTITY_NOT_FOUND, document.getNodeRef()));
     }
 
-    @TestRail(section = { TestGroup.REST_API,
-            TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, description = "Check that rating value is TRUE for a like rating")
+    @TestRail(section = { TestGroup.REST_API, TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, 
+            description = "Check that rating value is TRUE for a like rating")  
     @Test(groups = { TestGroup.REST_API, TestGroup.RATINGS, TestGroup.CORE })
     public void checkRatingValueIsTrueForLikedDoc() throws Exception
     {
 
-        returnedRatingModel = restClient.authenticateUser(adminUserModel).withCoreAPI().usingResource(document).likeDocument();
-        restClient.assertStatusCodeIs(HttpStatus.CREATED);
+        ratingsModel = restClient.authenticateUser(userModel).withCoreAPI().usingResource(document).getRatings();
+        restClient.assertStatusCodeIs(HttpStatus.OK);
 
-        returnedRatingModel.assertThat().field("myRating").is("true").and().field("id").is("likes");
+        ratingsModel.assertThat().entriesListContains("myRating", "true")
+            .assertThat().entriesListContains("id", "likes");
     }
 
-    @TestRail(section = { TestGroup.REST_API,
-            TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, description = "Check that rating value is an INTEGER value for stars rating")
+    @TestRail(section = { TestGroup.REST_API, TestGroup.RATINGS }, executionType = ExecutionType.REGRESSION, 
+            description = "Check that rating value is an INTEGER value for stars rating")
     @Test(groups = { TestGroup.REST_API, TestGroup.RATINGS, TestGroup.CORE })
     public void checkRatingValueIsIntegerForStarsRating() throws Exception
     {
 
-        returnedRatingModel = restClient.authenticateUser(userModel).withCoreAPI().usingResource(document).rateStarsToDocument(5);
-        restClient.assertStatusCodeIs(HttpStatus.CREATED);
+        ratingsModel = restClient.authenticateUser(userModel).withCoreAPI().usingResource(document).getRatings();
+        restClient.assertStatusCodeIs(HttpStatus.OK);
 
-        returnedRatingModel.assertThat().field("myRating").is("5").and().field("id").is("fiveStar").and().field("aggregate").isNotEmpty();
+        ratingsModel.assertThat().entriesListContains("myRating", "5")
+            .assertThat().entriesListContains("id", "fiveStar");
     }
 
 }
