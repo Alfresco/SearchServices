114440e463423701e7bd539cf3efda4598b5e144
Cristina Axinte <cristina.axinte@ness.com>
2017-02-08 11:18:00 +0200

    renamed as class contains full tests too

--- e2e-test/java/org/alfresco/rest/workflow/deployments/DeleteDeploymentCoreFullTests.java
+++ e2e-test/java/org/alfresco/rest/workflow/deployments/DeleteDeploymentCoreFullTests.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class DeleteDeploymentCoreTests extends RestTest
+public class DeleteDeploymentCoreFullTests extends RestTest
 {
     private UserModel adminUser;
     private UserModel userModel;
