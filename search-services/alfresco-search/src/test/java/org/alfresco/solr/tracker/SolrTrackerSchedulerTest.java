17fc20467edbe9b2b30cff0980f23f37377cae34
Michael Suzuki <michael.suzuki@alfresco.com>
2017-06-23 10:23:42 +0100

    Tidy imports

--- search-services/alfresco-search/src/test/java/org/alfresco/solr/tracker/SolrTrackerSchedulerTest.java
+++ search-services/alfresco-search/src/test/java/org/alfresco/solr/tracker/SolrTrackerSchedulerTest.java
@@ -23,11 +23,14 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 import java.util.Properties;
 
 import org.alfresco.solr.AlfrescoCoreAdminHandler;
