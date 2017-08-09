5e19918e9ce3589a30da230ae7e916c90c8572fc
Alexandru-Eusebiu Epure <Alexandru.Epure@ness.com>
2017-08-09 18:55:40 +0300

    REPO-2365 :    Extended Node class with PUT /nodes/{nodeId} (update a node)       Refactor getSharedLinksWithAllowableOperations TAS test using PUT /nodes/{nodeId} for creating permission and added a timer (max 15s) for the solr to index the sharedLink.

--- e2e-test/java/org/alfresco/rest/sharedlinks/GetSharedLinksFullTests.java
+++ e2e-test/java/org/alfresco/rest/sharedlinks/GetSharedLinksFullTests.java
@@ -1,10 +1,12 @@
 package org.alfresco.rest.sharedlinks;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+
 import org.alfresco.dataprep.CMISUtil.DocumentType;
 import org.alfresco.rest.RestTest;
-import org.alfresco.rest.model.RestSharedLinksModel;
+import org.alfresco.rest.model.RestNodeModel;
 import org.alfresco.rest.model.RestSharedLinksModelCollection;
-import org.alfresco.utility.constants.UserRole;
 import org.alfresco.utility.exception.DataPreparationException;
 import org.alfresco.utility.model.FileModel;
 import org.alfresco.utility.model.SiteModel;
@@ -38,17 +40,36 @@ public class GetSharedLinksFullTests extends RestTest
     public void getSharedLinksWithAllowableOperations() throws Exception
     {
         file = dataContent.usingUser(adminUser).usingSite(privateSite).createContent(DocumentType.TEXT_PLAIN);
-
-        RestSharedLinksModel sharedLink = restClient.authenticateUser(adminUser).withCoreAPI().usingSharedLinks().createSharedLink(file);
+        
+        /*
+         * { "permissions": { "isInheritanceEnabled": true, "locallySet": { "authorityId": "userModel.getUsername()",
+         * "name": "SiteConsumer", "accessStatus":"ALLOWED" } } }
+         */
+        JsonObject userPermission = Json.createObjectBuilder().add("permissions",
+                Json.createObjectBuilder().add("isInheritanceEnabled", true).add("locallySet", Json.createObjectBuilder()
+                        .add("authorityId", userModel.getUsername()).add("name", "SiteConsumer").add("accessStatus", "ALLOWED")))
+                .build();
+        String putBody = userPermission.toString();
+        restClient.authenticateUser(adminUser).withCoreAPI().usingNode(file).updateNode(putBody);
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        restClient.authenticateUser(adminUser).withCoreAPI().usingSharedLinks().createSharedLink(file);
         restClient.assertStatusCodeIs(HttpStatus.CREATED);
 
-        dataContent.usingUser(adminUser).usingResource(file).setPermissionsForUser(userModel, UserRole.SiteCollaborator, false);
-        restClient.authenticateUser(userModel).withCoreAPI().usingNode(file).usingParams("include=permissions").getNode();
+        // verify that the permision exists
+        RestNodeModel permissionsRequest = restClient.authenticateUser(userModel).withCoreAPI().usingNode(file)
+                .usingParams("include=permissions").getNode();
+        permissionsRequest.assertThat().field("permissions.locallySet.authorityId").is("[" + userModel.getUsername() + "]");
 
-        restClient.authenticateUser(userModel).withCoreAPI().usingSharedLinks().usingParams("include=allowableOperations").getSharedLinks();
-        restClient.assertStatusCodeIs(HttpStatus.OK);
-
-        restClient.authenticateUser(userModel).withCoreAPI().usingSharedLinks().getSharedLink(sharedLink);
-        restClient.assertStatusCodeIs(HttpStatus.OK);
+        // because the sharedLink take time to be created, we may need to wait for solr to index it.
+        int numberOfTries = 0;
+        RestSharedLinksModelCollection operationsRequest;
+        do
+        {
+            Thread.sleep(1000);
+            numberOfTries++;
+            operationsRequest = restClient.authenticateUser(userModel).withCoreAPI().usingSharedLinks().usingParams("include=allowableOperations").getSharedLinks();
+            restClient.assertStatusCodeIs(HttpStatus.OK);
+        } while (operationsRequest.getPagination().getCount() == 0 && numberOfTries < 15);
+        operationsRequest.getPagination().assertThat().field("count").is(1);
     }
 }
