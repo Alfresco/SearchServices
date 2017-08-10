b3a5a9c855b0ddbf8f816a243f201282aa4300ff
Gethin James <gethin.james@alfresco.com>
2017-08-10 17:36:33 +0200

    SEARCH-506: Changing the shard id router to use the node property

--- alfresco-search/src/main/java/org/alfresco/solr/tracker/ExplicitRouter.java
+++ alfresco-search/src/main/java/org/alfresco/solr/tracker/ExplicitRouter.java
@@ -2,16 +2,17 @@ package org.alfresco.solr.tracker;
 
 import org.alfresco.solr.client.Acl;
 import org.alfresco.solr.client.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Routes a document only if the shardInstance matches the provided shardId
  */
 public class ExplicitRouter implements DocRouter {
 
-    private final int shardId;
+    protected final static Logger log = LoggerFactory.getLogger(ExplicitRouter.class);
 
-    public ExplicitRouter(int shardId) {
-        this.shardId = shardId;
+    public ExplicitRouter() {
     }
 
     @Override
@@ -22,6 +23,37 @@ public class ExplicitRouter implements DocRouter {
 
     @Override
     public boolean routeNode(int shardCount, int shardInstance, Node node) {
-        return shardId == shardInstance;
+
+        if(shardCount <= 1)
+        {
+            return true;
+        }
+
+        String shardBy = node.getShardPropertyValue();
+
+        if (shardBy != null && !shardBy.isEmpty())
+        {
+            try
+            {
+                int shardid = Integer.parseInt(shardBy);
+                return shardid == shardInstance;
+            }
+            catch (NumberFormatException e)
+            {
+                if (log.isDebugEnabled())
+                {
+                    log.debug("EXPLICIT_ID routing specified but failed to parse a shard property value of "+shardBy);
+                }
+            }
+        }
+        else
+        {
+            if (log.isDebugEnabled())
+            {
+                log.debug("EXPLICIT_ID routing specified but no shard id property found ");
+            }
+        }
+
+        return false;
     }
 }
