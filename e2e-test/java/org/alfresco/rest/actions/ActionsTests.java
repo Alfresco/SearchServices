d7efa10b654387facb2d630c0dbcb01de6045cb5
Matt Ward <matt.ward@alfresco.com>
2017-12-01 16:47:02 +0000

    REPO-1308: added 'GET /action-definitions' error status checks
    Added a couple of error code checks: 400 (bad request) and
    401 (unauthorised) - the latter actually fails and raises the question
    of whether the actions API implmentation or the TAS test framework are
    wrong. Committing to capture the test code, but will sort it out
    one way or another.

--- e2e-test/java/org/alfresco/rest/actions/ActionsTests.java
+++ e2e-test/java/org/alfresco/rest/actions/ActionsTests.java
@@ -3,6 +3,7 @@ package org.alfresco.rest.actions;
 import org.alfresco.rest.RestTest;
 import org.alfresco.rest.model.RestActionDefinitionModelsCollection;
 import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.model.UserModel;
 import org.alfresco.utility.testrail.ExecutionType;
 import org.alfresco.utility.testrail.annotation.TestRail;
 import org.springframework.http.HttpStatus;
@@ -20,12 +21,44 @@ public class ActionsTests extends RestTest
     {
         restClient.authenticateUser(dataContent.getAdminUser());
 
-        RestActionDefinitionModelsCollection restActionDefinitions =  restClient.withCoreAPI().usingActions().listActionDefinitions();
+        RestActionDefinitionModelsCollection restActionDefinitions =  restClient.
+                withCoreAPI().
+                usingActions().
+                listActionDefinitions();
+        
         restClient.assertStatusCodeIs(HttpStatus.OK);
         assertFalse(restActionDefinitions.isEmpty());
         restActionDefinitions.assertThat().entriesListContains("name", "copy");
         restActionDefinitions.assertThat().entriesListContains("name", "move");
         restActionDefinitions.assertThat().entriesListContains("name", "check-out");
         restActionDefinitions.assertThat().entriesListContains("name", "check-in");
+    }
+
+    // TODO: TestGroup.ACTIONS not TestGroup.NODES
+    @TestRail(section = { TestGroup.REST_API,TestGroup.NODES }, executionType = ExecutionType.REGRESSION,
+            description = "Verify actions error conditions")
+    @Test(groups = { TestGroup.REST_API, TestGroup.NODES, TestGroup.REGRESSION})
+    public void testActionDefinitionsNegative() throws Exception{
+        // Badly formed request -> 400
+        {
+            restClient.authenticateUser(dataContent.getAdminUser()).
+                    // invalid skipCount
+                    withParams("skipCount=-1").
+                    withCoreAPI().
+                    usingActions().
+                    listActionDefinitions();
+            
+            restClient.assertStatusCodeIs(HttpStatus.BAD_REQUEST);
+
+        }
+
+        // Unauthorized -> 401
+        {
+
+            UserModel userUnauthorized = new UserModel("invalid-user", "invalid-pasword");
+            restClient.authenticateUser(userUnauthorized).withCoreAPI().usingActions().listActionDefinitions();
+            restClient.assertStatusCodeIs(HttpStatus.UNAUTHORIZED);
+
+        }
     }
 }
