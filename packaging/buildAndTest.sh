d06297f495624679d099b61b37adac3ad32f4b58
Tuna Aksoy <tuna.aksoy@alfresco.com>
2018-01-23 23:17:39 +0000

    SEARCH-663 (Reduce the number of build plans on Bamboo)

--- packaging/buildAndTest.sh
+++ packaging/buildAndTest.sh
@@ -10,8 +10,14 @@ then
    # set current working directory to the directory of the script
    cd "$bamboo_working_directory"
 
-   dockerImage="quay.io/alfresco/search-services:$bamboo_maven_version"
-   echo "Building $dockerImage from $nicebranch using version $bamboo_maven_version"
+   tag_version=`echo "$bamboo_maven_version"`
+   if [ "${bamboo_shortJobName}" = "Release" ]
+   then
+      tag_version=`echo "$bamboo_release_version"`
+   fi
+
+   dockerImage="quay.io/alfresco/search-services:$tag_version"
+   echo "Building $dockerImage from $nicebranch using version $tag_version"
 
    docker build -t $dockerImage packaging/target/docker-resources
 
