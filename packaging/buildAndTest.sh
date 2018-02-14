83713d1948336dbb306d535e1f6cba360e8e3097
tunaaksoy <tunaaksoy@users.noreply.github.com>
2018-02-14 23:00:09 +0000

    Feature/search 722 fix docker config files (#14)
    * SEARCH-722 (Fix Docker config files for Search 1.1.x branch)

--- packaging/buildAndTest.sh
+++ packaging/buildAndTest.sh
@@ -3,32 +3,39 @@ set -e
 
 [ "$DEBUG" ] && set -x
 
-# set current working directory to the directory of the script
-cd "$(dirname "$0")"
-
 nicebranch=`echo "$bamboo_planRepository_1_branch" | sed 's/\//_/'`
-dockerImage="docker-internal.alfresco.com/search-services:$bamboo_maven_version"
-echo "Building $dockerImage from $nicebranch using version $bamboo_maven_version"
 
-docker build --build-arg solrBranch=$nicebranch --build-arg solrVer=$bamboo_maven_version -t $dockerImage src/docker
-
-echo "Running tests"
-docker run --rm "$dockerImage" [ -d /opt/alfresco-search-services/solr ] || (echo "solr dir does not exist" && exit 1)
-docker run --rm "$dockerImage" [ -d /opt/alfresco-search-services/data/alfrescoModels ] || (echo "alfrescoModels dir does not exist" && exit 1)
-docker run --rm "$dockerImage" [ -e /opt/alfresco-search-services/solr.in.sh ] || (echo "solr.in.sh does not exist" && exit 1)
-docker run --rm "$dockerImage" grep -q Alfresco /opt/alfresco-search-services/solr.in.sh || (echo "solr.in.sh does not contain Alfresco config" && exit 1)
-docker run --rm "$dockerImage" grep -q Alfresco /opt/alfresco-search-services/solr.in.cmd || (echo "solr.in.cmd does not containAlfresco config" && exit 1)
-docker run --rm "$dockerImage" grep -q LOG4J_PROPS /opt/alfresco-search-services/solr.in.sh || (echo "solr.in.sh does not contain LOG4J_PROPS" && exit 1)
-docker run --rm "$dockerImage" grep -q LOG4J_CONFIG /opt/alfresco-search-services/solr.in.cmd || (echo "solr.in.cmd does not contain LOG4J_CONFIG" && exit 1)
-docker run --rm "$dockerImage" [ -e /opt/alfresco-search-services/solrhome/conf/shared.properties ] || (echo "shared.properties does not exist" && exit 1)
-docker run --rm "$dockerImage" /opt/alfresco-search-services/solr/bin/solr start
-
-if [ "${nicebranch}" == "local" ]
+if [ "${nicebranch}" = "master" ] || [ "${nicebranch#release}" != "${nicebranch}" ]
 then
-    echo "Skipping docker publish for local build"
-else
-    echo "Publishing $dockerImage..."
-    docker push "$dockerImage"
-fi
+   # set current working directory to the directory of the script
+   cd "$bamboo_working_directory"
 
-echo "Docker SUCCESS"
+   tag_version=`echo "$bamboo_maven_version"`
+   if [ "${bamboo_shortJobName}" = "Release" ]
+   then
+      tag_version=`echo "$bamboo_release_version"`
+   fi
+
+   dockerImage="quay.io/alfresco/search-services:$tag_version"
+   echo "Building $dockerImage from $nicebranch using version $tag_version"
+
+   docker build -t $dockerImage packaging/target/docker-resources
+
+   echo "Running tests"
+   docker run --rm "$dockerImage" [ -d /opt/alfresco-search-services/solr ] || (echo "solr dir does not exist" && exit 1)
+   docker run --rm "$dockerImage" [ -d /opt/alfresco-search-services/data/alfrescoModels ] || (echo "alfrescoModels dir does not exist" && exit 1)
+   docker run --rm "$dockerImage" [ -e /opt/alfresco-search-services/solr.in.sh ] || (echo "solr.in.sh does not exist" && exit 1)
+   docker run --rm "$dockerImage" grep -q Alfresco /opt/alfresco-search-services/solr.in.sh || (echo "solr.in.sh does not contain Alfresco config" && exit 1)
+   docker run --rm "$dockerImage" grep -q Alfresco /opt/alfresco-search-services/solr.in.cmd || (echo "solr.in.cmd does not containAlfresco config" && exit 1)
+   docker run --rm "$dockerImage" grep -q LOG4J_PROPS /opt/alfresco-search-services/solr.in.sh || (echo "solr.in.sh does not contain LOG4J_PROPS" && exit 1)
+   docker run --rm "$dockerImage" grep -q LOG4J_CONFIG /opt/alfresco-search-services/solr.in.cmd || (echo "solr.in.cmd does not contain LOG4J_CONFIG" && exit 1)
+   docker run --rm "$dockerImage" [ -e /opt/alfresco-search-services/solrhome/conf/shared.properties ] || (echo "shared.properties does not exist" && exit 1)
+   docker run --rm "$dockerImage" /opt/alfresco-search-services/solr/bin/solr start
+
+   echo "Publishing $dockerImage..."
+   docker push "$dockerImage"
+   
+   echo "Docker SUCCESS"
+else
+    echo "Only building and publishing docker images from master. Skipping for ${nicebranch}"
+fi
