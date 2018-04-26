2cd682d1b4dc9c4879e7c5b0945b2f342a7a73d0
Meenal Bhave <meenal.bhave@alfresco.com>
2018-04-26 09:53:25 +0100

    Search-811: Test Issue Fixed

--- e2e-test/java/org/alfresco/rest/search/AbstractSearchTest.java
+++ e2e-test/java/org/alfresco/rest/search/AbstractSearchTest.java
@@ -89,7 +89,7 @@ public class AbstractSearchTest extends RestTest
         
         file2 = new FileModel("cars.txt", "cars" + title, description, FileType.TEXT_PLAIN, "The landrover discovery is not a sports car ");
         
-        file3 = new FileModel("alfresco.txt", "alfresco " + title, "alfresco " + description, FileType.TEXT_PLAIN, "Alfresco text file for search ");
+        file3 = new FileModel("alfresco.txt", "alfresco", "alfresco", FileType.TEXT_PLAIN, "Alfresco text file for search ");
         
         file4 = new FileModel(unique_searchString, "unique" + title, description, FileType.TEXT_PLAIN, "Unique text file for search ");
         
