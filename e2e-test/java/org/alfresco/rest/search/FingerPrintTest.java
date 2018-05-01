709f237161c6332b46c188ab9e4f94029338c71c
Meenal Bhave <meenal.bhave@alfresco.com>
2018-05-01 16:50:28 +0100

    Simplified dataprep, removed thread.sleep and other unnecessary code

--- e2e-test/java/org/alfresco/rest/search/FingerPrintTest.java
+++ e2e-test/java/org/alfresco/rest/search/FingerPrintTest.java
@@ -18,10 +18,13 @@
  */
 package org.alfresco.rest.search;
 
+import org.alfresco.dataprep.SiteService.Visibility;
+import org.alfresco.utility.data.RandomData;
 import org.alfresco.utility.model.ContentModel;
 import org.alfresco.utility.model.FileModel;
 import org.alfresco.utility.model.FileType;
 import org.alfresco.utility.model.FolderModel;
+import org.alfresco.utility.model.SiteModel;
 import org.alfresco.utility.model.TestGroup;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -35,33 +38,43 @@ import org.testng.annotations.Test;
 public class FingerPrintTest extends AbstractSearchTest
 {
     private FileModel file1,file2,file3,file4;
+    
     @BeforeClass(alwaysRun=true)
     public void indexSimilarFile() throws Exception
     {
         adminUserModel = dataUser.getAdminUser();
         userModel = dataUser.createRandomTestUser();
-        siteModel = dataSite.usingUser(userModel).createPublicRandomSite();
+        
+        siteModel = new SiteModel(RandomData.getRandomName("SiteSearch"));
+        siteModel.setVisibility(Visibility.PRIVATE);
+        
+        siteModel = dataSite.usingUser(userModel).createSite(siteModel);
+        
         /*
          * Create the following file structure for preconditions : 
          *   |- folder
-         *        |-- fox.txt
+         *        |-- pangram-banana.txt
+         *        |-- pangram-taco.txt
+         *        |-- pangram-cat.txt
+         *        |-- dog.txt
          */
-        nodesBuilder = restClient.authenticateUser(userModel).withCoreAPI().usingNode(ContentModel.my()).defineNodes();
+        
         FolderModel folder = new FolderModel(SEARCH_DATA_SAMPLE_FOLDER);
         dataContent.usingUser(userModel).usingSite(siteModel).createFolder(folder);
+        
         file1 = new FileModel("pangram-banana.txt", FileType.TEXT_PLAIN, "The quick brown fox jumps over the lazy banana");
         file2 = new FileModel("pangram-taco.txt", FileType.TEXT_PLAIN, "The quick brown fox jumps over the lazy dog that ate the taco");
         file3 = new FileModel("pangram-cat.txt", FileType.TEXT_PLAIN, "The quick brown fox jumps over the lazy cat");
         file4 = new FileModel("dog.txt", FileType.TEXT_PLAIN, "The quick brown fox ate the lazy dog");
-        ContentModel cm = new ContentModel();
-        cm.setCmisLocation(folder.getCmisLocation());
-        cm.setName(folder.getName());
-        dataContent.usingUser(userModel).usingSite(siteModel).usingResource(cm).createContent(file1);
-        dataContent.usingUser(userModel).usingSite(siteModel).usingResource(cm).createContent(file2);
-        dataContent.usingUser(userModel).usingSite(siteModel).usingResource(cm).createContent(file3);
-        dataContent.usingUser(userModel).usingSite(siteModel).usingResource(cm).createContent(file4);
-        Thread.sleep(35000);//Allow indexing to complete.
+
+        dataContent.usingUser(userModel).usingSite(siteModel).usingResource(folder).createContent(file1);
+        dataContent.usingUser(userModel).usingSite(siteModel).usingResource(folder).createContent(file2);
+        dataContent.usingUser(userModel).usingSite(siteModel).usingResource(folder).createContent(file3);
+        dataContent.usingUser(userModel).usingSite(siteModel).usingResource(folder).createContent(file4);
+        
+        waitForIndexing(file4.getName(), true);
     }
+    
     /**
      * Search similar document based on document finger print.
      * The data prep should have loaded 2 files which one is similar
@@ -99,16 +112,20 @@ public class FingerPrintTest extends AbstractSearchTest
             m.getModel().assertThat().field("name").isNot("cars.txt");
         }
     }
+    
     @Test(groups= {TestGroup.REST_API, TestGroup.SEARCH, TestGroup.ASS_1})
     public void searchSimilar() throws Exception
     {
         String uuid = file2.getNodeRefWithoutVersion();
         Assert.assertNotNull(uuid);
-        // In the response eneity there is a score of each doc, change below threshold to bring more like or less.
+        
+        // In the response entity there is a score of each doc, change below threshold to bring more like or less.
         String fingerprint = String.format("FINGERPRINT:%s_68", uuid); 
         SearchResponse response = query(fingerprint);
+        
         int count = response.getEntries().size();
         Assert.assertTrue(count > 1);
+        
         for(SearchNodeModel m :response.getEntries())
         {
             switch (m.getModel().getName())
@@ -126,6 +143,7 @@ public class FingerPrintTest extends AbstractSearchTest
             m.getModel().assertThat().field("name").isNot("cars.txt");
         }
     }
+    
     @Test(groups= {TestGroup.REST_API, TestGroup.SEARCH, TestGroup.ASS_1})
     public void searchSimilar67Percent() throws Exception
     {
