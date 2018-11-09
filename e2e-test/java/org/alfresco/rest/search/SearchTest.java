3043409ec776538f1f552274a39f529d44494c09
Meenal Bhave <meenal.bhave@alfresco.com>
2018-11-09 17:36:44 +0000

    MNT-19537 Test Added for Content with Special characters

--- e2e-test/java/org/alfresco/rest/search/SearchTest.java
+++ e2e-test/java/org/alfresco/rest/search/SearchTest.java
@@ -20,6 +20,8 @@ package org.alfresco.rest.search;
 
 import junit.framework.Assert;
 import org.alfresco.rest.model.body.RestNodeLockBodyModel;
+import org.alfresco.utility.model.FileModel;
+import org.alfresco.utility.model.FileType;
 import org.alfresco.utility.model.TestGroup;
 import org.alfresco.utility.testrail.ExecutionType;
 import org.alfresco.utility.testrail.annotation.TestRail;
@@ -179,5 +181,25 @@ public class SearchTest extends AbstractSearchTest
         // Usual Fields such as 'name, id' aren't included in the response
         restClient.onResponse().assertThat().body("list.entries.entry[0].name", Matchers.nullValue());
         restClient.onResponse().assertThat().body("list.entries.entry[0].id", Matchers.nullValue());
+    }
+    
+    @Test(groups={TestGroup.SEARCH,TestGroup.REST_API})
+    public void searchSpecialCharacters() throws Exception
+    {        
+        // Create a file with Special Characters
+        String specialCharfileName = "è¥äæ§ç§-åæ.pdf";
+        FileModel file = new FileModel(specialCharfileName, "è¥äæ§ç§-åæ¬¯¸" + "è¥äæ§ç§-åæ¬¯¸", "è¥äæ§ç§-åæ¬¯¸", FileType.TEXT_PLAIN, "Text file with Special Characters: " + specialCharfileName);
+        dataContent.usingUser(userModel).usingSite(siteModel).createContent(file);
+
+        waitForIndexing(file.getName(), true);
+        
+        // Search
+        SearchRequest searchReq = createQuery("name:'" + specialCharfileName + "'");
+        SearchResponse nodes = query(searchReq);
+        
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        nodes.assertThat().entriesListIsNotEmpty();
+
+        restClient.onResponse().assertThat().body("list.entries.entry[0].name", Matchers.equalToIgnoringCase(specialCharfileName));
     }
 }
