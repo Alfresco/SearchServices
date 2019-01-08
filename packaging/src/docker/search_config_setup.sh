ef6b611aa9e4cca65028bf448523ec55b4d1b8f1
Tuna Aksoy <tuna.aksoy@alfresco.com>
2019-01-08 09:21:38 +0000

    SEARCH-1260: Removed uncommented config

--- packaging/src/docker/search_config_setup.sh
+++ packaging/src/docker/search_config_setup.sh
@@ -7,12 +7,6 @@ if [[ ! -z "$MAX_SOLR_RAM_PERCENTAGE" ]]; then
    MEM_CALC=$(expr $(cat /proc/meminfo | grep MemAvailable | awk '{print $2}') \* $MAX_SOLR_RAM_PERCENTAGE / 100)
    SOLR_MEM="-Xms${MEM_CALC}k -Xmx${MEM_CALC}k"
    sed -i -e "s/.*SOLR_JAVA_MEM=.*/SOLR_JAVA_MEM=\"${SOLR_MEM}\"/g" $SOLR_IN_FILE
-   
-#   if [[ ! -z "$MAX_RAM_PERCENTAGE" ]]; then
-#      echo 'export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMPercentage=$MAX_RAM_PERCENTAGE"' >> ~/.profile
-#   else
-#      echo 'export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMPercentage=$MAX_SOLR_RAM_PERCENTAGE"' >> ~/.profile
-#   fi
 fi
 
 if [[ ! -z "$SOLR_HEAP" ]]; then
