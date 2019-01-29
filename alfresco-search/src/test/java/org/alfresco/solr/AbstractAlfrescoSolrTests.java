742cfe098c1c305c2c6d25ca67fa2420def86543
eliaporciani <e.porciani@sease.io>
2019-01-29 19:51:43 +0100

    [SEARCH-1431] removed import *

--- alfresco-search/src/test/java/org/alfresco/solr/AbstractAlfrescoSolrTests.java
+++ alfresco-search/src/test/java/org/alfresco/solr/AbstractAlfrescoSolrTests.java
@@ -18,10 +18,16 @@
  */
 package org.alfresco.solr;
 
-import java.io.*;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.parsers.ParserConfigurationException;
