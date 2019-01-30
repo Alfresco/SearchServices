5dab43d9717550b9ffb20854ca44b840d87b2c7a
eliaporciani <e.porciani@sease.io>
2019-01-30 10:47:55 +0100

    [SEARCH-1431] Some fix after merge conflicts resolution.

--- alfresco-search/src/test/java/org/alfresco/solr/query/afts/requestHandler/AFTSIdentifierFieldsTest.java
+++ alfresco-search/src/test/java/org/alfresco/solr/query/afts/requestHandler/AFTSIdentifierFieldsTest.java
@@ -60,7 +60,7 @@ public class AFTSIdentifierFieldsTest extends AbstractRequestHandlerTest
                     Map<QName, PropertyValue> properties = new HashMap<>();
                     properties.put(PROP_CREATOR, new StringPropertyValue(username));
 
-                    addNode(h.getCore(),
+                    addNode(getCore(),
                         dataModel, 1, dbId, 1,
                         TYPE_CONTENT, null, properties, null,
                         "the_owner_of_this_node_is" + username,
