0faa3437e977c61cc5c2d55959d6fcc3c81fa139
Tom Page <tom.page@alfresco.com>
2019-02-19 16:32:39 +0000

    SEARCH-1422 Avoid NPE during cleanup method.
    Also SEARCH-1423, SEARCH-1424 and SEARCH-1425.

--- e2e-test/src/test/java/org/alfresco/cmis/search/SolrSearchByPathTests.java
+++ e2e-test/src/test/java/org/alfresco/cmis/search/SolrSearchByPathTests.java
@@ -7,12 +7,16 @@ import org.alfresco.utility.data.provider.XMLTestData;
 import org.alfresco.utility.data.provider.XMLTestDataProvider;
 import org.alfresco.utility.model.QueryModel;
 import org.alfresco.utility.model.TestGroup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SolrSearchByPathTests extends CmisTest
 {
+    /** Logger for the class. */
+    private static Logger LOGGER = LoggerFactory.getLogger(SolrSearchByPathTests.class);
     private XMLTestData testData;
 
     @BeforeClass(alwaysRun = true)
@@ -24,7 +28,14 @@ public class SolrSearchByPathTests extends CmisTest
     @AfterClass(alwaysRun = true)
     public void cleanupEnvironment()
     {
-        testData.cleanup(dataContent);
+        if (testData != null)
+        {
+            testData.cleanup(dataContent);
+        }
+        else
+        {
+            LOGGER.warn("testData is inexplicably null - skipping clean up.");
+        }
     }
 
     @Test(dataProviderClass = XMLTestDataProvider.class, dataProvider = "getAllData")
