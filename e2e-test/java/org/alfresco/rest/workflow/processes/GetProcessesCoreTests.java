d5385b52b8213098642ddd9ba03543db8ac25d38
Andrei Forascu <andrei.forascu@ness.com>
2019-03-13 15:34:10 +0200

    REPO-4243 / MNT-20407: Rest-Api Workflow processes and task tests are failing on ACS 6.0.1.1 - update utility version - cherry-pick 910508a22ce5367dfc91a1bcaa3696f1cc69135f from 6.1.N

--- e2e-test/java/org/alfresco/rest/workflow/processes/GetProcessesCoreTests.java
+++ e2e-test/java/org/alfresco/rest/workflow/processes/GetProcessesCoreTests.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.alfresco.dataprep.CMISUtil.DocumentType;
 import org.alfresco.rest.RestTest;
 import org.alfresco.rest.exception.JsonToModelConversionException;
+import org.alfresco.rest.model.RestProcessDefinitionModel;
 import org.alfresco.rest.model.RestProcessModel;
 import org.alfresco.rest.model.RestProcessModelsCollection;
 import org.alfresco.utility.model.FileModel;
@@ -31,6 +32,8 @@ public class GetProcessesCoreTests extends RestTest
     private UserModel adminUser, userWhoStartsTask, assignee ;
     private TaskModel task1, task2;
     private ProcessModel process3;
+    private RestProcessDefinitionModel adhocProcessDefinition;
+    private RestProcessDefinitionModel activitiReviewProcessDefinition;
 
     @BeforeClass(alwaysRun = true)
     public void dataPreparation() throws Exception
@@ -43,6 +46,8 @@ public class GetProcessesCoreTests extends RestTest
         task1 = dataWorkflow.usingUser(userWhoStartsTask).usingSite(siteModel).usingResource(document).createNewTaskAndAssignTo(assignee);  
         task2 = dataWorkflow.usingUser(userWhoStartsTask).usingSite(siteModel).usingResource(document).createNewTaskAndAssignTo(adminUser);
         process3 = dataWorkflow.usingUser(userWhoStartsTask).usingSite(siteModel).usingResource(document).createSingleReviewerTaskAndAssignTo(assignee);
+        adhocProcessDefinition = restClient.authenticateUser(userWhoStartsTask).withWorkflowAPI().getAllProcessDefinitions().getProcessDefinitionByKey("activitiAdhoc");
+        activitiReviewProcessDefinition = restClient.authenticateUser(userWhoStartsTask).withWorkflowAPI().getAllProcessDefinitions().getProcessDefinitionByKey("activitiReview");
     }
 
     @TestRail(section = { TestGroup.REST_API, TestGroup.WORKFLOW,TestGroup.PROCESSES }, executionType = ExecutionType.REGRESSION, 
@@ -68,7 +73,8 @@ public class GetProcessesCoreTests extends RestTest
         RestProcessModelsCollection processes = restClient.authenticateUser(userWhoStartsTask).where("processDefinitionKey='activitiReview'")
                 .withWorkflowAPI().getProcesses();
         restClient.assertStatusCodeIs(HttpStatus.OK);
-        processes.assertThat().entriesListIsNotEmpty().and().entriesListContains("processDefinitionId", "activitiReview:1:8")
-                .and().entriesListDoesNotContain("processDefinitionId", "activitiAdhoc:1:4");
+        
+        processes.assertThat().entriesListIsNotEmpty().and().entriesListContains("processDefinitionId", activitiReviewProcessDefinition.getId())
+                .and().entriesListDoesNotContain("processDefinitionId", adhocProcessDefinition.getId());
     }
 }
