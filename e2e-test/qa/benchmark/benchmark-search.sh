a4d9cab5e211d5ce86eb1e350906135e17057e0e
Lucian Tuce <ltuce@atrisktech.com>
2019-03-14 11:43:35 +0200

    following the comments part 2

--- e2e-test/qa/benchmark/benchmark-search.sh
+++ e2e-test/qa/benchmark/benchmark-search.sh
@@ -1,40 +1,40 @@
 #!/usr/bin/env bash
-set -ex
-echo "Benchmark Search RestAPI"
 
 REQ_HOST="localhost:9080"
 REQ_TESTS_API="http://$REQ_HOST/alfresco-bm-manager/api/v1/tests"
-TEST_NAME="DataTest"
 
-echo -ne "Create Test Data\n"
-#Create Test
-curl -s $REQ_TESTS_API -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"name":"'$TEST_NAME'","description":"Data","release":"alfresco-bm-load-data-3.0.1-SNAPSHOT","schema":"11"}'
+echo -ne "Create Search Test\n"
+#Create Search Test
+TEST_NAME="SearchTest"
+curl -s $REQ_TESTS_API -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"name":"'$TEST_NAME'","description":"Users","release":"alfresco-bm-rest-api-3.0.1-SNAPSHOT","schema":"12"}'
 
-echo -ne "Set MongoDB Data\n"
+echo -ne "Set MongoDB Search\n"
 #Set Mongo DB
 IP=$(hostname -I | awk '{print $1}')
 curl -s $REQ_TESTS_API"/"$TEST_NAME"/props/mongo.test.host" -X PUT -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"value":"'$IP':27017"}'
 
-echo -ne "Set Alfresco related attributes Data\n"
+echo "Set Alfresco related attributes Search"
 #Set Alfresco related attributes
 #ALF_SERVER=${bamboo.alfresco.url}
 #ALF_SERVER="ec2-34-245-148-19.eu-west-1.compute.amazonaws.com"
 ALF_SERVER=$(echo $ALFRESCO_URL | cut -d '/' -f3 | cut -d ':' -f1)
 ALF_PORT=$(echo $ALFRESCO_URL | cut -d ':' -f3)
 curl -s $REQ_TESTS_API"/"$TEST_NAME"/props/alfresco.server" -X PUT -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"value":"'$ALF_SERVER'"}'
-curl -s $REQ_TESTS_API"/"$TEST_NAME"/props/alfresco.url" -X PUT -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"value":"'$ALFRESCO_URL'/"}'
+curl -s $REQ_TESTS_API"/"$TEST_NAME"/props/alfresco.url" -X PUT -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"value":"http://'$ALF_SERVER:$ALF_PORT'/"}'
 curl -s $REQ_TESTS_API"/"$TEST_NAME"/props/alfresco.port" -X PUT -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"value":"'$ALF_PORT'"}'
 
-#Set Site attribute
-SITE_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)-Site-%s-%05d
-curl -s $REQ_TESTS_API"/"$TEST_NAME"/props/load.siteFormat" -X PUT -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"value":"'$SITE_NAME'"}'
+#Set Scenario Weightings
+echo -ne "Set Scenario Weightings\n"
+curl -s $REQ_TESTS_API"/"$TEST_NAME"/props/weight.4.read" -X PUT -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"value":0}'
+curl -s $REQ_TESTS_API"/"$TEST_NAME"/props/weight.5.write" -X PUT -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"value":0}'
 
-echo -ne "Create test instance Data\n"
+
+echo -ne "Create test instance Search\n"
 #Create test instance
-INSTANCE_NAME="RunTestData1"
+INSTANCE_NAME="RunTestSearch1"
 curl -s $REQ_TESTS_API"/"$TEST_NAME"/runs" -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"name":"'$INSTANCE_NAME'"}'
 
-echo -ne "Start the test Data\n"
+echo -ne "Start the test Search\n"
 #Start the test
 curl -s $REQ_TESTS_API"/"$TEST_NAME"/runs/"$INSTANCE_NAME"/schedule" -H "'Host: '"$REQ_HOST"/'" -H 'Content-Type: application/json;charset=utf-8' --data '{"version":0,"scheduled":0}'
 
@@ -55,4 +55,5 @@ do
     #log "Progress: "`echo $status | sed 's|^.*"progress" : \([^ ]*\) .*$|\1|g'`
 done
 
-curl -s -o search_data_results.xlsx -O -J $REQ_TESTS_API"/"$TEST_NAME"/runs/"$INSTANCE_NAME"/results/xlsx"
+#log "Output the CSV results."
+curl -s -o search_results.xlsx -O -J $REQ_TESTS_API"/"$TEST_NAME"/runs/"$INSTANCE_NAME"/results/xlsx"
