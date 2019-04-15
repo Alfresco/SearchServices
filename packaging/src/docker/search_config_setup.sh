22878a79d08e57eee5ef1f0db358c04f2a6dc370
sridharvellingiri <sridharraj.vellingiri@alfresco.com>
2019-04-15 10:10:35 +0100

    SEARCH-1546 Master slave docker compose configuration

--- packaging/src/docker/search_config_setup.sh
+++ packaging/src/docker/search_config_setup.sh
@@ -1,6 +1,65 @@
 #!/bin/bash
 set -e
 
+SOLR_CONF_FOLDER=$PWD/solrhome/templates/rerank/conf
+SOLR_CONFIG_FILE=$SOLR_CONF_FOLDER/solrconfig.xml
+SOLR_CORE_FILE=$SOLR_CONF_FOLDER/solrcore.properties
+
+if [[ $REPLICATION_TYPE == "master" ]]; then
+
+   findStringMaster='<requestHandler name="\/replication" class="solr\.ReplicationHandler" >'
+
+   replaceStringMaster="\n\t<lst name=\"master\"> \n"
+
+   if [[ $REPLICATION_AFTER == "" ]]; then
+      REPLICATION_AFTER=commit
+   fi
+
+   for i in $(echo $REPLICATION_AFTER | sed "s/,/ /g")
+   do
+      replaceStringMaster+="\t\t<str name=\"replicateAfter\">"$i"<\/str> \n"
+   done
+
+   if [[ ! -z "$REPLICATION_CONFIG_FILES" ]]; then
+      replaceStringMaster+="\t\t<str name=\"confFiles\">$REPLICATION_CONFIG_FILES<\/str> \n"
+   fi
+
+   replaceStringMaster+="\t<\/lst>"
+
+   sed -i -e "s/$findStringMaster/$findStringMaster$replaceStringMaster/g" $SOLR_CONFIG_FILE
+   sed -i -e "s/enable.alfresco.tracking=true/enable.alfresco.tracking=true\nenable.master=true\nenable.slave=false/g" $SOLR_CORE_FILE
+fi
+
+if [[ $REPLICATION_TYPE == "slave" ]]; then
+
+   if [[ $REPLICATION_MASTER_PROTOCOL == "" ]]; then
+      REPLICATION_MASTER_PROTOCOL=http
+   fi
+
+   if [[ $REPLICATION_MASTER_HOST == "" ]]; then
+      REPLICATION_MASTER_HOST=localhost
+   fi
+
+   if [[ $REPLICATION_MASTER_PORT == "" ]]; then
+      REPLICATION_MASTER_PORT=8083
+   fi
+
+   if [[ $REPLICATION_CORE_NAME == "" ]]; then
+      REPLICATION_CORE_NAME=alfresco
+   fi
+
+   if [[ $REPLICATION_POLL_INTERVAL == "" ]]; then
+      REPLICATION_POLL_INTERVAL=00:00:60
+   fi
+
+   sed -i -e 's/<requestHandler name="\/replication" class="solr\.ReplicationHandler" >/<requestHandler name="\/replication" class="solr\.ReplicationHandler" >\
+      <lst name="slave">\
+         <str name="masterUrl">'$REPLICATION_MASTER_PROTOCOL':\/\/'$REPLICATION_MASTER_HOST':'$REPLICATION_MASTER_PORT'\/solr\/'$REPLICATION_CORE_NAME'<\/str>\
+         <str name="pollInterval">'$REPLICATION_POLL_INTERVAL'<\/str>\
+      <\/lst>/g' $SOLR_CONFIG_FILE
+   sed -i -e "s/enable.alfresco.tracking=true/enable.alfresco.tracking=true\nenable.master=false\nenable.slave=true/g" $SOLR_CORE_FILE
+fi
+
 SOLR_IN_FILE=$PWD/solr.in.sh
 
 if [[ ! -z "$MAX_SOLR_RAM_PERCENTAGE" ]]; then
