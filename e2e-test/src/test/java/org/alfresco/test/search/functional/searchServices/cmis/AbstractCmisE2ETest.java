e8266feaa23b16366c23109d6d5ae19ef0a5dd4c
Tom Page <tom.page@alfresco.com>
2019-05-03 10:12:08 +0100

    Revert "Merge branch 'feature/SEARCH-1617_CMISScoring' into 'master'"
    This reverts commit 418b3ccbc351e9d60f7ee21fea1bdeb2f66bea3f, reversing
    changes made to a45afe1cd7810d208fe7a2a98c3a7d9d39bc6335.

--- e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/cmis/AbstractCmisE2ETest.java
+++ e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/cmis/AbstractCmisE2ETest.java
@@ -1,13 +1,13 @@
 package org.alfresco.test.search.functional.searchServices.cmis;
 
-import java.lang.reflect.Method;
-
 import org.alfresco.cmis.CmisProperties;
 import org.alfresco.cmis.CmisWrapper;
+
+import java.lang.reflect.Method;
+
 import org.alfresco.test.search.functional.searchServices.search.AbstractSearchServicesE2ETest;
-import org.alfresco.utility.Utility;
+import org.alfresco.utility.LogFactory;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -25,7 +25,7 @@ import org.testng.annotations.BeforeMethod;
 @Scope(value = "prototype")
 public abstract class AbstractCmisE2ETest extends AbstractSearchServicesE2ETest
 {
-    private static Logger LOGGER = LoggerFactory.getLogger(AbstractCmisE2ETest.class);
+    private static Logger LOG = LogFactory.getLogger();
 
     @Autowired
     protected CmisWrapper cmisApi;
@@ -38,50 +38,17 @@ public abstract class AbstractCmisE2ETest extends AbstractSearchServicesE2ETest
     @BeforeMethod(alwaysRun = true)
     public void showStartTestInfo(Method method)
     {
-        LOGGER.info(String.format("*** STARTING Test: [%s] ***", method.getName()));
+        LOG.info(String.format("*** STARTING Test: [%s] ***", method.getName()));
     }
 
     @AfterMethod(alwaysRun = true)
     public void showEndTestInfo(Method method)
     {
-        LOGGER.info(String.format("*** ENDING Test: [%s] ***", method.getName()));
+        LOG.info(String.format("*** ENDING Test: [%s] ***", method.getName()));
     }
     
     public Integer getSolrWaitTimeInSeconds()
     {
         return cmisProperties.envProperty().getSolrWaitTimeInSeconds();
     }
-    
-    /**
-     * Repeat SOLR Query till results count returns expectedCountResults
-     * @param query CMIS Query to be executed
-     * @param expectedCountResults Number of results expected
-     * @return true when results count is equals to expectedCountResults 
-     * @throws Exception
-     */
-    protected boolean waitForIndexing(String query, long expectedCountResults) throws Exception
-    {
-
-        for (int searchCount = 1; searchCount <= 3; searchCount++)
-        {
-            
-            try
-            {
-                cmisApi.withQuery(query).assertResultsCount().equals(expectedCountResults);
-                return true;
-            } 
-            catch (AssertionError ae)
-            {
-                LOGGER.debug(ae.toString());
-            }
-            
-            
-            Utility.waitToLoopTime(properties.getSolrWaitTimeInSeconds(), "Wait For Indexing");
-            
-        }
-
-        return false;
-    }
-
-    
 }
