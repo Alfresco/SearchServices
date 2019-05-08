f5a203e48ce9dfc73adf75035b952a57564e6c4d
eliaporciani <e.porciani@sease.io>
2019-05-08 17:42:22 +0200

    [MNT-20507] Added test for check if indexing work fine with \u007F char in filename.

---
+++ e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/search/SpecialCharacterSearchTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.test.search.functional.searchServices.search;
+
+import org.alfresco.test.search.functional.AbstractE2EFunctionalTest;
+import org.alfresco.utility.model.FileModel;
+import org.alfresco.utility.model.FileType;
+import org.alfresco.utility.model.TestGroup;
+import org.alfresco.utility.report.Bug;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ *
+ * @author Elia Porciani
+ */
+public class SpecialCharacterSearchTest extends AbstractE2EFunctionalTest
+{
+
+    /**
+     *
+     * Index a file with \u007F (delete char) in name.
+     * The goal is to check that the file is actually indexed in solr.
+     * @throws Exception
+     */
+    @Test(groups = { TestGroup.REST_API, TestGroup.SEARCH })
+    @Bug(id = "MNT-20507")
+    public void indexDeleteChar() throws Exception
+    {
+        FileModel file = new FileModel("Delete char\u007Ffile", FileType.TEXT_PLAIN, "content of \u007F file");
+        dataContent.usingUser(testUser).usingSite(testSite).createContent(file);
+        assertTrue(waitForIndexing("name:'" + file.getName()+"'", true));
+    }
+
+}
+
