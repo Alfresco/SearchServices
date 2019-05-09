2e8696f4971cea4203fe65edb4257c12c4490250
agazzarini <a.gazzarini@sease.io>
2019-05-09 18:39:24 +0200

    [MNT-20179] Test coverage improved

--- search-services/alfresco-search/src/test/java/org/alfresco/solr/tracker/DistributedAlfrescoSolrTrackerStateTest.java
+++ search-services/alfresco-search/src/test/java/org/alfresco/solr/tracker/DistributedAlfrescoSolrTrackerStateTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2014 Alfresco Software Limited.
+ * Copyright (C) 2005-2019 Alfresco Software Limited.
  *
  * This file is part of Alfresco
  *
@@ -21,6 +21,7 @@ package org.alfresco.solr.tracker;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static java.util.Optional.of;
+import static java.util.stream.IntStream.range;
 import static org.alfresco.solr.AlfrescoSolrUtils.MAX_WAIT_TIME;
 import static org.alfresco.solr.AlfrescoSolrUtils.assertShardAndCoreSummaryConsistency;
 import static org.alfresco.solr.AlfrescoSolrUtils.coreAdminHandler;
@@ -30,6 +31,7 @@ import static org.alfresco.solr.AlfrescoSolrUtils.getAclReaders;
 import static org.alfresco.solr.AlfrescoSolrUtils.getTransaction;
 import static org.alfresco.solr.AlfrescoSolrUtils.indexAclChangeSet;
 
+import org.alfresco.repo.index.shard.ShardState;
 import org.alfresco.repo.search.adaptor.lucene.QueryConstants;
 import org.alfresco.solr.AbstractAlfrescoDistributedTest;
 import org.alfresco.solr.AlfrescoCoreAdminHandler;
@@ -40,6 +42,7 @@ import org.alfresco.solr.client.Node;
 import org.alfresco.solr.client.NodeMetaData;
 import org.alfresco.solr.client.Transaction;
 import org.alfresco.solr.dataload.TestDataProvider;
+import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
@@ -52,6 +55,7 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * A partial state of {@link org.alfresco.solr.TrackerState} is exposed through two interfaces: AdminHandler.SUMMARY and
@@ -74,36 +78,8 @@ public class DistributedAlfrescoSolrTrackerStateTest extends AbstractAlfrescoDis
     @Before
     private void initData() throws Throwable
     {
-        int howManyNodes = 15;
-
-        AclChangeSet aclChangeSet = getAclChangeSet(1);
-
-        Acl acl = getAcl(aclChangeSet);
-        Acl acl2 = getAcl(aclChangeSet);
-
-        AclReaders aclReaders = getAclReaders(aclChangeSet, acl, singletonList("joel"), singletonList("phil"), null);
-        AclReaders aclReaders2 = getAclReaders(aclChangeSet, acl2, singletonList("jim"), singletonList("phil"), null);
-
-        indexAclChangeSet(aclChangeSet, asList(acl, acl2), asList(aclReaders, aclReaders2));
-
-        BooleanQuery.Builder builder = new BooleanQuery.Builder();
-        builder.add(new BooleanClause(new TermQuery(new Term(QueryConstants.FIELD_SOLR4_ID, "TRACKER!STATE!ACLTX")), BooleanClause.Occur.MUST));
-
-        BooleanQuery waitForQuery = builder.build();
-        waitForDocCountAllCores(waitForQuery, 1, MAX_WAIT_TIME);
-
-        Transaction txn = getTransaction(0, howManyNodes);
-        Map.Entry<List<Node>, List<NodeMetaData>> data = TestDataProvider.nSampleNodesWithSampleContent(acl, txn, howManyNodes);
-
-        indexTransaction(txn, data.getKey(), data.getValue());
-        builder = new BooleanQuery.Builder();
-        builder.add(new BooleanClause(new TermQuery(new Term(QueryConstants.FIELD_SOLR4_ID, "TRACKER!STATE!TX")), BooleanClause.Occur.MUST));
-        builder.add(new BooleanClause(LegacyNumericRangeQuery.newLongRange(QueryConstants.FIELD_S_TXID, txn.getId(), txn.getId() + 1, true, false), BooleanClause.Occur.MUST));
-        waitForQuery = builder.build();
-
-        waitForDocCountAllCores(waitForQuery, 1, MAX_WAIT_TIME);
-        waitForDocCountAllCores(new TermQuery(new Term(QueryConstants.FIELD_READER, "jim")), 1, MAX_WAIT_TIME);
-        waitForDocCount(new TermQuery(new Term("content@s___t@{http://www.alfresco.org/model/content/1.0}content", "world")), howManyNodes, MAX_WAIT_TIME);
+        Acl acl = createAndIndexSomeAclData();
+        createAndIndexTransactionWithSomeNodes(5, acl, "first");
     }
     
     @Test
@@ -118,7 +94,91 @@ public class DistributedAlfrescoSolrTrackerStateTest extends AbstractAlfrescoDis
                             .map(registry -> registry.getTrackerForCore(core.getName(), MetadataTracker.class))
                             .orElseThrow(() -> new IllegalStateException("Cannot retrieve the Metadata tracker on this test core."));
 
+            // 1. First consistency check: ShardState must have the same values of CoreAdmin.SUMMARY report
+            ShardState shardStateAfterFirstIndexingRound = tracker.getShardState();
+            assertShardAndCoreSummaryConsistency(shardStateAfterFirstIndexingRound, core);
+
+            // 2. Index additional ACLs
+            Acl acl = createAndIndexSomeAclData();
+
+            ShardState shardStateAfterIndexingSomeAdditionalAcl= tracker.getShardState();
+
+            // 3. We indexed only ACLs, so ACL data must be different while Transaction data must be the same
+            assertEquals(shardStateAfterFirstIndexingRound.getLastIndexedTxId(), shardStateAfterIndexingSomeAdditionalAcl.getLastIndexedTxId());
+            assertEquals(shardStateAfterFirstIndexingRound.getLastIndexedTxCommitTime(), shardStateAfterIndexingSomeAdditionalAcl.getLastIndexedTxCommitTime());
+            assertNotEquals(shardStateAfterFirstIndexingRound.getLastIndexedChangeSetId(), shardStateAfterIndexingSomeAdditionalAcl.getLastIndexedChangeSetId());
+            assertNotEquals(shardStateAfterFirstIndexingRound.getLastIndexedChangeSetCommitTime(), shardStateAfterIndexingSomeAdditionalAcl.getLastIndexedChangeSetCommitTime());
+
+            // Second consistency check: ShardState must have the same values of CoreAdmin.SUMMARY report
+            assertShardAndCoreSummaryConsistency(shardStateAfterIndexingSomeAdditionalAcl, core);
+
+            // 4. Index a transaction with 10 other nodes
+            createAndIndexTransactionWithSomeNodes(10, acl, "second");
+
+            ShardState shardStateAfterIndexingAnAdditionalTransaction = tracker.getShardState();
+            assertNotEquals(shardStateAfterIndexingSomeAdditionalAcl.getLastIndexedTxId(), shardStateAfterIndexingAnAdditionalTransaction.getLastIndexedTxId());
+            assertNotEquals(shardStateAfterIndexingSomeAdditionalAcl.getLastIndexedTxCommitTime(), shardStateAfterIndexingAnAdditionalTransaction.getLastIndexedTxCommitTime());
+            assertEquals(shardStateAfterIndexingSomeAdditionalAcl.getLastIndexedChangeSetId(), shardStateAfterIndexingAnAdditionalTransaction.getLastIndexedChangeSetId());
+            assertEquals(shardStateAfterIndexingSomeAdditionalAcl.getLastIndexedChangeSetCommitTime(), shardStateAfterIndexingAnAdditionalTransaction.getLastIndexedChangeSetCommitTime());
+
+            // 5. Third consistency check: ShardState must have the same values of CoreAdmin.SUMMARY report
             assertShardAndCoreSummaryConsistency(tracker.getShardState(), core);
         });
+    }
+
+    private Acl createAndIndexSomeAclData()
+    {
+        try {
+            AclChangeSet aclChangeSet = getAclChangeSet(1);
+
+            Acl acl = getAcl(aclChangeSet);
+            Acl acl2 = getAcl(aclChangeSet);
+
+            AclReaders aclReaders = getAclReaders(aclChangeSet, acl, singletonList("joel"), singletonList("phil"), null);
+            AclReaders aclReaders2 = getAclReaders(aclChangeSet, acl2, singletonList("jim"), singletonList("phil"), null);
+
+            indexAclChangeSet(aclChangeSet, asList(acl, acl2), asList(aclReaders, aclReaders2));
+
+            BooleanQuery.Builder builder = new BooleanQuery.Builder();
+            builder.add(new BooleanClause(new TermQuery(new Term(QueryConstants.FIELD_SOLR4_ID, "TRACKER!STATE!ACLTX")), BooleanClause.Occur.MUST));
+            builder.add(new BooleanClause(LegacyNumericRangeQuery.newLongRange(QueryConstants.FIELD_S_ACLTXID, aclChangeSet.getId(), aclChangeSet.getId() + 1, true, false), BooleanClause.Occur.MUST));
+            BooleanQuery waitForQuery = builder.build();
+            waitForDocCountAllCores(waitForQuery, 1, MAX_WAIT_TIME);
+
+            return acl;
+        }
+        catch (Exception exception)
+        {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    /**
+     * Creates and indexes a transaction with a certain number of nodes.
+     *
+     * @param howManyTestNodes how many nodes we want to index.
+     * @param acl the related ACL.
+     * @param sampleTextContent a sample text content that will be used to assert nodes have been actually indexed.
+     */
+    private void createAndIndexTransactionWithSomeNodes(int howManyTestNodes, Acl acl, String sampleTextContent)
+    {
+        try
+        {
+            Transaction txn = getTransaction(0, howManyTestNodes);
+            Map.Entry<List<Node>, List<NodeMetaData>> data = TestDataProvider.nSampleNodesWithSampleContent(acl, txn, howManyTestNodes);
+
+            indexTransaction(txn, data.getKey(), data.getValue(), range(0, howManyTestNodes).mapToObj(index -> sampleTextContent).collect(Collectors.toList()));
+
+            BooleanQuery.Builder builder = new BooleanQuery.Builder();
+            builder.add(new BooleanClause(new TermQuery(new Term(QueryConstants.FIELD_SOLR4_ID, "TRACKER!STATE!TX")), BooleanClause.Occur.MUST));
+            builder.add(new BooleanClause(LegacyNumericRangeQuery.newLongRange(QueryConstants.FIELD_S_TXID, txn.getId(), txn.getId() + 1, true, false), BooleanClause.Occur.MUST));
+            BooleanQuery waitForQuery = builder.build();
+
+            waitForDocCountAllCores(waitForQuery, 1, MAX_WAIT_TIME);
+        }
+        catch (Exception exception)
+        {
+            throw new RuntimeException(exception);
+        }
     }
 }
