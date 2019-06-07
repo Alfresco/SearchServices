007e5a2669e5571fe8fd2b97b4f2c336e312e2c8
Angel Borroy <angel.borroy@alfresco.com>
2019-06-07 13:51:15 +0200

    This change should not apply to containers and it should remain secure comms as none.

--- search-services/packaging/src/docker/search_config_setup.sh
+++ search-services/packaging/src/docker/search_config_setup.sh
@@ -18,4 +18,18 @@ if [[ ! -z "$SOLR_JAVA_MEM" ]]; then
    sed -i -e "s/.*SOLR_JAVA_MEM=.*/SOLR_JAVA_MEM=\"$SOLR_JAVA_MEM\"/g" $SOLR_IN_FILE
 fi
 
+# By default Docker Image is using plain HTTP for communications with Repository
+# TLS Mutual Auth can be enabled by setting ALFRESCO_SECURE_COMMS to any value different from 'none' ('https' is recommended)
+if [[ -z "$ALFRESCO_SECURE_COMMS" || "none" == "$ALFRESCO_SECURE_COMMS" ]]; then
+   sed -i 's/alfresco.secureComms=https/alfresco.secureComms=none/' ${PWD}/solrhome/templates/rerank/conf/solrcore.properties
+   sed -i 's/alfresco.secureComms=https/alfresco.secureComms=none/' ${PWD}/solrhome/templates/noRerank/conf/solrcore.properties
+   # Apply also the setting to existing SOLR cores property files when existing
+   if [[ -f ${PWD}/solrhome/alfresco/conf/solrcore.properties ]]; then
+       sed -i 's/alfresco.secureComms=https/alfresco.secureComms=none/' ${PWD}/solrhome/alfresco/conf/solrcore.properties
+   fi
+   if [[ -f ${PWD}/solrhome/archive/conf/solrcore.properties ]]; then
+       sed -i 's/alfresco.secureComms=https/alfresco.secureComms=none/' ${PWD}/solrhome/archive/conf/solrcore.properties
+   fi
+fi
+
 bash -c "$@"
