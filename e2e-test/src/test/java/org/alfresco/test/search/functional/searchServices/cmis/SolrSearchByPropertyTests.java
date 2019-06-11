6561ca81fe2881c4da58c9d63a647d4c61bebd6e
unknown <keerat.lalia@alfresco.com>
2019-06-11 08:46:40 +0100

    Fixed property tests to reproduce those 5 failures

--- e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/cmis/SolrSearchByPropertyTests.java
+++ e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/cmis/SolrSearchByPropertyTests.java
@@ -2,60 +2,118 @@ package org.alfresco.test.search.functional.searchServices.cmis;
 
 import org.alfresco.utility.Utility;
 import org.alfresco.utility.data.provider.XMLDataConfig;
-import org.alfresco.utility.data.provider.XMLTestData;
+import org.alfresco.utility.data.CustomObjectTypeProperties;
 import org.alfresco.utility.data.provider.XMLTestDataProvider;
-import org.alfresco.utility.exception.TestConfigurationException;
 import org.alfresco.utility.model.QueryModel;
+import org.alfresco.utility.model.FileModel;
+import org.alfresco.utility.model.FolderModel;
 import org.alfresco.search.TestGroup;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SolrSearchByPropertyTests extends AbstractCmisE2ETest
 {
-    /** Logger for the class. */
-    private static Logger LOGGER = LoggerFactory.getLogger(SolrSearchByPropertyTests.class);
-    private XMLTestData testData;
+    private FolderModel guestf, tesf, restf, testtttf, testf, testf1, testf2, testf3, testf4;
+    private FileModel guestc, restc, tesc, testtttc, testc, testc1, testc2, testc3;
+    private String siteDoclibNodeRef;
 
     @BeforeClass(alwaysRun = true)
-    public void deployCustomModel() throws TestConfigurationException
+    @Override
+    public void searchServicesDataPreparation() throws Exception
     {
-        deployCustomModel("model/tas-model.xml");
-        cmisApi.authenticateUser(dataUser.getAdminUser());
-    }
-    
-    @AfterClass(alwaysRun = true)
-    public void cleanupEnvironment()
-    {
-        if (testData != null)
-        {
-            testData.cleanup(dataContent);
-        }
-        else
-        {
-            LOGGER.warn("testData is inexplicably null - skipping clean up.");
-        }
-    }
+        dataContent.usingAdmin().deployContentModel("model/tas-model.xml");
 
-    @Test(dataProviderClass = XMLTestDataProvider.class, dataProvider = "getAllData")
-    @XMLDataConfig(file = "src/test/resources/testdata/search-by-property.xml")
-    public void prepareDataForSearchWithProperty(XMLTestData testData) throws Exception
-    {
-        this.testData = testData;
-        this.testData.createUsers(dataUser);
-        this.testData.createSitesStructure(dataSite, dataContent, dataUser);
+        // Folders
+        guestf = new FolderModel("guestf");
+        tesf = new FolderModel("tesf");
+
+        // SubFolders
+        restf = new FolderModel("restf");
+        testtttf = new FolderModel("testtttf");
+        testf = new FolderModel("testf");
+        testf1 = new FolderModel("testf1");
+        testf2 = new FolderModel("testf2");
+        testf3 = new FolderModel("testf3");
+        testf4 = new FolderModel("testf4");
+
+        // Files
+        guestc = new FileModel("guestc.txt");
+        restc = new FileModel("restc.txt");
+        tesc = new FileModel("tesc.txt");
+        testtttc = new FileModel("testtttc.txt");
+        testc = new FileModel("testc.txt");
+        testc1 = new FileModel("testc1.txt");
+        testc2 = new FileModel("testc2.txt");
+        testc3 = new FileModel("testc3.txt");
+        
+        cmisApi.authenticateUser(testUser);
+        siteDoclibNodeRef = cmisApi.withCMISUtil().getObjectId(String.format("/Sites/%s/documentLibrary",testSite.getId()));
+        
+        //Sites
+        dataContent.usingUser(testUser).usingSite(testSite).createCustomContent(guestf, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "guestf text")
+                        .addProperty("tas:IntPropertyF", 222));       
+        dataContent.usingUser(testUser).usingSite(testSite).createCustomContent(tesf, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "tesf text")
+                        .addProperty("tas:IntPropertyF", 224));
+        
+        // Sites >> Folders
+        dataContent.usingUser(testUser).usingResource(guestf).createCustomContent(restf, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "restf text")
+                        .addProperty("tas:IntPropertyF", 223));
+        dataContent.usingUser(testUser).usingResource(guestf).createCustomContent(testtttf, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "testtttf text")
+                        .addProperty("tas:IntPropertyF", 225));
+        dataContent.usingUser(testUser).usingResource(guestf).createCustomContent(testf, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "testf text")
+                        .addProperty("tas:IntPropertyF", 226));
+        dataContent.usingUser(testUser).usingResource(guestf).createCustomContent(testf1, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "testf1 text")
+                        .addProperty("tas:IntPropertyF", 2221));
+        dataContent.usingUser(testUser).usingResource(guestf).createCustomContent(testf2, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "testf2 text")
+                        .addProperty("tas:IntPropertyF", 2222));
+        dataContent.usingUser(testUser).usingResource(guestf).createCustomContent(testf3, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "testf3 text")
+                        .addProperty("tas:IntPropertyF", 2223));
+        dataContent.usingUser(testUser).usingResource(guestf).createCustomContent(testf4, "F:tas:folder",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyF", "testf4 text")
+                        .addProperty("tas:IntPropertyF", 2224));
+
+        // Sites >> Files
+        dataContent.usingUser(testUser).usingResource(tesf).createCustomContent(guestc, "D:tas:document",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyC", "guestc text")
+                        .addProperty("tas:IntPropertyC", 222));
+        dataContent.usingUser(testUser).usingResource(tesf).createCustomContent(restc, "D:tas:document",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyC", "restc text")
+                        .addProperty("tas:IntPropertyC", 223));
+        dataContent.usingUser(testUser).usingResource(tesf).createCustomContent(tesc, "D:tas:document",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyC", "tesc text")
+                        .addProperty("tas:IntPropertyC", 224));
+        dataContent.usingUser(testUser).usingResource(tesf).createCustomContent(testtttc, "D:tas:document",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyC", "testtttc text")
+                        .addProperty("tas:IntPropertyC", 225));
+        dataContent.usingUser(testUser).usingResource(tesf).createCustomContent(testc, "D:tas:document",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyC", "testc text")
+                        .addProperty("tas:IntPropertyC", 226));
+        dataContent.usingUser(testUser).usingResource(tesf).createCustomContent(testc1, "D:tas:document",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyC", "testc1 text")
+                        .addProperty("tas:IntPropertyC", 2221));
+        dataContent.usingUser(testUser).usingResource(tesf).createCustomContent(testc2, "D:tas:document",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyC", "testc2 text")
+                        .addProperty("tas:IntPropertyC", 2222));
+        dataContent.usingUser(testUser).usingResource(tesf).createCustomContent(testc3, "D:tas:document",
+                new CustomObjectTypeProperties().addProperty("tas:TextPropertyC", "testc3 text")
+                        .addProperty("tas:IntPropertyC", 2223));
+
         // wait for solr index
         Utility.waitToLoopTime(getSolrWaitTimeInSeconds());
     }
 
-    @Test(groups = { TestGroup.CMIS },
-            dataProviderClass = XMLTestDataProvider.class, dataProvider = "getQueriesData", dependsOnMethods = "prepareDataForSearchWithProperty")
+    @Test(groups = { TestGroup.CMIS }, dataProviderClass = XMLTestDataProvider.class, dataProvider = "getQueriesData")
     @XMLDataConfig(file = "src/test/resources/testdata/search-by-property.xml")
-    public void executeSortedSearchByID(QueryModel query) throws Exception
-    {
-        cmisApi.withQuery(query.getValue())
-            .applyNodeRefsFrom(testData).assertResultsCount().equals(query.getResults());
+    public void executeSearchByAspect(QueryModel query) throws Exception
+    {     
+        cmisApi.authenticateUser(testUser).withQuery(query.getValue()).assertResultsCount().equals(query.getResults());
     }
 }
