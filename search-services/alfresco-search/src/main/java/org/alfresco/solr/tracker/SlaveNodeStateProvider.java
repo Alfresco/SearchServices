c981a204d1697bc439d7ef8a67799f01266d4698
agazzarini <a.gazzarini@sease.io>
2019-10-18 07:00:35 +0200

    [ SEARCH-1752 ] NodeStateProvider and SlaveNodeStateProvider

---
+++ search-services/alfresco-search/src/main/java/org/alfresco/solr/tracker/SlaveNodeStateProvider.java
@@ -0,0 +1,65 @@
+package org.alfresco.solr.tracker;
+
+import org.alfresco.httpclient.AuthenticationException;
+import org.alfresco.repo.index.shard.ShardState;
+import org.apache.commons.codec.EncoderException;
+
+import java.io.IOException;
+
+/**
+ * Despite belonging to the Tracker ecosystem, this component is actually a publisher, which periodically informs
+ * Alfresco about the state of the hosting slave node.
+ * As the name suggests, this worker is scheduled only when the hosting node acts as a slave.
+ * It allows Solr's master/slave setup to be used with dynamic shard registration.
+ *
+ * In this scenario the slave is polling a "tracking" Solr node. The tracker below calls
+ * the repo to register the state of the node without pulling any real transactions from the repo.
+ *
+ * This allows the repo to register the replica so that it will be included in queries. But the slave Solr node
+ * will pull its data from a "tracking" Solr node using Solr's master/slave replication, rather then tracking the repository.
+ *
+ * @author Andrea Gazzarini
+ * @since 1.5
+ */
+public class SlaveNodeStateProvider extends MetadataTracker
+{
+    @Override
+    protected void doTrack()
+    {
+        try
+        {
+            ShardState shardstate = getShardState();
+            client.getTransactions(0L, null, 0L, null, 0, shardstate);
+        }
+        catch (EncoderException exception )
+        {
+            log.error("Unable to publish this node state. " +
+                    "A failure condition has been met during the outbound subscription message encoding process. " +
+                    "See the stacktrace below for further details.", exception);
+        }
+        catch (IOException exception )
+        {
+            log.error("Unable to publish this node state. " +
+                    "Detected an I/O failure while sending the subscription state message. " +
+                    "See the stacktrace below for further details.", exception);
+        }
+        catch (AuthenticationException exception)
+        {
+            log.error("Unable to publish this node state. " +
+                    "Authentication failure detected while sending the subscription state message. " +
+                    "See the stacktrace below for further details.", exception);
+        }
+    }
+
+    @Override
+    public void maintenance()
+    {
+        // Do nothing here
+    }
+
+    @Override
+    public boolean hasMaintenance()
+    {
+        return false;
+    }
+}
