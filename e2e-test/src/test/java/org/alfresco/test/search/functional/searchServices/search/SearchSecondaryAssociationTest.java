e1ac8148956a15b29c370ffd435f7fd77a9e6342
mbhave <meenal.bhave@alfresco.com>
2019-11-22 12:21:04 +0000

    Search-1313: Added copyright info

--- e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/search/SearchSecondaryAssociationTest.java
+++ e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/search/SearchSecondaryAssociationTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2019 Alfresco Software, Ltd. All rights reserved.
+ * License rights for this program may be obtained from Alfresco Software, Ltd.
+ * pursuant to a written agreement and any use of this program without such an
+ * agreement is prohibited.
+ */
+
 package org.alfresco.test.search.functional.searchServices.search;
 
 import org.alfresco.rest.model.RestNodeAssociationModelCollection;
