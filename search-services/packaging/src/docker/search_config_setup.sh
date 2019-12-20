f3b612334976877ce8db144b5a48af6eb39689d8
sridharvellingiri <sridharraj.vellingiri@alfresco.com>
2019-12-20 14:39:14 +0000

    SEARCH-1894 Update Slave replica

--- search-services/packaging/src/docker/search_config_setup.sh
+++ search-services/packaging/src/docker/search_config_setup.sh
@@ -4,39 +4,53 @@ set -e
 # Slave replica service can be enabled using "REPLICATION_TYPE=slave" environment value.
 
 SOLR_CONFIG_FILE=$PWD/solrhome/templates/rerank/conf/solrconfig.xml
+
 if [[ $REPLICATION_TYPE == "master" ]]; then
 
-   findStringMaster='<requestHandler name="\/replication" class="org\.alfresco\.solr\.handler\.AlfrescoReplicationHandler">/<requestHandler name="\/replication" class="org\.alfresco\.solr\.handler\.AlfrescoReplicationHandler">'
+   findStringMaster='<requestHandler name="\/replication" class="org\.alfresco\.solr\.handler\.AlfrescoReplicationHandler">'
+
    replaceStringMaster="\n\t<lst name=\"master\"> \n"
+
    if [[ $REPLICATION_AFTER == "" ]]; then
       REPLICATION_AFTER=commit
    fi
+
    for i in $(echo $REPLICATION_AFTER | sed "s/,/ /g")
    do
       replaceStringMaster+="\t\t<str name=\"replicateAfter\">"$i"<\/str> \n"
    done
+
    if [[ ! -z "$REPLICATION_CONFIG_FILES" ]]; then
       replaceStringMaster+="\t\t<str name=\"confFiles\">$REPLICATION_CONFIG_FILES<\/str> \n"
    fi
+
    replaceStringMaster+="\t<\/lst>"
+
    sed -i "s/$findStringMaster/$findStringMaster$replaceStringMaster/g" $SOLR_CONFIG_FILE
 fi
+
 if [[ $REPLICATION_TYPE == "slave" ]]; then
+
    if [[ $REPLICATION_MASTER_PROTOCOL == "" ]]; then
       REPLICATION_MASTER_PROTOCOL=http
    fi
+
    if [[ $REPLICATION_MASTER_HOST == "" ]]; then
       REPLICATION_MASTER_HOST=localhost
    fi
+
    if [[ $REPLICATION_MASTER_PORT == "" ]]; then
       REPLICATION_MASTER_PORT=8083
    fi
+
    if [[ $REPLICATION_CORE_NAME == "" ]]; then
       REPLICATION_CORE_NAME=alfresco
    fi
+
    if [[ $REPLICATION_POLL_INTERVAL == "" ]]; then
       REPLICATION_POLL_INTERVAL=00:00:30
    fi
+
    sed -i 's/<requestHandler name="\/replication" class="org\.alfresco\.solr\.handler\.AlfrescoReplicationHandler">/<requestHandler name="\/replication" class="org\.alfresco\.solr\.handler\.AlfrescoReplicationHandler">\
       <lst name="slave">\
          <str name="masterUrl">'$REPLICATION_MASTER_PROTOCOL':\/\/'$REPLICATION_MASTER_HOST':'$REPLICATION_MASTER_PORT'\/solr\/'$REPLICATION_CORE_NAME'<\/str>\
