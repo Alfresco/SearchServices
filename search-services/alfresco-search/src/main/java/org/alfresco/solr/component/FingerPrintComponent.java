3395d39816f8bc51b269927bc042ea12bcc10ca3
eliaporciani <e.porciani@sease.io>
2020-01-16 17:50:03 +0100

    [SEARCH-1694]
    Removed copy field.
    Fix distributed fingerprint tests

--- search-services/alfresco-search/src/main/java/org/alfresco/solr/component/FingerPrintComponent.java
+++ search-services/alfresco-search/src/main/java/org/alfresco/solr/component/FingerPrintComponent.java
@@ -31,6 +31,8 @@ import org.alfresco.solr.content.SolrContentStore;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.search.LegacyNumericRangeQuery;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
@@ -48,6 +50,9 @@ import org.apache.solr.search.DocIterator;
 import org.apache.solr.search.DocListAndSet;
 import org.apache.solr.search.SolrIndexSearcher;
 import org.apache.solr.util.plugin.SolrCoreAware;
+
+import static org.alfresco.repo.search.adaptor.lucene.QueryConstants.FIELD_FINGERPRINT;
+import static org.alfresco.repo.search.adaptor.lucene.QueryConstants.FIELD_LID;
 
 /**
  * @author Joel Bernstein
@@ -72,30 +77,58 @@ public class FingerPrintComponent extends SearchComponent implements SolrCoreAwa
             return;
         }
 
-        DocListAndSet results = responseBuilder.getResults();
-        if (results.docList.matches() == 0 ){
-            return;
+        String id = responseBuilder.req.getParams().get("id");
+        NamedList response = responseBuilder.rsp.getValues();
+        IndexSchema schema = responseBuilder.req.getSchema();
+        SolrIndexSearcher searcher = responseBuilder.req.getSearcher();
+
+        Query q;
+
+        // Make a distinction:
+        // if id is a number is taken as DBID, otherwise as LID
+        if(isNumber(id))
+        {
+            long dbid = Long.parseLong(id);
+            q = LegacyNumericRangeQuery.newLongRange("DBID", dbid, dbid + 1, true, false);
+        }
+        else
+        {
+            String query = id.startsWith("workspace") ? id : "workspace://SpacesStore/"+id;
+            q = new TermQuery(new Term(FIELD_LID, query));
         }
 
-        int docId = results.docList.iterator().nextDoc();
-
-        NamedList response = responseBuilder.rsp.getValues();
-        Document doc = responseBuilder.req.getSearcher().doc(docId, Set.of("MINHASH"));
+        TopDocs docs = searcher.search(q, 1);
+        Set<String> fields = new HashSet();
+        fields.add("MINHASH");
 
         NamedList fingerPrint = new NamedList();
-
         List<Object> values = new ArrayList<>();
-        IndexSchema schema = responseBuilder.req.getCore().getLatestSchema();
-        IndexableField[] minHashes = doc.getFields("MINHASH");
-        for (IndexableField minHash : minHashes)
-        {
-            SchemaField sf = schema.getFieldOrNull(minHash.name());
-            Object value = DocsStreamer.getValue(sf, minHash);
-            values.add(value);
+        if(docs.totalHits == 1) {
+            ScoreDoc scoreDoc = docs.scoreDocs[0];
+            Document doc = searcher.doc(scoreDoc.doc, fields);
+
+
+            IndexableField[] minHashes = doc.getFields("MINHASH");
+            for (IndexableField minHash : minHashes)
+            {
+                SchemaField sf = schema.getFieldOrNull(minHash.name());
+                Object value = DocsStreamer.getValue(sf, minHash);
+                values.add(value);
+                fingerPrint.add("MINHASH", values);
+            }
         }
 
-        fingerPrint.add("MINHASH", values);
         response.add("fingerprint", fingerPrint);
+    }
+
+    private boolean isNumber(String s) {
+        for(int i=0; i<s.length(); i++) {
+            if(!Character.isDigit(s.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public String getDescription() {
