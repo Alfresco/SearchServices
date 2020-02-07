627dcd1b1441c909b893560f2432c64ecd1f6a23
Angel Borroy <angel.borroy@contractor.alfresco.com>
2020-02-07 09:30:01 +0000

    Solr Admin REST API reworking: * Added action status (success, error, scheduled) for every response * Detailed log with INFO level available in MetadataTracker and AclTracker maintenance actions * E2E Tests

--- search-services/alfresco-search/src/main/java/org/alfresco/solr/tracker/MetadataTracker.java
+++ search-services/alfresco-search/src/main/java/org/alfresco/solr/tracker/MetadataTracker.java
@@ -352,9 +352,14 @@ public class MetadataTracker extends AbstractTracker implements Tracker
 
                     // Index the transaction doc after the node - if this is not found then a reindex will be done.
                     this.infoSrv.indexTransaction(info, false);
+                    log.info("INDEX ACTION - Transaction {} has been indexed", transactionId);
                     requiresCommit = true;
 
                     trackerStats.addTxDocs(nodes.size());
+                }
+                else
+                {
+                    log.info("INDEX ACTION - Transaction {} was not found in database, it has NOT been reindexed", transactionId);
                 }
             }
 
@@ -395,6 +400,7 @@ public class MetadataTracker extends AbstractTracker implements Tracker
                 node.setTxnId(Long.MAX_VALUE);
 
                 this.infoSrv.indexNode(node, false);
+                log.info("INDEX ACTION - Node {} has been reindexed", node.getId());
                 requiresCommit = true;
             }
             checkShutdown();
@@ -444,6 +450,11 @@ public class MetadataTracker extends AbstractTracker implements Tracker
 
                     // Index the transaction doc after the node - if this is not found then a reindex will be done.
                     this.infoSrv.indexTransaction(info, true);
+                    log.info("REINDEX ACTION - Transaction {} has been reindexed", transactionId);
+                }
+                else
+                {
+                    log.info("REINDEX ACTION - Transaction {} was not found in database, it has NOT been reindexed", transactionId);
                 }
             }
 
@@ -486,6 +497,7 @@ public class MetadataTracker extends AbstractTracker implements Tracker
                 node.setTxnId(Long.MAX_VALUE);
 
                 this.infoSrv.indexNode(node, true);
+                log.info("REINDEX ACTION - Node {} has been reindexed", node.getId());
                 requiresCommit = true;
             }
             checkShutdown();
@@ -507,6 +519,7 @@ public class MetadataTracker extends AbstractTracker implements Tracker
             if (query != null)
             {
                 this.infoSrv.reindexNodeByQuery(query);
+                log.info("REINDEX ACTION - Nodes from query {} have been reindexed", query);
                 requiresCommit = true;
             }
             checkShutdown();
@@ -531,6 +544,7 @@ public class MetadataTracker extends AbstractTracker implements Tracker
                 // make sure it is cleaned out so we do not miss deletes
                 this.infoSrv.deleteByTransactionId(transactionId);
                 requiresCommit = true;
+                log.info("PURGE ACTION - Purged transactionId {}", transactionId);
             }
             checkShutdown();
         }
@@ -551,6 +565,7 @@ public class MetadataTracker extends AbstractTracker implements Tracker
             {
                 // make sure it is cleaned out so we do not miss deletes
                 this.infoSrv.deleteByNodeId(nodeId);
+                log.info("PURGE ACTION - Purged nodeId {}", nodeId);
             }
             checkShutdown();
         }
