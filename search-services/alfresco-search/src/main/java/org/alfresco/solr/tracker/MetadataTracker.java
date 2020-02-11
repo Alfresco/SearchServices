7e16a5bd5f62c910d57f962f63d8474c82c83df4
Angel Borroy <angel.borroy@contractor.alfresco.com>
2020-02-11 10:01:27 +0000

    Merge branch 'feature/SEARCH-2014_SolrAdminAPI_14x' into 'release/V1.4.x'
    Feature/search 2014 solr admin api 14x
    
    See merge request search_discovery/insightengine!368
    
    (cherry picked from commit 98343dfc59425a4f07fd4450d104e8c426b72802)

--- search-services/alfresco-search/src/main/java/org/alfresco/solr/tracker/MetadataTracker.java
+++ search-services/alfresco-search/src/main/java/org/alfresco/solr/tracker/MetadataTracker.java
@@ -359,9 +359,14 @@ public class MetadataTracker extends CoreStatePublisher implements Tracker
 
                     // Index the transaction doc after the node - if this is not found then a reindex will be done.
                     this.infoSrv.indexTransaction(info, false);
+                    log.info("INDEX ACTION - Transaction {} has been indexed", transactionId);
                     requiresCommit = true;
 
                     trackerStats.addTxDocs(nodes.size());
+                }
+                else
+                {
+                    log.info("INDEX ACTION - Transaction {} was not found in database, it has NOT been reindexed", transactionId);
                 }
             }
 
@@ -402,6 +407,7 @@ public class MetadataTracker extends CoreStatePublisher implements Tracker
                 node.setTxnId(Long.MAX_VALUE);
 
                 this.infoSrv.indexNode(node, false);
+                log.info("INDEX ACTION - Node {} has been reindexed", node.getId());
                 requiresCommit = true;
             }
             checkShutdown();
@@ -452,6 +458,11 @@ public class MetadataTracker extends CoreStatePublisher implements Tracker
 
                     // Index the transaction doc after the node - if this is not found then a reindex will be done.
                     this.infoSrv.indexTransaction(info, true);
+                    log.info("REINDEX ACTION - Transaction {} has been reindexed", transactionId);
+                }
+                else
+                {
+                    log.info("REINDEX ACTION - Transaction {} was not found in database, it has NOT been reindexed", transactionId);
                 }
             }
 
@@ -494,6 +505,7 @@ public class MetadataTracker extends CoreStatePublisher implements Tracker
                 node.setTxnId(Long.MAX_VALUE);
 
                 this.infoSrv.indexNode(node, true);
+                log.info("REINDEX ACTION - Node {} has been reindexed", node.getId());
                 requiresCommit = true;
             }
             checkShutdown();
@@ -515,6 +527,7 @@ public class MetadataTracker extends CoreStatePublisher implements Tracker
             if (query != null)
             {
                 this.infoSrv.reindexNodeByQuery(query);
+                log.info("REINDEX ACTION - Nodes from query {} have been reindexed", query);
                 requiresCommit = true;
             }
             checkShutdown();
@@ -539,6 +552,7 @@ public class MetadataTracker extends CoreStatePublisher implements Tracker
                 // make sure it is cleaned out so we do not miss deletes
                 this.infoSrv.deleteByTransactionId(transactionId);
                 requiresCommit = true;
+                log.info("PURGE ACTION - Purged transactionId {}", transactionId);
             }
             checkShutdown();
         }
@@ -559,6 +573,7 @@ public class MetadataTracker extends CoreStatePublisher implements Tracker
             {
                 // make sure it is cleaned out so we do not miss deletes
                 this.infoSrv.deleteByNodeId(nodeId);
+                log.info("PURGE ACTION - Purged nodeId {}", nodeId);
             }
             checkShutdown();
         }
--- search-services/alfresco-search/src/test/java/org/alfresco/solr/AlfrescoCoreAdminHandlerIT.java
+++ search-services/alfresco-search/src/test/java/org/alfresco/solr/AlfrescoCoreAdminHandlerIT.java
@@ -368,7 +368,7 @@ public class AlfrescoCoreAdminHandlerIT
 
         invalidNames.forEach(spy::setupNewDefaultCores);
 
-        verify(spy, never()).newCore(any(), anyInt(), any(), any(), anyInt(), anyInt(), anyInt(), any(), any(), any());
+        verify(spy, never()).newCore(any(), anyInt(), any(), any(), anyInt(), anyInt(), anyInt(), any(), any());
 
         reset(spy);
 
@@ -376,16 +376,17 @@ public class AlfrescoCoreAdminHandlerIT
         String commaSeparatedNames = String.join(",", invalidNames);
         spy.setupNewDefaultCores(commaSeparatedNames);
 
-        verify(spy, never()).newCore(any(), anyInt(), any(), any(), anyInt(), anyInt(), anyInt(), any(), any(), any());
+        verify(spy, never()).newCore(any(), anyInt(), any(), any(), anyInt(), anyInt(), anyInt(), any(), any());
     }
 
     @Test
     public void coreNamesAreTrimmed_oneCoreNameAtTime() {
         AlfrescoCoreAdminHandler spy = spy(new AlfrescoCoreAdminHandler() {
             @Override
-            protected void newCore(String coreName, int numShards, StoreRef storeRef, String templateName, int replicationFactor, int nodeInstance, int numNodes, String shardIds, Properties extraProperties, SolrQueryResponse rsp)
+            protected NamedList<Object> newCore(String coreName, int numShards, StoreRef storeRef, String templateName, int replicationFactor, int nodeInstance, int numNodes, String shardIds, Properties extraProperties)
             {
                 // Do nothing here otherwise we cannot spy it
+                return null;
             }
         });
 
@@ -399,18 +400,19 @@ public class AlfrescoCoreAdminHandlerIT
 
         coreNames.forEach(spy::setupNewDefaultCores);
 
-        verify(spy).newCore(eq(ARCHIVE_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(ARCHIVE_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null), any());
-        verify(spy).newCore(eq(ALFRESCO_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(ALFRESCO_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null), any());
-        verify(spy).newCore(eq(VERSION_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(VERSION_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null), any());
+        verify(spy).newCore(eq(ARCHIVE_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(ARCHIVE_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null));
+        verify(spy).newCore(eq(ALFRESCO_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(ALFRESCO_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null));
+        verify(spy).newCore(eq(VERSION_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(VERSION_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null));
     }
 
     @Test
     public void validAndInvalidCoreNames() {
         AlfrescoCoreAdminHandler spy = spy(new AlfrescoCoreAdminHandler() {
             @Override
-            protected void newCore(String coreName, int numShards, StoreRef storeRef, String templateName, int replicationFactor, int nodeInstance, int numNodes, String shardIds, Properties extraProperties, SolrQueryResponse rsp)
+            protected NamedList<Object> newCore(String coreName, int numShards, StoreRef storeRef, String templateName, int replicationFactor, int nodeInstance, int numNodes, String shardIds, Properties extraProperties)
             {
                 // Do nothing here otherwise we cannot spy it
+                return null;
             }
         });
 
@@ -426,8 +428,8 @@ public class AlfrescoCoreAdminHandlerIT
         String commaSeparatedNames = String.join(",", coreNames);
         spy.setupNewDefaultCores(commaSeparatedNames);
 
-        verify(spy).newCore(eq(ARCHIVE_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(ARCHIVE_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null), any());
-        verify(spy).newCore(eq(ALFRESCO_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(ALFRESCO_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null), any());
-        verify(spy).newCore(eq(VERSION_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(VERSION_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null), any());
+        verify(spy).newCore(eq(ARCHIVE_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(ARCHIVE_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null));
+        verify(spy).newCore(eq(ALFRESCO_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(ALFRESCO_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null));
+        verify(spy).newCore(eq(VERSION_CORE_NAME), eq(1), eq(STORE_REF_MAP.get(VERSION_CORE_NAME)), anyString(), eq(1), eq(1), eq(1), eq(null), eq(null));
     }
 }
