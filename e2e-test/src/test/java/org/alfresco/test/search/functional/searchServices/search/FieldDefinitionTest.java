872d6edb609e104f674c94156187ba1bb1b93966
Keerat <keerat.lalia@alfresco.com>
2020-05-06 13:18:36 +0100

    SEARCH_2139 Adding e2e test coverage for the text and mltext fields in the solr schema

--- e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/search/FieldDefinitionTest.java
+++ e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/search/FieldDefinitionTest.java
@@ -3,7 +3,6 @@ package org.alfresco.test.search.functional.searchServices.search;
 import org.alfresco.rest.search.SearchResponse;
 import org.alfresco.utility.data.CustomObjectTypeProperties;
 import org.alfresco.utility.model.FileModel;
-import org.alfresco.utility.model.FolderModel;
 import org.springframework.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -15,40 +14,154 @@ import org.testng.annotations.Test;
  */
 public class FieldDefinitionTest extends AbstractSearchServicesE2ETest {
 	
-	private FolderModel Folder1;
-	private FileModel File1;
+	private FileModel File1, File2;
 		
 	@BeforeClass(alwaysRun = true)
     public void dataPreparation() throws Exception
     {        
         dataContent.usingAdmin().deployContentModel("model/search-2139-model.xml");
         
-        Folder1 = new FolderModel("folder1");
-        
         File1 = new FileModel("standard-file1.txt");
-        File1.setContent("file1");
-        
-    //    dataContent.usingUser(testUser).usingResource(Folder1).createCustomContent(File1, "D:text:document", new CustomObjectTypeProperties()
-    //            .addProperty("allfieldtypes:mltextLOVPartial", "text file-1"));
-        
-     //   dataContent.usingUser(testUser).usingSite(testSite).createCustomContent(File1, "D:allfieldtypes:document", new CustomObjectTypeProperties()
-       //         .addProperty("allfieldtypes:mltextLOVPartial", "text folder-2"));
         
         dataContent.usingUser(testUser).usingSite(testSite).createCustomContent(File1, "cmis:document", new CustomObjectTypeProperties());
 
-        cmisApi.authenticateUser(testUser).usingResource(File1).addSecondaryTypes("P:allfieldtypes:text", "P:allfieldtypes:content")
-                .updateProperty("allfieldtypes:mltextLOVPartial", "field definition test")
-                .updateProperty("allfieldtypes:textPatternUnique", "field definition test");
-//                .updateProperty("allfieldtypes:contentTokenizedBoth", "field definition content test");
+        cmisApi.authenticateUser(testUser).usingResource(File1).addSecondaryTypes("P:allfieldtypes:text")
+                .updateProperty("allfieldtypes:mltextLOVPartial", "file1")
+                .updateProperty("allfieldtypes:textPatternUnique", "file2")
+		        .updateProperty("allfieldtypes:mltextFree", "file3")
+		        .updateProperty("allfieldtypes:textLOVPartial", "file4");
+        
+        File2 = new FileModel("standard-file2.txt");
+        
+        dataContent.usingUser(testUser).usingSite(testSite).createCustomContent(File2, "cmis:document", new CustomObjectTypeProperties());
 
-
+        cmisApi.authenticateUser(testUser).usingResource(File2).addSecondaryTypes("P:allfieldtypes:text")
+                .updateProperty("allfieldtypes:textFree", "text field definition test")
+                .updateProperty("allfieldtypes:textPatternMany", "mltext field definition test")
+		        .updateProperty("allfieldtypes:textLOVWhole", "text field not tokenised")
+		        .updateProperty("allfieldtypes:mltextLOVWhole", "mltext field not tokenised");
+        
+        waitForMetadataIndexing(File1.getName(), true);
+        waitForMetadataIndexing(File2.getName(), true);
     }
 	
+	// A test to test the text field in the solr schema, using a single word 
 	@Test(priority = 1)
     public void testTextField()
     {
-		SearchResponse response = queryAsUser(testUser, "allfieldtypes_mltextLOVPartial:text file-1");
+		SearchResponse response = queryAsUser(testUser, "allfieldtypes_textPatternUnique:file2");
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        Assert.assertEquals(response.getPagination().getCount(), 1);
+        
+        response = queryAsUser(testUser, "allfieldtypes_textLOVPartial:file4");
         restClient.assertStatusCodeIs(HttpStatus.OK);
         Assert.assertEquals(response.getPagination().getCount(), 1);
     }
+	
+	// A test to test the mltext field in the solr schema, using a single word 
+	@Test(priority = 2)
+    public void testmlTextField()
+    {
+		SearchResponse response = queryAsUser(testUser, "allfieldtypes_mltextLOVPartial:file1");
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        Assert.assertEquals(response.getPagination().getCount(), 1);
+        
+        response = queryAsUser(testUser, "allfieldtypes_mltextFree:file3");
+        restClient.assertStatusCodeIs(HttpStatus.OK);
+        Assert.assertEquals(response.getPagination().getCount(), 1);
+    }
+
+	// A test to test the text field in the solr schema, where the field hasn't been defined
+	@Test(priority = 3)
+	public void testTextFieldNotDefined()
+	{
+		SearchResponse response = queryAsUser(testUser, "allfieldtypes_textPatternMany:file1");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 0);
+	    
+	    response = queryAsUser(testUser, "allfieldtypes:textNone:file2");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 0);
+	}
+	
+	// A test to test the mltext field in the solr schema, where the field hasn't been defined
+	@Test(priority = 4)
+	public void testmlTextFieldNotDefined()
+	{
+		SearchResponse response = queryAsUser(testUser, "allfieldtypes_mltextNone:file1");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 0);
+	    
+	    response = queryAsUser(testUser, "allfieldtypes_mltextPatternUnique:file2");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 0);
+	}
+	
+	// A test to test the tokenised text field in the solr schema
+	@Test(priority = 5)
+	public void testTextFieldTokenised()
+	{
+		SearchResponse response = queryAsUser(testUser, "allfieldtypes_textFree:\"text field definition test\"");
+	    restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 1);
+	    
+	    response = queryAsUser(testUser, "allfieldtypes_textFree:\"text\"");
+	    restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 1);
+	    
+	    response = queryAsUser(testUser, "allfieldtypes_textFree:\"definition test\"");
+	    restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 1);
+	}
+	
+	// A test to test a non tokenised text field in the solr schema
+	@Test(priority = 6)
+	public void testTextFieldNotTokenised()
+	{
+		SearchResponse response = queryAsUser(testUser, "allfieldtypes_textLOVWhole:\"text field not tokenised\"");
+	    restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 1);
+	    
+	    response = queryAsUser(testUser, "allfieldtypes_textLOVWhole:\"text\"");
+	    restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 0);
+	    
+	    response = queryAsUser(testUser, "allfieldtypes_textLOVWhole:\"field not\"");
+	    restClient.assertStatusCodeIs(HttpStatus.OK);
+	    Assert.assertEquals(response.getPagination().getCount(), 0);
+	}
+	
+	// A test to test the tokenised mltext field in the solr schema
+	@Test(priority = 7)
+	public void testmlTextFieldTokenised()
+	{
+		SearchResponse response = queryAsUser(testUser, "allfieldtypes_textPatternMany:\"mltext field definition test\"");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+		Assert.assertEquals(response.getPagination().getCount(), 1);
+		
+		response = queryAsUser(testUser, "allfieldtypes_textPatternMany:\"mltext\"");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+		Assert.assertEquals(response.getPagination().getCount(), 1);
+		
+		response = queryAsUser(testUser, "allfieldtypes_textPatternMany:\"field definition\"");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+		Assert.assertEquals(response.getPagination().getCount(), 1);
+	}
+	
+	// A test to test the non tokenised mltext field in the solr schema
+	@Test(priority = 7)
+	public void testmlTextFieldNotTokenised()
+	{
+		SearchResponse response = queryAsUser(testUser, "allfieldtypes_mltextLOVWhole:\"mltext field not tokenised\"");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+		Assert.assertEquals(response.getPagination().getCount(), 1);
+		
+		response = queryAsUser(testUser, "allfieldtypes_mltextLOVWhole:\"mltext\"");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+		Assert.assertEquals(response.getPagination().getCount(), 0);
+		
+		response = queryAsUser(testUser, "allfieldtypes_mltextLOVWhole:\"not tokenised\"");
+		restClient.assertStatusCodeIs(HttpStatus.OK);
+		Assert.assertEquals(response.getPagination().getCount(), 0);
+	}
 }
