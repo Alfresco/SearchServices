3bf3677779b8cdf9cf208ae26ffa61d5b5f494d3
Alex Mukha <alex.mukha@alfresco.com>
2020-07-08 12:11:35 +0100

    Add config for CMIS API tests

---
+++ e2e-test/helpers/start-alfresco.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eux
+
+# Start Alfresco and Solr.
+
+# The location for the docker-compose files.
+DOCKER_RESOURCE_FOLDER=$1
+# The search docker image.
+SEARCH_IMAGE=$2
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export DOCKER_CLIENT_TIMEOUT=120
+export COMPOSE_HTTP_TIMEOUT=120
+
+# Pull the image, to ensure we're not using a stale image from the build agent.
+docker pull "$SEARCH_IMAGE"
+
+# Build the images and call docker-compose.
+cd "$DOCKER_RESOURCE_FOLDER"
+docker-compose up -d --build --force-recreate
+
+$SCRIPT_DIR/wait-service-to-start.sh
