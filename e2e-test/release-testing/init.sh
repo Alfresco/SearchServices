0ef7f260513c7348181f541fccdcab568f23fe4b
dcerbo <davide.cerbo@alfresco.com>
2021-07-05 16:57:12 +0200

    SEARCH-2885 add error handling when version is wrong

--- e2e-test/release-testing/init.sh
+++ e2e-test/release-testing/init.sh
@@ -1,4 +1,12 @@
 ALF_VERSION=$1
+
+if [ ! -f $ALF_VERSION.env ]; then
+    echo "Version \"$ALF_VERSION\" not found!"
+    echo "Available versions are:" 
+    echo "$(ls *.env | awk '{ print substr( $0, 0, length($0)-4 ) }')"
+    exit 1
+fi
+
 echo "Copying .env for version $ALF_VERSION"
 FOLDERS=$(find . -name 'docker-compose.yml' -exec dirname {} \;)
 
