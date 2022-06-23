e0e651322fb87bf22749bd58f8d2539844ac016f
Domenico Sibilio <domenicosibilio@gmail.com>
2022-06-23 12:43:33 +0200

    ACS-3193 isNull(Class<T>) has been removed
    (cherry picked from commit cfb56f33d1e7116eaa5e0661e3a11a506a405929)

--- search-services/alfresco-search/src/test/java/org/alfresco/solr/tracker/MetadataTrackerTest.java
+++ search-services/alfresco-search/src/test/java/org/alfresco/solr/tracker/MetadataTrackerTest.java
@@ -127,7 +127,7 @@ public class MetadataTrackerTest
         // Subsequent calls to getTransactions must return a different set of transactions to avoid an infinite loop
         when(repositoryClient.getTransactions(anyLong(), anyLong(), anyLong(), anyLong(), anyInt())).thenReturn(txs)
                     .thenReturn(txs).thenReturn(mock(Transactions.class));
-        when(repositoryClient.getTransactions(anyLong(), anyLong(), anyLong(), anyLong(), anyInt(), isNull(ShardState.class))).thenReturn(txs)
+        when(repositoryClient.getTransactions(anyLong(), anyLong(), anyLong(), anyLong(), anyInt(), isNull())).thenReturn(txs)
         .thenReturn(txs).thenReturn(mock(Transactions.class));
         when(repositoryClient.getTransactions(anyLong(), anyLong(), anyLong(), anyLong(), anyInt(), any(ShardState.class))).thenReturn(txs)
         .thenReturn(txs).thenReturn(mock(Transactions.class));
@@ -157,7 +157,7 @@ public class MetadataTrackerTest
         List<Transaction> txsList = new ArrayList<>();
         when(txs.getTransactions()).thenReturn(txsList);
 
-        when(repositoryClient.getTransactions(anyLong(), anyLong(), anyLong(), anyLong(), anyInt(), isNull(ShardState.class))).thenReturn(txs);
+        when(repositoryClient.getTransactions(anyLong(), anyLong(), anyLong(), anyLong(), anyInt(), isNull())).thenReturn(txs);
         when(repositoryClient.getTransactions(anyLong(), anyLong(), anyLong(), anyLong(), anyInt(), any(ShardState.class))).thenReturn(txs);
         when(repositoryClient.getTransactions(anyLong(), anyLong(), anyLong(), anyLong(), anyInt())).thenReturn(txs);
 
