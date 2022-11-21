cb30043b35b629524f446a38de8e1235490f51e7
kcichonczyk <kamil.cichonczyk@hyland.com>
2022-11-21 16:38:44 +0100

    added header

--- e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/search/SearchCasesTest.java
+++ e2e-test/src/test/java/org/alfresco/test/search/functional/searchServices/search/SearchCasesTest.java
@@ -1,3 +1,14 @@
+/*-
+ * #%L
+ * Alfresco Elasticsearch Connector :: Database Connector
+ * %%
+ * Copyright (C) 2022 Alfresco Software Limited
+ * %%
+ * License rights for this program may be obtained from Alfresco Software, Ltd.
+ * pursuant to a written agreement and any use of this program without such an
+ * agreement is prohibited.
+ * #L%
+ */
 package org.alfresco.test.search.functional.searchServices.search;
 
 import org.alfresco.rest.search.FacetFieldBucket;
